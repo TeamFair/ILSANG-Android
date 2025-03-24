1d899f15de65facaac9235f179dc255ec97355b3
kimseongwoo <tjddn929@naver.com>
2025-03-25 00:14:15 +0900

    [Chore] Compose Preview 기능을 위한 라이브러리 추가

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
