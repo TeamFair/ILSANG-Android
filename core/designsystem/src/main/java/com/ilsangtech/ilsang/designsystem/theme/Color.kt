b5581021ef1ea32025da385b9baa7092a4612188
kimseongwoo <tjddn929@naver.com>
2025-03-24 23:02:35 +0900

    [Chore] 일상 색상 리소스 추가

---
+++ core/designsystem/src/main/java/com/ilsangtech/ilsang/designsystem/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.ilsangtech.ilsang.designsystem.theme
+
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFF7C25EB)
+val primary500 = Color(0xFF6512CF)
+val primary300 = Color(0xFFA367F1)
+val primary200 = Color(0xFFE5D3FB)
+val primary100 = Color(0xFFF5EEFD)
+
+val secondary = Color(0xFF5DFA7F)
+
+val gray500 = Color(0xFF575B6C)
+val gray400 = Color(0xFF848794)
+val gray300 = Color(0xFFB6B7BD)
+val gray200 = Color(0xFFCACBD1)
+val gray100 = Color(0xFFE9EAEC)
+
+val background = Color(0xFFF6F6F6)
+
+val ilsangColors = lightColorScheme(
+    primary = primary,
+    secondary = secondary,
+    background = background
+)
