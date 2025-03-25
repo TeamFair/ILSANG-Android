e95d6979be1807d00ea41f404a6a21ae2ac82235
kimseongwoo <tjddn929@naver.com>
2025-03-25 21:38:21 +0900

    [Refactor] 기본 템플릿 코드 제거

--- app/src/main/java/com/ilsangtech/ilsang/MainActivity.kt
+++ app/src/main/java/com/ilsangtech/ilsang/MainActivity.kt
@@ -10,21 +10,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ilsangtech.ilsang.ui.theme.ILSANGTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ILSANGTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
@@ -36,12 +32,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ILSANGTheme {
-        Greeting("Android")
-    }
 }
