7a3a5a26d3e446de536b7ae50922f3bc81bd078d
kimseongwoo <tjddn929@naver.com>
2025-03-29 17:05:56 +0900

    [Chore] Tutorial 모듈, TutorialScreen 생성

---
+++ feature/tutorial/src/main/java/com/ilsangtech/ilsang/feature/tutorial/TutorialScreen.kt
@@ -0,0 +1,15 @@
+package com.ilsangtech.ilsang.feature.tutorial
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TutorialScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TutorialScreenPreview() {
+    TutorialScreen()
+}
