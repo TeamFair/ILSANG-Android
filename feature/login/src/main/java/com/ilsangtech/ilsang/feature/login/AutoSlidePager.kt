7b3184d8c540830cef7a0bc7728586d1ea2dc807
kimseongwoo <tjddn929@naver.com>
2025-03-29 15:51:06 +0900

    [Feat] 자동 순환 스크롤 페이저 구현 - 2.5초 주기로 0.4초에 걸쳐 페이지 자동 전환 - 현재 페이지 외 다른 페이지는 크기 조절, 알파 값 적용 - 마지막 페이지에 도달할 때 첫 번째 페이지가 옆에 보여지기까지 약간의 시간이 소요

---
+++ feature/login/src/main/java/com/ilsangtech/ilsang/feature/login/AutoSlidePager.kt
@@ -0,0 +1,86 @@
+package com.ilsangtech.ilsang.feature.login
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+
+@Composable
+fun AutoSlidePager(
+    modifier: Modifier = Modifier,
+    pageList: List<@Composable () -> Unit>
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val actualPageCount = 5
+    val fakePageCount = actualPageCount + 2
+    val pagerState = rememberPagerState(initialPage = 1) { fakePageCount }
+
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val contentPadding = (screenWidth - 180.dp) / 2
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(2500)
+            coroutineScope.launch {
+                pagerState.animateScrollToPage(
+                    page = pagerState.currentPage + 1,
+                    animationSpec = tween(
+                        durationMillis = 400,
+                        easing = LinearOutSlowInEasing
+                    )
+                )
+            }
+        }
+    }
+
+    LaunchedEffect(pagerState.currentPage) {
+        coroutineScope.launch {
+            when (pagerState.currentPage) {
+                0 -> { // 첫 번째 앞의 가짜 페이지
+                    delay(200)
+                    pagerState.scrollToPage(page = actualPageCount) // 마지막 페이지로 순간 이동
+                }
+
+                fakePageCount - 1 -> { // 마지막 뒤의 가짜 페이지
+                    delay(200)
+                    pagerState.scrollToPage(1) // 첫 번째 페이지로 순간 이동
+                }
+            }
+        }
+    }
+    HorizontalPager(
+        modifier = modifier.fillMaxWidth(),
+        pageSize = PageSize.Fixed(180.dp),
+        contentPadding = PaddingValues(horizontal = contentPadding),
+        state = pagerState,
+    ) { pageIndex ->
+        val pageOffset = (pagerState.currentPage - pageIndex) + pagerState.currentPageOffsetFraction
+        val scale = lerp(0.83f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
+        val pageAlpha = lerp(0.2f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
+
+        Box(
+            modifier = Modifier.graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+                alpha = pageAlpha
+            }
+        ) {
+            pageList[pageIndex].invoke()
+        }
+    }
+}
