971e851f780dc30adb7ed4458ef31860cd9cab0b
kimseongwoo <tjddn929@naver.com>
2025-03-29 16:17:19 +0900

    [Feat] LoginScreen 구현

--- feature/login/src/main/java/com/ilsangtech/ilsang/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/ilsangtech/ilsang/feature/login/LoginScreen.kt
@@ -1,10 +1,12 @@
 package com.ilsangtech.ilsang.feature.login
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -15,6 +17,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -42,7 +45,32 @@ fun LoginScreen() {
             .fillMaxSize()
             .navigationBarsPadding(),
         color = Color.White
-    ) {}
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(Modifier.weight(1f))
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                LoginTitle()
+                Spacer(Modifier.height(28.dp))
+                AutoSlidePager(
+                    pageList = listOf(
+                        { WaterQuestCard() },
+                        { MusicQuestCard() },
+                        { WeatherQuestCard() },
+                        { CoffeeQuestCard() },
+                        { EggQuestCard() },
+                        { WaterQuestCard() },
+                        { MusicQuestCard() }
+                    )
+                )
+            }
+
+            Spacer(Modifier.weight(1f))
+            LoginButton { }
+            Spacer(Modifier.height(38.dp))
+        }
+    }
 }
 
 @Composable
