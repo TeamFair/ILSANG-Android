f1a9ba997ddc2c9d9a8809d9e1c61345fb8638fe
kimseongwoo <tjddn929@naver.com>
2025-03-29 15:55:49 +0900

    [Feat] LoginButton 구현

--- feature/login/src/main/java/com/ilsangtech/ilsang/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/ilsangtech/ilsang/feature/login/LoginScreen.kt
@@ -1,17 +1,36 @@
 package com.ilsangtech.ilsang.feature.login
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ilsangtech.ilsang.designsystem.R.font.pretendard_bold
+import com.ilsangtech.ilsang.designsystem.theme.background
+import com.ilsangtech.ilsang.designsystem.theme.gray100
 import com.ilsangtech.ilsang.designsystem.theme.primary
 import com.ilsangtech.ilsang.designsystem.theme.secondary
 import com.ilsangtech.ilsang.designsystem.theme.title01
@@ -53,6 +72,44 @@ fun LoginTitle() {
     )
 }
 
+@Composable
+fun LoginButton(login: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = background,
+        ),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 18.dp),
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = gray100
+        ),
+        onClick = login
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.google_logo),
+            tint = Color.Unspecified,
+            contentDescription = null
+        )
+        Spacer(Modifier.width(16.dp))
+        Text(
+            text = "Google로 로그인하기",
+            color = Color.Black,
+            style = loginButtonTextStyle
+        )
+    }
+}
+
+private val loginButtonTextStyle = TextStyle(
+    color = Color.Black,
+    fontSize = 15.sp,
+    fontFamily = FontFamily(Font(pretendard_bold)),
+    lineHeight = 24.sp,
+)
+
 @Preview
 @Composable
 fun LoginScreenPreview() {
@@ -63,4 +120,10 @@ fun LoginScreenPreview() {
 @Composable
 fun LoginTitlePreview() {
     LoginTitle()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginButtonPreview() {
+    LoginButton {}
 }
