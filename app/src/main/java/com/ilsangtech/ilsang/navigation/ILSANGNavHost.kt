3507e7dec9a07b2acb902d055b78238c0b32f333
kimseongwoo <tjddn929@naver.com>
2025-03-30 16:19:00 +0900

    [Feat] NavHost 함수 구현

---
+++ app/src/main/java/com/ilsangtech/ilsang/navigation/ILSANGNavHost.kt
@@ -0,0 +1,34 @@
+package com.ilsangtech.ilsang.navigation
+
+import HomeScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ilsangtech.ilsang.feature.login.LoginScreen
+import com.ilsangtech.ilsang.feature.tutorial.TutorialScreen
+
+@Composable
+fun ILSANGNavHost(
+    startDestination: String,
+    login: () -> Unit
+) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable("login") {
+            LoginScreen(login = login)
+        }
+
+        composable("tutorial") {
+            TutorialScreen {
+                navController.navigate("home")
+            }
+        }
+        composable("home") {
+            HomeScreen()
+        }
+    }
+}
