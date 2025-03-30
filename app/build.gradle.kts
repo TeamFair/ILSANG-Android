a20d15bbe408d47b8749ce88ecef02954688ffbc
kimseongwoo <tjddn929@naver.com>
2025-03-30 10:56:44 +0900

    [Chore] feature:login 모듈 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":feature:login"))
+
     implementation(libs.androidx.core.splashscreen)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
