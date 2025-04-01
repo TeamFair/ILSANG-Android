1b16809d5897d2a05630ccb45a3dbdb8454fadb0
kimseongwoo <tjddn929@naver.com>
2025-04-01 23:39:26 +0900

    [Feat] Banner API 인터페이스, Hilt를 통한 싱글톤 객체 생성

---
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/BannerApiService.kt
@@ -0,0 +1,15 @@
+package com.ilsangtech.ilsang.core.network.api
+
+import com.ilsangtech.ilsang.core.network.model.banner.BannerSearchRequest
+import com.ilsangtech.ilsang.core.network.model.banner.NetworkBanner
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BannerApiService {
+    @GET("open/v1/banners")
+    suspend fun getBanners(
+        @Query("searchRequest") searchRequest: BannerSearchRequest,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): List<NetworkBanner>
+}
