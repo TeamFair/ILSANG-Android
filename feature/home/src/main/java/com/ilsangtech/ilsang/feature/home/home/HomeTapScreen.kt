f063adfa02c6f1b569d523f45aa2eb12d7ca9490
kimseongwoo <tjddn929@naver.com>
2025-04-01 20:44:23 +0900

    [Feat] 홈 탭 스크린 함수 생성 및 홈 탭 상단바 구현

---
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/home/HomeTapScreen.kt
@@ -0,0 +1,69 @@
+package com.ilsangtech.ilsang.feature.home.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ilsangtech.ilsang.feature.home.R
+
+@Composable
+fun HomeTapScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn {
+            item { HomeTapTopBar() }
+            item {}
+        }
+    }
+}
+
+@Composable
+fun HomeTapTopBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color.White)
+            .statusBarsPadding()
+            .padding(
+                top = 8.dp,
+                bottom = 16.dp
+            )
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.home_ilsang_logo),
+            tint = Color.Unspecified,
+            contentDescription = null
+        )
+        Spacer(Modifier.weight(1f))
+        Icon(
+            modifier = Modifier.size(36.dp),
+            painter = painterResource(id = R.drawable.default_user_profile),
+            tint = Color.Unspecified,
+            contentDescription = null
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun HomeTapScreenPreview() {
+    HomeTapScreen()
+}
