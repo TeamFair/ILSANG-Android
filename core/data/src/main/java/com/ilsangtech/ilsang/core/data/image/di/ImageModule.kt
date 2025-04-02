93ae5c08f93be6041bc2a7d2d111414268c7064a
kimseongwoo <tjddn929@naver.com>
2025-04-02 21:25:44 +0900

    [Feat] Hilt 의존성 주입 모듈 ImageModule 구현

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/di/ImageModule.kt
@@ -0,0 +1,28 @@
+package com.ilsangtech.ilsang.core.data.image.di
+
+import com.ilsangtech.ilsang.core.data.image.datasource.ImageDataSource
+import com.ilsangtech.ilsang.core.data.image.datasource.ImageDataSourceImpl
+import com.ilsangtech.ilsang.core.data.image.repository.ImageRepositoryImpl
+import com.ilsangtech.ilsang.core.domain.ImageRepository
+import com.ilsangtech.ilsang.core.network.api.ImageApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ImageModule {
+    @Provides
+    @Singleton
+    fun provideImageDataSource(imageApiService: ImageApiService): ImageDataSource {
+        return ImageDataSourceImpl(imageApiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageRepository(imageDataSource: ImageDataSource): ImageRepository {
+        return ImageRepositoryImpl(imageDataSource)
+    }
+}
