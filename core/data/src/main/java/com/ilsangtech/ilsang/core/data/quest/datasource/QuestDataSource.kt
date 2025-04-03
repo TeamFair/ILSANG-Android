a8af5e62f743b7ed9fff22e0db91f6e6b6ad0000
kimseongwoo <tjddn929@naver.com>
2025-04-03 23:15:24 +0900

    [Feat] 완료되지 않은 퀘스트 API 응답 데이터 처리를 담당하는 QuestDataSource 생성,QuestDataSourceImpl 구현

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/quest/datasource/QuestDataSource.kt
@@ -0,0 +1,13 @@
+package com.ilsangtech.ilsang.core.data.quest.datasource
+
+import com.ilsangtech.ilsang.core.network.model.quest.UncompletedTotalQuestResponse
+
+interface QuestDataSource {
+    suspend fun getUncompletedTotalQuest(
+        authorization: String,
+        popularYn: Boolean? = null,
+        page: Int = 0,
+        size: Int = 8,
+        sort: List<String> = emptyList()
+    ): UncompletedTotalQuestResponse
+}
---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/quest/datasource/QuestDataSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.ilsangtech.ilsang.core.data.quest.datasource
+
+import com.ilsangtech.ilsang.core.network.api.QuestApiService
+import com.ilsangtech.ilsang.core.network.model.quest.PageableRequest
+import com.ilsangtech.ilsang.core.network.model.quest.UncompletedTotalQuestResponse
+import javax.inject.Inject
+
+class QuestDataSourceImpl @Inject constructor(
+    private val questApiService: QuestApiService
+) : QuestDataSource {
+    override suspend fun getUncompletedTotalQuest(
+        authorization: String,
+        popularYn: Boolean?,
+        page: Int,
+        size: Int,
+        sort: List<String>
+    ): UncompletedTotalQuestResponse {
+        return questApiService.getUncompletedTotalQuest(
+            authorization = authorization,
+            popularYn = popularYn,
+            pageable = PageableRequest(
+                page = page,
+                size = size,
+                sort = sort
+            )
+        )
+    }
+}
