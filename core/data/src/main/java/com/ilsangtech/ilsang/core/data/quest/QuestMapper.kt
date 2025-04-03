8f79b017ea646cd594c596fd2b0abe98c6dfe00d
kimseongwoo <tjddn929@naver.com>
2025-04-03 23:10:37 +0900

    [Feat] Quest Model 클래스, Model 클래스 변환 함수 구현

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/quest/QuestMapper.kt
@@ -0,0 +1,28 @@
+package com.ilsangtech.ilsang.core.data.quest
+
+import com.ilsangtech.ilsang.core.model.Quest
+import com.ilsangtech.ilsang.core.network.model.quest.QuestNetworkModel
+import com.ilsangtech.ilsang.core.network.model.quest.RewardNetworkModel
+
+fun QuestNetworkModel.toQuest(): Quest {
+    return Quest(
+        createDate = createDate,
+        creatorRole = creatorRole,
+        expireDate = expireDate,
+        favoriteYn = favoriteYn,
+        imageId = imageId,
+        mainImageId = mainImageId,
+        marketId = marketId,
+        missionId = missionId,
+        missionTitle = missionTitle,
+        missionType = missionType,
+        popularYn = popularYn,
+        questId = questId,
+        rewardNetworkModelList = rewardNetworkModelList.map(RewardNetworkModel::toReward),
+        score = score,
+        status = status,
+        target = target,
+        type = type,
+        writer = writer
+    )
+}
