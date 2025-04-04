7b2c9cca4bde18a33b561a32d5c0d3d44660a8ce
kimseongwoo <tjddn929@naver.com>
2025-04-04 23:54:49 +0900

    [Fix] content, discountRate, target 프로퍼티 nullable로 변경

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/quest/RewardNetworkModel.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/quest/RewardNetworkModel.kt
@@ -4,12 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RewardNetworkModel(
-    val content: String,
-    val discountRate: Int,
+    val content: String?,
+    val discountRate: Int?,
     val quantity: Int,
     val questId: String,
     val rewardId: String,
-    val target: String,
+    val target: String?,
     val title: String,
     val type: String
 )
