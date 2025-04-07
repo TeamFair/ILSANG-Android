38fbfc59aa93fe881359ea47304329cbad5ca1a9
kimseongwoo <tjddn929@naver.com>
2025-04-07 20:24:32 +0900

    [Fix] DI import문 수정

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/di/RankDataModule.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/di/RankDataModule.kt
@@ -9,7 +9,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Singleton
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
