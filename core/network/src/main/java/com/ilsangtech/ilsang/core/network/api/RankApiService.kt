4ab7c8a86e4b3678a7459bdab49787af109a6d52
kimseongwoo <tjddn929@naver.com>
2025-04-07 20:21:03 +0900

    [Fix] 상위 랭크 유저 API 경로 수정

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/RankApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/RankApiService.kt
@@ -4,6 +4,6 @@ import com.ilsangtech.ilsang.core.network.model.rank.TopUsersResponse
 import retrofit2.http.GET
 
 interface RankApiService {
-    @GET("v1/rank/top-users")
+    @GET("open/v1/rank/top-users")
     suspend fun getTopRankUsers(): TopUsersResponse
 }
