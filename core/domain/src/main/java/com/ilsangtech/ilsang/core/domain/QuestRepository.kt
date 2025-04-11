21dc927ce5821785a30d4893b7c37408fea24022
swkim1 <swkim1@hds-secu.com>
2025-04-11 10:23:59 +0900

    [Feat] 미완료 기본/반복/이벤트 퀘스트 목록 조회 함수 Repository 인터페이스에 추가

--- core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/QuestRepository.kt
+++ core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/QuestRepository.kt
@@ -8,4 +8,14 @@ interface QuestRepository {
     suspend fun getRecommendedQuests(): List<Quest>
 
     suspend fun getLargeRewardQuests(): Map<String, List<Quest>>
+
+    // 미완료한 기본 퀘스트 목록 조회
+    suspend fun getUncompletedNormalQuests(): List<Quest>
+
+    // 미완료한 반복 퀘스트 목록 조회
+    suspend fun getUncompletedRepeatQuests(status: String): List<Quest>
+
+    // 미완료한 이벤트 퀘스트 목록 조회
+    suspend fun getUncompletedEventQuests(): List<Quest>
+
 }
