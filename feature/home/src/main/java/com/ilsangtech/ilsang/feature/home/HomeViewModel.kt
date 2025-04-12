fa1e6cddf95fddf7aa891ea53733d57e310a7492
kimseongwoo <tjddn929@naver.com>
2025-04-13 08:29:18 +0900

    [Feature] 홈 탭 및 퀘스트 탭 UI 상태 관리 로직 구현

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeViewModel.kt
@@ -6,11 +6,18 @@ import com.ilsangtech.ilsang.core.domain.BannerRepository
 import com.ilsangtech.ilsang.core.domain.QuestRepository
 import com.ilsangtech.ilsang.core.domain.RankRepository
 import com.ilsangtech.ilsang.core.domain.UserRepository
+import com.ilsangtech.ilsang.core.model.QuestType
+import com.ilsangtech.ilsang.core.model.RepeatQuestPeriod
+import com.ilsangtech.ilsang.core.model.RewardType
 import com.ilsangtech.ilsang.feature.home.home.HomeTapSuccessData
 import com.ilsangtech.ilsang.feature.home.home.HomeTapUiState
+import com.ilsangtech.ilsang.feature.home.quest.QuestTabUiData
+import com.ilsangtech.ilsang.feature.home.quest.QuestTabUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
@@ -55,4 +62,82 @@ class HomeViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = HomeTapUiState.Loading
     )
+
+    private var _selectedQuestType = MutableStateFlow(QuestType.NORMAL)
+    val selectedQuestType = _selectedQuestType.asStateFlow()
+
+    private var _selectedRewardType = MutableStateFlow(RewardType.STRENGTH)
+    val selectedRewardType = _selectedRewardType.asStateFlow()
+
+    private var _selectedRepeatPeriod = MutableStateFlow<RepeatQuestPeriod>(RepeatQuestPeriod.DAILY)
+    val selectedRepeatPeriod = _selectedRepeatPeriod.asStateFlow()
+
+    private var _selectedSortType = MutableStateFlow("포인트 높은 순")
+    val selectedSortType = _selectedSortType.asStateFlow()
+
+    val questTabUiState: StateFlow<QuestTabUiState> = combine(
+        selectedQuestType, selectedRewardType, selectedRepeatPeriod, selectedSortType
+    ) { questType, rewardType, repeatPeriod, sortType ->
+        try {
+            val questList = when (questType) {
+                QuestType.NORMAL -> questRepository.getUncompletedNormalQuests()
+                QuestType.REPEAT -> questRepository.getUncompletedRepeatQuests(
+                    when (repeatPeriod) {
+                        RepeatQuestPeriod.DAILY -> "DAILY"
+                        RepeatQuestPeriod.WEEKLY -> "WEEKLY"
+                        RepeatQuestPeriod.MONTHLY -> "MONTHLY"
+                    }
+                )
+
+                QuestType.EVENT -> questRepository.getUncompletedEventQuests()
+                else -> emptyList()
+            }.filter { quest ->
+                quest.rewardList.find { it.content == rewardType.name } != null
+            }.sortedBy {
+                when (sortType) {
+                    "포인트 높은 순" -> {
+                        it.rewardList.sumOf { reward ->
+                            -reward.quantity
+                        }
+                    }
+
+                    "포인트 낮은 순" -> {
+                        it.rewardList.sumOf { reward ->
+                            reward.quantity
+                        }
+                    }
+
+                    else -> {
+                        -it.score
+                    }
+                }
+            }
+
+            QuestTabUiState.Success(
+                data = QuestTabUiData(questList)
+            )
+        } catch (e: Exception) {
+            QuestTabUiState.Error(e)
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = QuestTabUiState.Loading
+    )
+
+    fun selectQuestType(questType: QuestType) {
+        _selectedQuestType.value = questType
+    }
+
+    fun selectRewardType(rewardType: RewardType) {
+        _selectedRewardType.value = rewardType
+    }
+
+    fun selectRepeatPeriod(repeatQuestPeriod: RepeatQuestPeriod) {
+        _selectedRepeatPeriod.value = repeatQuestPeriod
+    }
+
+    fun selectSortType(sortType: String) {
+        _selectedSortType.value = sortType
+    }
 }
