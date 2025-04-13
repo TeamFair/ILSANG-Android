192af4082f807dc77ef4b6096aade4d1ae2b890e
kimseongwoo <tjddn929@naver.com>
2025-04-13 10:29:07 +0900

    [Feat] 각 퀘스트 카드 양 옆 패딩 적용 및 간격 조정

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/quest/QuestTabScreen.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/quest/QuestTabScreen.kt
@@ -1,5 +1,6 @@
 package com.ilsangtech.ilsang.feature.home.quest
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -79,7 +81,10 @@ fun QuestTabScreen(
                     onSelectSortType = onSelectSortType
                 )
                 LazyColumn(
-                    modifier = Modifier.offset(y = 64.dp)
+                    modifier = Modifier
+                        .offset(y = 64.dp)
+                        .padding(horizontal = 20.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
                     if (questTabUiState is QuestTabUiState.Success) {
                         items(questTabUiState.data.questList) { quest ->
