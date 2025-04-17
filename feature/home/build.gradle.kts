af60eaa8a269604e2885b96eaa9442429d75e886
kimseongwoo <tjddn929@naver.com>
2025-04-17 23:47:43 +0900

    [Chore] :feature:home 모듈에 Paging 라이브러리 의존성 추가

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:util"))
 
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
     implementation(libs.hilt.navigation.compose)
