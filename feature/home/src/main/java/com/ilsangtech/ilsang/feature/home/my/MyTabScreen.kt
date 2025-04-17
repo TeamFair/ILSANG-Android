68ce30267a3a698e63d3f2e1c2a8baa14fa26bcc
kimseongwoo <tjddn929@naver.com>
2025-04-17 23:59:00 +0900

    [Feat] MyTabScreen 생성 및 UI 컴포넌트 적용

---
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/MyTabScreen.kt
@@ -0,0 +1,95 @@
+package com.ilsangtech.ilsang.feature.home.my
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.ilsangtech.ilsang.core.model.Challenge
+import com.ilsangtech.ilsang.core.model.UserInfo
+import com.ilsangtech.ilsang.feature.home.HomeViewModel
+import com.ilsangtech.ilsang.feature.home.my.component.MyChallengeContent
+import com.ilsangtech.ilsang.feature.home.my.component.MyTabHeader
+import com.ilsangtech.ilsang.feature.home.my.component.MyTabMenuContent
+import com.ilsangtech.ilsang.feature.home.my.component.UserProfileCard
+import kotlinx.coroutines.flow.asFlow
+
+@Composable
+fun MyTabScreen(homeViewModel: HomeViewModel) {
+    val userInfo by homeViewModel.userInfo.collectAsStateWithLifecycle()
+    val challengePager = homeViewModel.challengePager.collectAsLazyPagingItems()
+
+    MyTabScreen(
+        userInfo = userInfo,
+        challengePager = challengePager
+    )
+}
+
+@Composable
+fun MyTabScreen(
+    userInfo: UserInfo?,
+    challengePager: LazyPagingItems<Challenge>
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var selectedMenu by remember { mutableStateOf(MyTabMenu.CHALLENGE) }
+        Column(
+            modifier = Modifier
+                .statusBarsPadding()
+                .navigationBarsPadding()
+                .padding(horizontal = 16.dp)
+        ) {
+            MyTabHeader()
+            Spacer(Modifier.height(5.dp))
+            UserProfileCard(userInfo!!)
+            Spacer(Modifier.height(16.dp))
+            MyTabMenuContent(selectedMenu = selectedMenu) {
+                selectedMenu = it
+            }
+            when (selectedMenu) {
+                MyTabMenu.CHALLENGE -> {
+                    MyChallengeContent(challengePager)
+                }
+
+                MyTabMenu.ACTIVITY -> {}
+                MyTabMenu.INFO -> {}
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyTabScreenPreview() {
+    MyTabScreen(
+        userInfo = UserInfo(
+            accessToken = "",
+            authorization = "",
+            nickname = "김일상1234",
+            email = "",
+            profileImage = null,
+            completeChallengeCount = 0,
+            couponCount = 0,
+            xpPoint = 0,
+            status = ""
+        ),
+        challengePager = emptyList<PagingData<Challenge>>().asFlow().collectAsLazyPagingItems()
+    )
+}
