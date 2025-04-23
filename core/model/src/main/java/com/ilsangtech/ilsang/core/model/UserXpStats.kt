fcc02c6fb1bab8916ce54e7d74c2f1db019c4f5c
kimseongwoo <tjddn929@naver.com>
2025-04-23 20:00:51 +0900

    [Feat] 유저 스텟 도메인 모델 클래스 구현

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/UserXpStats.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.core.model
+
+data class UserXpStats(
+    val charmStat: Int = 0,
+    val funStat: Int = 0,
+    val intellectStat: Int = 0,
+    val sociabilityStat: Int = 0,
+    val strengthStat: Int = 0
+)
