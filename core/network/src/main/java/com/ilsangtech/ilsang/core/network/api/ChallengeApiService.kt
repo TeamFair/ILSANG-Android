e20b787e086908843295a1a460f13f9ebae23d6a
kimseongwoo <tjddn929@naver.com>
2025-04-26 23:06:47 +0900

    [Feat] 챌린지 등록 API 함수 추가

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/ChallengeApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/ChallengeApiService.kt
@@ -1,8 +1,12 @@
 package com.ilsangtech.ilsang.core.network.api
 
+import com.ilsangtech.ilsang.core.network.model.challenge.ChallengeSubmitRequest
+import com.ilsangtech.ilsang.core.network.model.challenge.ChallengeSubmitResponse
 import com.ilsangtech.ilsang.core.network.model.challenge.ChallengesResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ChallengeApiService {
@@ -16,4 +20,10 @@ interface ChallengeApiService {
         @Query("page") page: Int,
         @Query("size") size: Int
     ): ChallengesResponse
+
+    @POST("customer/challenge")
+    suspend fun submitChallenge(
+        @Header("authorization") authorization: String,
+        @Body challengeSubmitRequest: ChallengeSubmitRequest
+    ): ChallengeSubmitResponse
 }
