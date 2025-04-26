01dbf211283e8cbbdc6c85c21dbdc953515d52ca
kimseongwoo <tjddn929@naver.com>
2025-04-26 23:39:49 +0900

    [Feat] ImageDatasource 내 이미지 업로드 함수 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/datasource/ImageDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/datasource/ImageDataSource.kt
@@ -1,7 +1,10 @@
 package com.ilsangtech.ilsang.core.data.image.datasource
 
 import com.ilsangtech.ilsang.core.network.model.image.ImageResponse
+import com.ilsangtech.ilsang.core.network.model.image.ImageUploadResponse
 
 interface ImageDataSource {
     suspend fun getImage(imageId: String): ImageResponse
+
+    suspend fun uploadImage(imageBytes: ByteArray): ImageUploadResponse
 }
--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/datasource/ImageDataSourceImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/datasource/ImageDataSourceImpl.kt
@@ -2,6 +2,12 @@ package com.ilsangtech.ilsang.core.data.image.datasource
 
 import com.ilsangtech.ilsang.core.network.api.ImageApiService
 import com.ilsangtech.ilsang.core.network.model.image.ImageResponse
+import com.ilsangtech.ilsang.core.network.model.image.ImageUploadResponse
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
 import javax.inject.Inject
 
 class ImageDataSourceImpl @Inject constructor(
@@ -9,5 +15,14 @@ class ImageDataSourceImpl @Inject constructor(
 ) : ImageDataSource {
     override suspend fun getImage(imageId: String): ImageResponse {
         return imageApiService.getImage(imageId)
+    }
+
+    override suspend fun uploadImage(imageBytes: ByteArray): ImageUploadResponse {
+        val requestBody = imageBytes.toRequestBody("image/*".toMediaType())
+        return imageApiService.uploadImage(MultipartBody.Part.createFormData(
+            name = "image",
+            filename = "image.jpg",
+            body = requestBody
+        ))
     }
 }
