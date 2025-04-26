2ef2c111ec4c12998db7ed0c32ec53b79d4f8fec
kimseongwoo <tjddn929@naver.com>
2025-04-26 11:29:47 +0900

    [Feat] 닉네임 수정 화면 이동 애니메이션을 슬라이드로 처리

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/navigation/MyTapNavigation.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/navigation/MyTapNavigation.kt
@@ -1,5 +1,7 @@
 package com.ilsangtech.ilsang.feature.home.my.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
@@ -24,7 +26,21 @@ fun NavGraphBuilder.myTabNavigation(
                 navigateToNicknameEdit = navigateToNicknameEdit
             )
         }
-        composable("${HomeTap.My.name}/Edit") {
+        composable(
+            route = "${HomeTap.My.name}/Edit",
+            enterTransition = {
+                slideIntoContainer(
+                    animationSpec = tween(300),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    animationSpec = tween(300),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            }
+        ) {
             NicknameEditScreen(
                 homeViewModel = homeViewModel,
                 navigateToMyTabMain = navigateToMyTabMain
