330b41d9816430cb9f9de43de68536542da2202d
kimseongwoo <tjddn929@naver.com>
2025-04-27 10:38:19 +0900

    [Refactor] 챌린지 등록 요청 DTO Serializable 어노테이션 추가

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/challenge/ChallengeSubmitRequest.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/challenge/ChallengeSubmitRequest.kt
@@ -1,5 +1,8 @@
 package com.ilsangtech.ilsang.core.network.model.challenge
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ChallengeSubmitRequest(
     val questId: String,
     val receiptImageId: String
