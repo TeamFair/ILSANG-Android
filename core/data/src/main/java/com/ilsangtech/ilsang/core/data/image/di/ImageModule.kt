3fc6624d476fdaa6c647407a3062ad369bf8072e
kimseongwoo <tjddn929@naver.com>
2025-04-27 10:44:09 +0900

    [Chore] ImageRepository 의존성 주입 함수 수정

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/di/ImageModule.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/image/di/ImageModule.kt
@@ -4,6 +4,7 @@ import com.ilsangtech.ilsang.core.data.image.datasource.ImageDataSource
 import com.ilsangtech.ilsang.core.data.image.datasource.ImageDataSourceImpl
 import com.ilsangtech.ilsang.core.data.image.repository.ImageRepositoryImpl
 import com.ilsangtech.ilsang.core.domain.ImageRepository
+import com.ilsangtech.ilsang.core.domain.UserRepository
 import com.ilsangtech.ilsang.core.network.api.ImageApiService
 import dagger.Module
 import dagger.Provides
@@ -22,7 +23,13 @@ object ImageModule {
 
     @Provides
     @Singleton
-    fun provideImageRepository(imageDataSource: ImageDataSource): ImageRepository {
-        return ImageRepositoryImpl(imageDataSource)
+    fun provideImageRepository(
+        userRepository: UserRepository,
+        imageDataSource: ImageDataSource
+    ): ImageRepository {
+        return ImageRepositoryImpl(
+            userRepository = userRepository,
+            imageDataSource = imageDataSource
+        )
     }
 }
