918ab393978a0ea8a81f1910a27914e491cfa54f
kimseongwoo <tjddn929@naver.com>
2025-05-01 15:01:45 +0900

    [Feat] ChallengeDataSource 내 랜덤 챌린지 조회 함수 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/challenge/datasource/ChallengeDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/challenge/datasource/ChallengeDataSource.kt
@@ -2,6 +2,7 @@ package com.ilsangtech.ilsang.core.data.challenge.datasource
 
 import com.ilsangtech.ilsang.core.network.model.challenge.ChallengeSubmitResponse
 import com.ilsangtech.ilsang.core.network.model.challenge.ChallengesResponse
+import com.ilsangtech.ilsang.core.network.model.challenge.RandomChallengeResponse
 
 interface ChallengeDataSource {
     suspend fun getChallenges(
@@ -19,4 +20,10 @@ interface ChallengeDataSource {
         questId: String,
         imageId: String
     ): ChallengeSubmitResponse
+
+    suspend fun getRandomChallenges(
+        authorization: String,
+        page: Int,
+        size: Int
+    ): RandomChallengeResponse
 }
