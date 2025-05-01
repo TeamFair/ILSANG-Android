8cc0418d8f57c92f5577826b3295e0077b4ca61e
kimseongwoo <tjddn929@naver.com>
2025-05-01 15:24:57 +0900

    [Feat] 날짜 포멧 변환 오브젝트 클래스 구현

---
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/util/DateConverter.kt
@@ -0,0 +1,34 @@
+package com.ilsangtech.ilsang.feature.home.util
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+object DateConverter {
+    fun timeAgoSinceDate(dateString: String, withYear: Boolean = true): String {
+        val datePart = dateString.split(".").firstOrNull() ?: return ""
+
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+        inputFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+        val date: Date = try {
+            inputFormat.parse(datePart) ?: return ""
+        } catch (e: Exception) {
+            return ""
+        }
+
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+
+        calendar.time = date
+        val dateYear = calendar.get(Calendar.YEAR)
+
+        val outputFormat = when {
+            withYear && currentYear != dateYear -> SimpleDateFormat("yyyy년 M월 d일", Locale.getDefault())
+            else -> SimpleDateFormat("M월 d일", Locale.getDefault())
+        }
+        return outputFormat.format(date)
+    }
+}
