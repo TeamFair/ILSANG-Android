efd8d09365a46d375ca384b9c1e479a37d347722
kimseongwoo <tjddn929@naver.com>
2025-06-03 17:23:07 +0900

    [Feat] 사용하지 않은 코드 제거 및 프로필 이미지 UI 원형으로 표시하도록 수정

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/component/UserProfileContent.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/component/UserProfileContent.kt
@@ -15,16 +15,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -35,9 +33,8 @@ import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.ilsangtech.ilsang.core.model.UserInfo
 import com.ilsangtech.ilsang.designsystem.R.font.pretendard_bold
-import com.ilsangtech.ilsang.designsystem.R.font.pretendard_regular
 import com.ilsangtech.ilsang.designsystem.theme.gray100
-import com.ilsangtech.ilsang.designsystem.theme.gray200
+import com.ilsangtech.ilsang.designsystem.theme.gray100
 import com.ilsangtech.ilsang.designsystem.theme.gray500
 import com.ilsangtech.ilsang.designsystem.theme.primary
 import com.ilsangtech.ilsang.feature.home.BuildConfig
@@ -56,82 +53,6 @@ fun UserProfileContent(
 }
 
 @Composable
-fun DefaultProfileCardContent(userInfo: UserInfo) {
-    Row(
-        modifier = Modifier.padding(18.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column(modifier = Modifier.padding(top = 13.dp)) {
-            AsyncImage(
-                modifier = Modifier.size(57.dp),
-                model = userInfo.profileImage?.let { BuildConfig.IMAGE_URL + it },
-                error = painterResource(R.drawable.default_user_profile),
-                placeholder = painterResource(R.drawable.default_user_profile),
-                contentDescription = "프로필 이미지"
-            )
-            Box(
-                modifier = Modifier
-                    .width(57.dp)
-                    .offset(y = (-13).dp)
-                    .clip(RoundedCornerShape(20.dp))
-                    .background(Color.White)
-                    .border(
-                        width = 1.dp,
-                        color = primary,
-                        shape = RoundedCornerShape(20.dp)
-                    )
-                    .padding(
-                        vertical = 4.dp,
-                        horizontal = 6.dp
-                    ),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Lv. " + XpLevelCalculator.getCurrentLevel(userInfo.xpPoint),
-                    style = userProfileCardLevelTextStyle
-                )
-            }
-        }
-        Spacer(Modifier.width(19.dp))
-        Column {
-            NicknameWithUnderline(userInfo.nickname!!)
-            Spacer(Modifier.height(6.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                LinearProgressIndicator(
-                    modifier = Modifier
-                        .height(8.dp)
-                        .weight(1f),
-                    color = primary,
-                    trackColor = Color(0xFFEDEBEB),
-                    progress = {
-                        0.3f
-                    },
-                    gapSize = (-5).dp,
-                    drawStopIndicator = {}
-                )
-                Spacer(Modifier.width(7.dp))
-                Text(
-                    text = "${XpLevelCalculator.getXpPointInCurrentLevel(userInfo.xpPoint)}XP",
-                    style = userProfileCardXpTextStyle,
-                    color = primary
-                )
-            }
-            Spacer(Modifier.height(8.dp))
-            Text(
-                text = "다음 레벨까지 "
-                        + XpLevelCalculator.getRequiredXpPointForNextLevel(userInfo.xpPoint)
-                        + "XP 남았어요!",
-                style = userProfileCardLevelDescriptionTextStyle
-            )
-        }
-    }
-}
-
-
-@Composable
 fun MyInfoProfileContent(
     userInfo: UserInfo,
     navigateToNicknameEdit: () -> Unit
@@ -148,8 +69,11 @@ fun MyInfoProfileContent(
     ) {
         Box(modifier = Modifier.padding(bottom = 4.dp)) {
             AsyncImage(
-                modifier = Modifier.size(57.dp),
+                modifier = Modifier
+                    .size(57.dp)
+                    .clip(CircleShape),
                 model = userInfo.profileImage?.let { BuildConfig.IMAGE_URL + it },
+                contentScale = ContentScale.Crop,
                 placeholder = painterResource(R.drawable.default_user_profile),
                 error = painterResource(R.drawable.default_user_profile),
                 contentDescription = "프로필 이미지"
@@ -178,44 +102,6 @@ fun MyInfoProfileContent(
             )
             Spacer(Modifier.height(4.dp))
             UserLevelBadge(xpPoint = userInfo.xpPoint)
-        }
-    }
-}
-
-@Composable
-fun NicknameWithUnderline(nickname: String) {
-    SubcomposeLayout { constraints ->
-
-        val nicknameMeasurables = subcompose("nickname") {
-            Text(
-                text = nickname,
-                style = profileNicknameTextStyle
-            )
-        }
-        val nicknamePlaceables = nicknameMeasurables.map { it.measure(constraints) }
-        val nicknameWidth = nicknamePlaceables.maxOf { it.width }
-        val nicknameHeight = nicknamePlaceables.maxOf { it.height }
-
-        val dividerMeasurables = subcompose("divider") {
-            HorizontalDivider(
-                modifier = Modifier.width(nicknameWidth.toDp()),
-                color = gray200
-            )
-        }
-        val dividerPlaceables = dividerMeasurables.map { it.measure(constraints) }
-        val dividerHeight = dividerPlaceables.maxOf { it.height }
-
-        val totalHeight = nicknameHeight + dividerHeight
-
-        layout(nicknameWidth, totalHeight) {
-            var y = 0
-            nicknamePlaceables.forEach {
-                it.placeRelative(0, y)
-                y += it.height
-            }
-            dividerPlaceables.forEach {
-                it.placeRelative(0, y + 1.dp.roundToPx())
-            }
         }
     }
 }
@@ -259,20 +145,6 @@ private val profileNicknameTextStyle = TextStyle(
     fontFamily = FontFamily(Font(pretendard_bold)),
     fontSize = 16.sp,
     lineHeight = 18.sp,
-    color = gray500
-)
-
-private val userProfileCardXpTextStyle = TextStyle(
-    fontFamily = FontFamily(Font(pretendard_bold)),
-    fontSize = 13.sp,
-    lineHeight = 12.sp,
-    color = primary
-)
-
-private val userProfileCardLevelDescriptionTextStyle = TextStyle(
-    fontFamily = FontFamily(Font(pretendard_regular)),
-    fontSize = 13.sp,
-    lineHeight = 12.sp,
     color = gray500
 )
 
