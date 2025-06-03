dd30e57f35738dda196aed8db5ba90a14c15d888
kimseongwoo <tjddn929@naver.com>
2025-06-03 16:42:33 +0900

    [Chore] 종속 항목 변경에 따른 의존성 주입 모듈 함수 변경

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/di/UserDataModule.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/di/UserDataModule.kt
@@ -4,6 +4,7 @@ import com.ilsangtech.ilsang.core.data.user.datasource.UserDataSource
 import com.ilsangtech.ilsang.core.data.user.datasource.UserDataSourceImpl
 import com.ilsangtech.ilsang.core.data.user.repository.UserRepositoryImpl
 import com.ilsangtech.ilsang.core.datastore.UserDataStore
+import com.ilsangtech.ilsang.core.domain.ImageRepository
 import com.ilsangtech.ilsang.core.domain.UserRepository
 import com.ilsangtech.ilsang.core.network.api.AuthApiService
 import com.ilsangtech.ilsang.core.network.api.UserApiService
@@ -32,8 +33,13 @@ object UserDataModule {
     @Singleton
     fun provideUserRepository(
         userDataSource: UserDataSource,
-        userDataStore: UserDataStore
+        userDataStore: UserDataStore,
+        imageRepository: ImageRepository
     ): UserRepository {
-        return UserRepositoryImpl(userDataSource, userDataStore)
+        return UserRepositoryImpl(
+            userDataSource = userDataSource,
+            userDataStore = userDataStore,
+            imageRepository = imageRepository
+        )
     }
 }
