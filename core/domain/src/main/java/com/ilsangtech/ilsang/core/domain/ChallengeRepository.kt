4c6cd2de01218b85ea2c805b592859dfbda822e3
kimseongwoo <tjddn929@naver.com>
2025-06-12 22:29:08 +0900

    [Feat] 별도로 ChallengeNetworkModel에 대한 페이징 소스 구현 및 Repository에 적용

--- core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/ChallengeRepository.kt
+++ core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/ChallengeRepository.kt
@@ -1,23 +1,20 @@
 package com.ilsangtech.ilsang.core.domain
 
+import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import com.ilsangtech.ilsang.core.model.Challenge
 import com.ilsangtech.ilsang.core.model.RandomChallenge
+import kotlinx.coroutines.flow.Flow
 
 interface ChallengeRepository {
     val randomChallengePagingSource: PagingSource<Int, RandomChallenge>
-
-    suspend fun getChallengesWithTotal(
-        page: Int,
-        size: Int
-    ): Pair<List<Challenge>, Int>
 
     suspend fun getRandomChallengesWithTotal(
         page: Int,
         size: Int
     ): Pair<List<RandomChallenge>, Int>
 
-    fun getChallengePaging(): PagingSource<Int, Challenge>
+    fun getChallengePaging(userId: String? = null): Flow<PagingData<Challenge>>
 
     suspend fun submitChallenge(
         imageBytes: ByteArray,
