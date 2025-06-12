724dd8ea746b929a1d1f97aba1663889872087c6
kimseongwoo <tjddn929@naver.com>
2025-06-12 22:32:39 +0900

    [Feat] ProfileBaseRoute, ChallengeRoute 직렬확 객체 생성

--- feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/navigation/ProfileNavigation.kt
+++ feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/navigation/ProfileNavigation.kt
@@ -5,8 +5,18 @@ import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
 
 @Serializable
+data object ProfileBaseRoute
+
+@Serializable
 data class ProfileRoute(val userId: String)
 
 fun NavGraphBuilder.profileRoute(popBackStack: () -> Unit) {
     composable<ProfileRoute> {}
+@Serializable
+data class ChallengeRoute(
+    val receiptImageId: String?,
+    val questImageId: String?,
+    val likeCount: Int,
+    val title: String
+)
 }
