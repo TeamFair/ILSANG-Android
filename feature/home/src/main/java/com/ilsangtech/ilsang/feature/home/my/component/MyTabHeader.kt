5600c734f68d1d8c14abf92c1e86ffa0b3cbf3b7
kimseongwoo <tjddn929@naver.com>
2025-06-16 22:02:03 +0900

    [Feat] 마이 탭 헤더에 패딩 적용 및 설정 아이콘 클릭 이벤트 적용

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/component/MyTabHeader.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/component/MyTabHeader.kt
@@ -1,8 +1,10 @@
 package com.ilsangtech.ilsang.feature.home.my.component
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.ilsangtech.ilsang.designsystem.R.font.pretendard_bold
@@ -20,9 +23,13 @@ import com.ilsangtech.ilsang.designsystem.theme.gray500
 import com.ilsangtech.ilsang.feature.home.R
 
 @Composable
-fun MyTabHeader() {
+fun MyTabHeader(
+    onSettingIconClick: () -> Unit
+) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -31,6 +38,11 @@ fun MyTabHeader() {
         )
         Spacer(Modifier.weight(1f))
         Icon(
+            modifier = Modifier.clickable(
+                interactionSource = null,
+                indication = null,
+                onClick = onSettingIconClick
+            ),
             painter = painterResource(R.drawable.setting),
             contentDescription = "설정",
             tint = gray500
@@ -48,5 +60,5 @@ private val myTabHeaderTitleTextStyle = TextStyle(
 @Preview(showBackground = true, backgroundColor = 0xF6F6F6)
 @Composable
 fun MyTabHeaderPreview() {
-    MyTabHeader()
+    MyTabHeader {}
 }
