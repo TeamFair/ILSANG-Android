707f4a7aaf704235ad147adb973fa14ab4dce030
kimseongwoo <tjddn929@naver.com>
2025-06-17 13:02:47 +0900

    [Feat] 회원탈퇴 전체 화면 UI 구현

---
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/my/WithdrawalScreen.kt
@@ -0,0 +1,109 @@
+package com.ilsangtech.ilsang.feature.home.my
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ilsangtech.ilsang.designsystem.theme.gray500
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
+import com.ilsangtech.ilsang.feature.home.R
+import com.ilsangtech.ilsang.feature.home.my.component.WithdrawalButton
+import com.ilsangtech.ilsang.feature.home.my.component.WithdrawalContent
+import com.ilsangtech.ilsang.feature.home.my.component.WithdrawalDialog
+
+@Composable
+fun WithdrawalScreen(
+    onWithdrawalButtonClick: () -> Unit,
+    onBackButtonClick: () -> Unit
+) {
+    var showWithdrawalDialog by remember { mutableStateOf(false) }
+
+    if (showWithdrawalDialog) {
+        WithdrawalDialog(
+            onDismissRequest = { showWithdrawalDialog = false },
+            onWithdrawalButtonClick = onWithdrawalButtonClick
+        )
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Column {
+            WithdrawalScreenHeader(onBackButtonClick = onBackButtonClick)
+            WithdrawalContent(modifier = Modifier.weight(1f))
+            WithdrawalButton(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .padding(bottom = 8.dp),
+                onWithdrawalButtonClick = { showWithdrawalDialog = true }
+            )
+        }
+    }
+}
+
+@Composable
+private fun WithdrawalScreenHeader(
+    onBackButtonClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(vertical = 12.dp)
+    ) {
+        Icon(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(start = 16.dp)
+                .clickable(
+                    onClick = onBackButtonClick,
+                    indication = null,
+                    interactionSource = null
+                ),
+            painter = painterResource(id = R.drawable.back),
+            tint = Color.Unspecified,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "회원 탈퇴",
+            style = TextStyle(
+                fontFamily = pretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 17.sp
+            ),
+            color = gray500
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun WithdrawalScreenPreview() {
+    WithdrawalScreen(
+        onWithdrawalButtonClick = {},
+        onBackButtonClick = {}
+    )
+}
