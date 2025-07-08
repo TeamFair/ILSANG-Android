87dc8492c12ba051b3a3c0a98995d5127cc15815
kimseongwoo <tjddn929@naver.com>
2025-07-08 23:16:27 +0900

    [Fix] UserRepository 내 로그인 함수를 OAuth 로그인으로 적용, currentUser의 Authorization 없이 함수를 호출할 수 있도록 적용

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/repository/UserRepositoryImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/repository/UserRepositoryImpl.kt
@@ -9,7 +9,7 @@ import com.ilsangtech.ilsang.core.domain.UserRepository
 import com.ilsangtech.ilsang.core.model.MyInfo
 import com.ilsangtech.ilsang.core.model.UserInfo
 import com.ilsangtech.ilsang.core.model.UserXpStats
-import com.ilsangtech.ilsang.core.network.model.auth.LoginRequest
+import com.ilsangtech.ilsang.core.network.model.auth.OAuthLoginRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -22,35 +22,42 @@ class UserRepositoryImpl @Inject constructor(
 
     override val shouldShowOnBoarding: Flow<Boolean> = userDataStore.shouldShowOnBoarding
 
-    override suspend fun login(
-        email: String,
-        accessToken: String
-    ) {
-        val userResponse = userDataSource.login(
-            loginRequest = LoginRequest(
-                accessToken = accessToken,
-                channel = "GOOGLE",
-                email = email,
-                refreshToken = "",
-                userType = "CUSTOMER"
+    override suspend fun login(idToken: String) {
+        val loginResponse = userDataSource.login(
+            OAuthLoginRequest(
+                idToken = idToken,
+                pushToken = "",
+                deviceUuid = "",
+                provider = "GOOGLE",
+                osType = "AOS"
             )
         )
-        val userInfoResponse = userDataSource.getUserInfo(
-            authorization = userResponse.data.authorization,
-            userId = null
-        )
+        val (accessToken, refreshToken) = loginResponse.data
+        with(userDataStore) {
+            setAccessToken(accessToken)
+            refreshToken?.let { setRefreshToken(refreshToken) }
+        }
+        updateMyInfo()
+    }
 
+    override suspend fun updateMyInfo() {
+        val userInfoResponse = userDataSource.getUserInfo(userId = null)
         currentUser = MyInfo(
-            authorization = userResponse.data.authorization,
-            email = email,
-            accessToken = accessToken,
             completeChallengeCount = userInfoResponse.userInfoNetworkModel.completeChallengeCount,
             couponCount = userInfoResponse.userInfoNetworkModel.couponCount,
             nickname = userInfoResponse.userInfoNetworkModel.nickname,
             profileImage = userInfoResponse.userInfoNetworkModel.profileImage,
             xpPoint = userInfoResponse.userInfoNetworkModel.xpPoint,
-            status = userInfoResponse.userInfoNetworkModel.status,
+            status = userInfoResponse.userInfoNetworkModel.status
         )
+    }
+
+    override suspend fun getUserInfo(userId: String): Result<UserInfo> {
+        return runCatching {
+            val userInfoResponse = userDataSource.getUserInfo(userId = userId)
+            val userInfoNetworkModel = userInfoResponse.userInfoNetworkModel
+            userInfoNetworkModel.toUserInfo()
+        }
     }
 
     override suspend fun logout(): Result<Unit> {
@@ -69,75 +76,29 @@ class UserRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun updateMyInfo() {
-        currentUser?.authorization?.let {
-            val userInfoResponse = userDataSource.getUserInfo(
-                authorization = it,
-                userId = null
-            )
-            currentUser = currentUser?.copy(
-                completeChallengeCount = userInfoResponse.userInfoNetworkModel.completeChallengeCount,
-                couponCount = userInfoResponse.userInfoNetworkModel.couponCount,
-                nickname = userInfoResponse.userInfoNetworkModel.nickname,
-                profileImage = userInfoResponse.userInfoNetworkModel.profileImage,
-                xpPoint = userInfoResponse.userInfoNetworkModel.xpPoint,
-                status = userInfoResponse.userInfoNetworkModel.status
-            )
-        }
-    }
-
-    override suspend fun getUserInfo(userId: String): Result<UserInfo> {
-        return runCatching {
-            currentUser?.authorization?.let {
-                val userInfoResponse = userDataSource.getUserInfo(
-                    authorization = it,
-                    userId = userId
-                )
-                val userInfoNetworkModel = userInfoResponse.userInfoNetworkModel
-                userInfoNetworkModel.toUserInfo()
-            } ?: throw Exception("User is not logged in")
-        }
-    }
-
     override suspend fun getUserXpStats(customerId: String?): UserXpStats {
         return userDataSource.getUserXpStats(
-            authorization = currentUser?.authorization!!,
             customerId = customerId
         ).userXpStatsNetworkModel.toUserXpStats()
     }
 
     override suspend fun updateUserNickname(nickname: String) {
-        currentUser?.authorization?.let {
-            userDataSource.updateUserNickname(
-                authorization = it,
-                nickname = nickname
-            )
-        }
+        userDataSource.updateUserNickname(nickname = nickname)
     }
 
     override suspend fun updateUserImage(imageData: ByteArray): Result<Unit> {
         return runCatching {
-            currentUser?.authorization?.let {
-                val imageId = imageRepository.uploadImage(
-                    authorization = it,
-                    type = "USER_PROFILE_IMAGE",
-                    imageBytes = imageData
-                )
-                userDataSource.updateUserImage(
-                    authorization = it,
-                    imageId = imageId
-                )
-            } ?: throw Exception("User is not logged in")
+            val imageId = imageRepository.uploadImage(
+                type = "USER_PROFILE_IMAGE",
+                imageBytes = imageData
+            )
+            userDataSource.updateUserImage(imageId)
         }
     }
 
     override suspend fun deleteUserImage(): Result<Unit> {
         return runCatching {
-            currentUser?.authorization?.let {
-                userDataSource.deleteUserImage(
-                    authorization = it
-                )
-            } ?: throw Exception("User is not logged in")
+            userDataSource.deleteUserImage()
         }
     }
 
