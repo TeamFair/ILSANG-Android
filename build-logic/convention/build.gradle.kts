4a9bacea1f19ac357d9d3bb1fec8dafeb9074ff9
kimseongwoo <tjddn929@naver.com>
2025-07-19 16:28:17 +0900

    [Chore#54] build-logic 디렉토리 내 convention 모듈 생성

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.ilsangtech.ilsang.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradle.plugin)
+    compileOnly(libs.plugin.kotlin.serializationPlugin)
+}
