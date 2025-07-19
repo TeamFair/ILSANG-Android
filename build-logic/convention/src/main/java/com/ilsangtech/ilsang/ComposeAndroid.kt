bbaf8cdca824c0717eda132967477c093a07ef27
kimseongwoo <tjddn929@naver.com>
2025-07-19 16:32:07 +0900

    [Chore#54] Compose 종속성 구성 확장 함수 생성

---
+++ build-logic/convention/src/main/java/com/ilsangtech/ilsang/ComposeAndroid.kt
@@ -0,0 +1,33 @@
+package com.ilsangtech.ilsang
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+
+internal fun Project.configureComposeAndroid() {
+    with(plugins) {
+        apply("org.jetbrains.kotlin.plugin.compose")
+    }
+
+    androidExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        dependencies {
+            val bom = findLibrary("androidx-compose-bom")
+            add("implementation", platform(bom))
+            add("androidTestImplementation", platform(bom))
+
+            add("implementation", findLibrary("androidx-ui"))
+            add("implementation", findLibrary("androidx-ui-tooling-preview"))
+            add("debugImplementation", findLibrary("androidx-ui-tooling"))
+            add("androidTestImplementation", findLibrary("androidx-ui-test-manifest"))
+            add("androidTestImplementation", findLibrary("androidx-ui-test-junit4"))
+        }
+    }
+    extensions.getByType<ComposeCompilerGradlePluginExtension>().apply {
+        includeSourceInformation.set(true)
+    }
+}
