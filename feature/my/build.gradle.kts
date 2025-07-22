b77f6a0e167a058995c8b75e03cbfad9b96375fb
kimseongwoo <tjddn929@naver.com>
2025-07-22 21:42:12 +0900

    [Chore#54] my 모듈에 필요한 라이브러리, feature 플러그인 추가

--- feature/my/build.gradle.kts
+++ feature/my/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ilsang.android.feature)
 }
 
 android {
@@ -33,7 +34,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.graphics.shapes)
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
