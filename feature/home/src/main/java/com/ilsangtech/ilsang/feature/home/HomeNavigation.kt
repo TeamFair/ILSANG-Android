619c3da6d9daefbee16a79e6f5f2918eee0864af
kimseongwoo <tjddn929@naver.com>
2025-07-22 23:08:03 +0900

    [Feat#54] 마이 탭을 제외한 home navigation 생성 및 적용

---
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeNavigation.kt
@@ -0,0 +1,88 @@
+package com.ilsangtech.ilsang.feature.home
+
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.ilsangtech.ilsang.feature.home.approval.ApprovalScreen
+import com.ilsangtech.ilsang.feature.home.home.HomeTapScreen
+import com.ilsangtech.ilsang.feature.home.quest.QuestTabScreen
+import com.ilsangtech.ilsang.feature.home.ranking.RankingScreen
+
+fun NavGraphBuilder.homeNavigation(
+    navController: NavHostController,
+    navigateToProfile: (String) -> Unit
+) {
+    navigation(
+        route = "home",
+        startDestination = HomeTap.Home.name
+    ) {
+        composable(HomeTap.Home.name) { backStackEntry ->
+            val homeViewModel = hiltViewModel<HomeViewModel>(backStackEntry)
+            val userInfo by homeViewModel.myInfo.collectAsStateWithLifecycle()
+            HomeTapScreen(
+                userNickname = userInfo?.nickname,
+                homeViewModel = homeViewModel,
+                onApproveButtonClick = {
+                    homeViewModel.selectQuest(it)
+                },
+                navigateToQuestTab = {
+                    homeViewModel.selectSortType("포인트 높은 순")
+                    navController.navigate(HomeTap.Quest.name) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                navigateToMyTab = {
+                    navController.navigate(HomeTap.My.name) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                navigateToSubmit = {
+                    navController.navigate("Submit")
+                },
+                navigateToRankingTab = {
+                    navController.navigate(HomeTap.Ranking.name) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                navigateToProfile = navigateToProfile
+            )
+        }
+        composable(HomeTap.Quest.name) { backStackEntry ->
+            val homeViewModel = hiltViewModel<HomeViewModel>(backStackEntry)
+            QuestTabScreen(
+                homeViewModel = homeViewModel,
+                navigateToSubmit = {
+                    navController.navigate("Submit")
+                }
+            )
+        }
+        composable(HomeTap.Approval.name) {
+            ApprovalScreen(
+                navigateToProfile = navigateToProfile
+            )
+        }
+        composable(HomeTap.Ranking.name) { backStackEntry ->
+            val homeViewModel = hiltViewModel<HomeViewModel>(backStackEntry)
+            RankingScreen(
+                homeViewModel = homeViewModel,
+                navigateToProfile = navigateToProfile
+            )
+        }
+    }
+}
