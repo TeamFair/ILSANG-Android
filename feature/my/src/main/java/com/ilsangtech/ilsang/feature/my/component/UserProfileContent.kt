c52b6b41ec18985ddc71914d2e83b6fecb9be631
swkim1 <swkim1@hds-secu.com>
2025-07-23 17:57:29 +0900

    [Design#54] 마이탭 프로필 UI 수정

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/component/UserProfileContent.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/component/UserProfileContent.kt
@@ -3,12 +3,11 @@ package com.ilsangtech.ilsang.feature.my.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,55 +20,71 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.ilsangtech.ilsang.core.model.MyInfo
+import com.ilsangtech.ilsang.core.util.XpLevelCalculator
 import com.ilsangtech.ilsang.designsystem.R.font.pretendard_bold
 import com.ilsangtech.ilsang.designsystem.theme.gray100
 import com.ilsangtech.ilsang.designsystem.theme.gray500
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
 import com.ilsangtech.ilsang.designsystem.theme.primary
-import com.ilsangtech.ilsang.core.util.XpLevelCalculator
 import com.ilsangtech.ilsang.feature.my.BuildConfig
 import com.ilsangtech.ilsang.feature.my.R
 
 @Composable
-fun UserProfileContent(
+internal fun UserProfileContent(
+    modifier: Modifier = Modifier,
     myInfo: MyInfo,
     navigateToNicknameEdit: () -> Unit
 ) {
     MyInfoProfileContent(
+        modifier = modifier,
         myInfo = myInfo,
         navigateToNicknameEdit = navigateToNicknameEdit
     )
 }
 
 @Composable
-fun MyInfoProfileContent(
+private fun MyInfoProfileContent(
+    modifier: Modifier = Modifier,
     myInfo: MyInfo,
     navigateToNicknameEdit: () -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(
-                indication = null,
-                interactionSource = null,
-                onClick = navigateToNicknameEdit
-            ),
-        verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = modifier.clickable(
+            onClick = navigateToNicknameEdit,
+            indication = null,
+            interactionSource = null
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Box(modifier = Modifier.padding(bottom = 4.dp)) {
+        Box(
+            modifier = Modifier
+                .drawBehind {
+                    drawCircle(
+                        radius = ((115 - 9).dp.toPx() / 2),
+                        color = gray100,
+                        style = Stroke(width = 9.dp.toPx())
+                    )
+                },
+            contentAlignment = Alignment.Center
+        ) {
             AsyncImage(
                 modifier = Modifier
-                    .size(57.dp)
+                    .size(85.5.dp)
                     .clip(CircleShape),
                 model = myInfo.profileImage?.let { BuildConfig.IMAGE_URL + it },
                 contentScale = ContentScale.Crop,
@@ -77,48 +92,52 @@ fun MyInfoProfileContent(
                 error = painterResource(R.drawable.default_user_profile),
                 contentDescription = "프로필 이미지"
             )
+        }
+        UserLevelBadge(
+            modifier = Modifier.offset(y = (-15).dp),
+            xpPoint = myInfo.xpPoint
+        )
+        Spacer(Modifier.width(15.5.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = myInfo.nickname.orEmpty(),
+                style = profileNicknameTextStyle
+            )
             Box(
                 modifier = Modifier
-                    .offset(y = 4.dp)
                     .size(18.dp)
                     .clip(CircleShape)
                     .background(Color.Black)
-                    .align(Alignment.BottomEnd),
-                contentAlignment = Alignment.Center
             ) {
                 Icon(
+                    modifier = Modifier.align(Alignment.Center),
                     painter = painterResource(R.drawable.edit),
                     tint = gray100,
                     contentDescription = null
                 )
             }
-        }
-        Spacer(Modifier.width(12.dp))
-        Column {
-            Text(
-                text = myInfo.nickname!!,
-                style = profileNicknameTextStyle,
-            )
-            Spacer(Modifier.height(4.dp))
-            UserLevelBadge(xpPoint = myInfo.xpPoint)
+
         }
     }
 }
 
 @Composable
-fun UserLevelBadge(
+private fun UserLevelBadge(
     modifier: Modifier = Modifier,
     xpPoint: Int
 ) {
     Box(
         modifier = modifier
-            .width(57.dp)
-            .clip(RoundedCornerShape(20.dp))
+            .width(85.5.dp)
+            .clip(RoundedCornerShape(30.dp))
             .background(Color.White)
             .border(
                 width = 1.dp,
                 color = primary,
-                shape = RoundedCornerShape(20.dp)
+                shape = RoundedCornerShape(30.dp)
             )
             .padding(
                 vertical = 4.dp,
@@ -134,9 +153,10 @@ fun UserLevelBadge(
 }
 
 private val userProfileCardLevelTextStyle = TextStyle(
-    fontFamily = FontFamily(Font(pretendard_bold)),
-    fontSize = 13.sp,
-    lineHeight = 12.sp,
+    fontFamily = pretendardFontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 19.5.sp,
+    lineHeight = 18.sp,
     color = primary
 )
 
@@ -147,28 +167,12 @@ private val profileNicknameTextStyle = TextStyle(
     color = gray500
 )
 
-@Preview
-@Composable
-fun UserProfileCardPreview() {
-    UserProfileContent(
-        MyInfo(
-            nickname = "김일상1234",
-            email = "",
-            profileImage = null,
-            completeChallengeCount = 0,
-            couponCount = 0,
-            xpPoint = 160,
-            status = ""
-        )
-    ) {}
-}
-
 @Preview(showBackground = true)
 @Composable
 fun MyInfoProfileContentPreview() {
     MyInfoProfileContent(
-        MyInfo(
-            nickname = "김일상1234",
+        myInfo = MyInfo(
+            nickname = "김일상123닉네임 길이가 길어",
             email = "",
             profileImage = null,
             completeChallengeCount = 0,
