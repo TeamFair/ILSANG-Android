e03b38fcaf2f0bb9968bf75771984bcfa64be8c2
kimseongwoo <tjddn929@naver.com>
2025-07-23 23:01:46 +0900

    [Feat#54] 칭호 (Title) 도메인 모델 클래스 생성

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/Title.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.core.model
+
+data class Title(
+    val id: String,
+    val name: String,
+    val type: String,
+    val condition: String?,
+    val createdAt: String
+)
