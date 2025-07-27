191c910ea4fb5c852ec1f224531641b5525c56cf
kimseongwoo <tjddn929@naver.com>
2025-07-27 15:33:26 +0900

    [Refactor#54] title 도에인 모델에 historyId 프로퍼티 추가

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/Title.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/Title.kt
@@ -2,6 +2,7 @@ package com.ilsangtech.ilsang.core.model
 
 data class Title(
     val id: String,
+    val historyId: String? = null,
     val name: String,
     val type: String,
     val condition: String?,
