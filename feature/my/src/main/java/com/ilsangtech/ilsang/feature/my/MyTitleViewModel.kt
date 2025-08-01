e3fe093dde7d19a0e3ac3a0a716a104c24b4cade
swkim1 <swkim1@hds-secu.com>
2025-08-01 15:55:25 +0900

    [Feat#54] MyTitleViewModel 내 선택된 칭호 상태 관리 및 내 칭호 업데이트 함수 구현

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/MyTitleViewModel.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/MyTitleViewModel.kt
@@ -3,14 +3,21 @@ package com.ilsangtech.ilsang.feature.my
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ilsangtech.ilsang.core.domain.TitleRepository
+import com.ilsangtech.ilsang.core.domain.UserRepository
+import com.ilsangtech.ilsang.core.model.Title
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyTitleViewModel @Inject constructor(
+    private val userRepository: UserRepository,
     private val titleRepository: TitleRepository
 ) : ViewModel() {
     val myTitleUiState = flow {
@@ -20,4 +27,23 @@ class MyTitleViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = emptyList()
     )
+
+    private val _selectedTitle = MutableStateFlow<Title?>(null)
+    val selectedTitle = _selectedTitle.asStateFlow()
+
+    fun selectTitle(title: Title) {
+        _selectedTitle.update {
+            if (it != title) title else null
+        }
+    }
+
+    fun updateUserTitle() {
+        viewModelScope.launch {
+            selectedTitle.value.let { title ->
+                title?.historyId?.let { titleHistoryId ->
+                    userRepository.updateUserTitle(titleHistoryId)
+                }
+            }
+        }
+    }
 }
