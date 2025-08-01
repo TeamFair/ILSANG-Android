4742890bce10a460e178606d19fbfabe39abe9ae
swkim1 <swkim1@hds-secu.com>
2025-08-01 13:40:09 +0900

    [Feat#61] 퀘스트 모듈 화면 이동 navigation 구현

---
+++ feature/quest/src/main/java/com/ilsangtech/ilsang/feature/quest/navigation/QuestNavigation.kt
@@ -0,0 +1,19 @@
+package com.ilsangtech.ilsang.feature.quest.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.ilsangtech.ilsang.feature.quest.QuestTabScreen
+
+fun NavGraphBuilder.questNavigation(
+    onNavigateToSubmit: () -> Unit
+) {
+    navigation(
+        route = "QuestBaseRoute",
+        startDestination = "Quest"
+    ) {
+        composable("Quest") {
+            QuestTabScreen(navigateToSubmit = onNavigateToSubmit)
+        }
+    }
+}
