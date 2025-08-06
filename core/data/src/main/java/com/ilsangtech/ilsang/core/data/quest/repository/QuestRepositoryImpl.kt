5df3730c245c9a3f36b658642d15096f523e8879
kimseongwoo <tjddn929@naver.com>
2025-08-06 14:33:36 +0900

    [Feat#60] 퀘스트 리스트 반환 함수를 flow 타입으로 변경, 즐겨찾기 성공 시 로컬 캐시에 저장 후 리스트 정보에 반영하도록 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/quest/repository/QuestRepositoryImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/quest/repository/QuestRepositoryImpl.kt
@@ -5,65 +5,114 @@ import com.ilsangtech.ilsang.core.data.quest.toQuest
 import com.ilsangtech.ilsang.core.domain.QuestRepository
 import com.ilsangtech.ilsang.core.model.Quest
 import com.ilsangtech.ilsang.core.network.model.quest.QuestNetworkModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.update
 
 class QuestRepositoryImpl(
     private val questDataSource: QuestDataSource,
 ) : QuestRepository {
-    override suspend fun getPopularQuests(): List<Quest> {
-        return questDataSource.getUncompletedTotalQuest(
-            popularYn = true,
-            size = 8,
-        ).data.map(QuestNetworkModel::toQuest)
-    }
+    private val questCache = MutableStateFlow<Map<String, Boolean>>(emptyMap())
 
-    override suspend fun getRecommendedQuests(): List<Quest> {
-        return questDataSource.getUncompletedTotalQuest(
-            popularYn = false,
-            size = 10
-        ).data.map(QuestNetworkModel::toQuest)
-    }
-
-    override suspend fun getLargeRewardQuests(): Map<String, List<Quest>> {
-        val rewardContentList = listOf("INTELLECT", "SOCIABILITY", "STRENGTH", "FUN", "CHARM")
-        return rewardContentList.associateWith { rewardContent ->
-            questDataSource.getLargeRewardQuest(
-                rewardContent = rewardContent,
-                size = 3
+    override suspend fun getPopularQuests(): Flow<List<Quest>> = flow {
+        emit(
+            questDataSource.getUncompletedTotalQuest(
+                popularYn = true,
+                size = 8,
             ).data.map(QuestNetworkModel::toQuest)
+        )
+    }.combine(questCache) { quests, questCache ->
+        quests.map { quest ->
+            quest.copy(favoriteYn = questCache[quest.questId] ?: quest.favoriteYn)
         }
     }
 
-    override suspend fun getUncompletedNormalQuests(): List<Quest> {
-        return questDataSource.getUncompletedNormalQuest(
-            page = 0,
-            size = 60
-        ).data.map(QuestNetworkModel::toQuest)
+    override suspend fun getRecommendedQuests(): Flow<List<Quest>> = flow {
+        emit(
+            questDataSource.getUncompletedTotalQuest(
+                popularYn = false,
+                size = 10
+            ).data.map(QuestNetworkModel::toQuest)
+        )
+    }.combine(questCache) { quests, questCache ->
+        quests.map { quest ->
+            quest.copy(favoriteYn = questCache[quest.questId] ?: quest.favoriteYn)
+        }
     }
 
-    override suspend fun getUncompletedRepeatQuests(status: String): List<Quest> {
-        return questDataSource.getUncompletedRepeatQuest(
-            page = 0,
-            size = 60,
-            status = status
-        ).data.map(QuestNetworkModel::toQuest)
+    override suspend fun getLargeRewardQuests(): Flow<Map<String, List<Quest>>> = flow {
+        val rewardContentList = listOf("INTELLECT", "SOCIABILITY", "STRENGTH", "FUN", "CHARM")
+        emit(
+            rewardContentList.associateWith { rewardContent ->
+                questDataSource.getLargeRewardQuest(
+                    rewardContent = rewardContent,
+                    size = 3
+                ).data.map(QuestNetworkModel::toQuest)
+            }
+        )
+    }.combine(questCache) { quests, questCache ->
+        quests.mapValues { (_, quests) ->
+            quests.map { quest ->
+                quest.copy(favoriteYn = questCache[quest.questId] ?: quest.favoriteYn)
+            }
+        }
     }
 
-    override suspend fun getUncompletedEventQuests(): List<Quest> {
-        return questDataSource.getUncompletedEventQuest(
-            page = 0,
-            size = 60
-        ).data.map(QuestNetworkModel::toQuest)
+    override suspend fun getUncompletedNormalQuests(): Flow<List<Quest>> = flow {
+        emit(
+            questDataSource.getUncompletedNormalQuest(
+                page = 0,
+                size = 60
+            ).data.map(QuestNetworkModel::toQuest)
+        )
+    }.combine(questCache) { quests, questCache ->
+        quests.map { quest ->
+            quest.copy(favoriteYn = questCache[quest.questId] ?: quest.favoriteYn)
+        }
     }
 
-    override suspend fun registerFavoriteQuest(questId: String): Result<Unit> {
-        return runCatching {
+    override suspend fun getUncompletedRepeatQuests(status: String): Flow<List<Quest>> = flow {
+        emit(
+            questDataSource.getUncompletedRepeatQuest(
+                page = 0,
+                size = 60,
+                status = status
+            ).data.map(QuestNetworkModel::toQuest)
+        )
+    }.combine(questCache) { quests, questCache ->
+        quests.map { quest ->
+            quest.copy(favoriteYn = questCache[quest.questId] ?: quest.favoriteYn)
+        }
+    }
+
+    override suspend fun getUncompletedEventQuests(): Flow<List<Quest>> = flow {
+        emit(
+            questDataSource.getUncompletedEventQuest(
+                page = 0,
+                size = 60
+            ).data.map(QuestNetworkModel::toQuest)
+        )
+    }.combine(questCache) { quests, questCache ->
+        quests.map { quest ->
+            quest.copy(favoriteYn = questCache[quest.questId] ?: quest.favoriteYn)
+        }
+    }
+
+    override suspend fun registerFavoriteQuest(questId: String) {
+        runCatching {
             questDataSource.registerFavoriteQuest(questId)
+        }.onSuccess {
+            questCache.update { it + (questId to true) }
         }
     }
 
-    override suspend fun deleteFavoriteQuest(questId: String): Result<Unit> {
-        return runCatching {
+    override suspend fun deleteFavoriteQuest(questId: String) {
+        runCatching {
             questDataSource.deleteFavoriteQuest(questId)
+        }.onSuccess {
+            questCache.update { it + (questId to false) }
         }
     }
 }
