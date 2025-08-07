94189ba600248a4f82dbb02adb5920c7c54aab2b
kimseongwoo <tjddn929@naver.com>
2025-08-07 11:50:17 +0900

    [Feat#60] 구현한 하단 시트 UI 컴포넌트를 조합하여 QuestBottomSheet 구

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/bottomsheet/QueustBottomSheet.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/bottomsheet/QueustBottomSheet.kt
@@ -1,6 +1,5 @@
 package com.ilsangtech.ilsang.core.ui.quest.bottomsheet
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,10 +8,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -20,11 +17,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -32,31 +28,18 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import coil3.compose.AsyncImage
 import com.ilsangtech.ilsang.core.model.Quest
-import com.ilsangtech.ilsang.core.model.QuestType
 import com.ilsangtech.ilsang.core.model.Reward
-import com.ilsangtech.ilsang.core.model.RewardType
-import com.ilsangtech.ilsang.core.ui.BuildConfig
-import com.ilsangtech.ilsang.core.ui.R
-import com.ilsangtech.ilsang.designsystem.R.font.pretendard_bold
 import com.ilsangtech.ilsang.designsystem.R.font.pretendard_regular
 import com.ilsangtech.ilsang.designsystem.R.font.pretendard_semibold
 import com.ilsangtech.ilsang.designsystem.component.IlsangBottomSheet
-import com.ilsangtech.ilsang.designsystem.theme.background
-import com.ilsangtech.ilsang.designsystem.theme.badge01TextStyle
 import com.ilsangtech.ilsang.designsystem.theme.gray100
 import com.ilsangtech.ilsang.designsystem.theme.gray500
-import com.ilsangtech.ilsang.designsystem.theme.heading01
-import com.ilsangtech.ilsang.designsystem.theme.heading02
 import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
 import com.ilsangtech.ilsang.designsystem.theme.primary
-import com.ilsangtech.ilsang.designsystem.theme.primary100
 import com.ilsangtech.ilsang.designsystem.theme.primary300
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -76,11 +59,9 @@ fun QuestBottomSheet(
             isFavorite = quest.favoriteYn,
             onFavoriteClick = onFavoriteClick
         )
-        QuestBottomSheetContent(
-            modifier = Modifier.padding(horizontal = 20.dp),
-            quest = quest,
-            onApproveButtonClick = onApproveButtonClick
-        )
+        QuestBottomSheetContent(quest = quest)
+        Spacer(Modifier.height(16.dp))
+        QuestBottomSheetFooter(onClick = onApproveButtonClick)
     }
 }
 
@@ -121,81 +102,40 @@ private fun QuestBottomSheetHeader(
 @Composable
 private fun QuestBottomSheetContent(
     modifier: Modifier = Modifier,
-    quest: Quest,
-    onApproveButtonClick: () -> Unit
+    quest: Quest
 ) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(top = 16.dp),
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        QuestInfoContent(quest = quest)
+        Row(modifier = Modifier.fillMaxWidth()) {
+            QuestApprovalExample(
+                modifier = Modifier.weight(1f),
+                imageId = null,
+                onImageClick = {}
+            )
+            MyQuestRank(
+                modifier = Modifier.weight(1f),
+                rank = quest.score
+            )
+        }
+        ObtainablePointsContent(quest.rewardList)
+    }
+}
+
+@Composable
+private fun QuestBottomSheetFooter(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Row(
-            modifier = Modifier.padding(top = 10.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AsyncImage(
-                modifier = Modifier
-                    .size(80.dp)
-                    .clip(CircleShape)
-                    .background(Color(0xFFF1F5FF)),
-                model = BuildConfig.IMAGE_URL + quest.imageId,
-                contentDescription = quest.missionTitle
-            )
-            Spacer(Modifier.width(8.dp))
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text =
-                        (QuestType.entries.find {
-                            it.name == quest.type
-                        }?.title ?: "기본") + " 퀘스트",
-                    style = questBottomSheetQuestTypeTextStyle
-                )
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = quest.missionTitle,
-                    style = questBottomSheetQuestTitleStyle,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .background(
-                        color = primary100,
-                        shape = RoundedCornerShape(12.dp)
-                    )
-                    .padding(10.dp)
-            ) {
-                Text(
-                    text = "${quest.rewardList.sumOf { it.quantity }}XP",
-                    style = heading01,
-                    color = primary,
-                )
-            }
-        }
-
-        Box(
-            modifier = Modifier
-                .background(
-                    color = primary,
-                    shape = RoundedCornerShape(12.dp)
-                )
-                .padding(
-                    horizontal = 8.dp,
-                    vertical = 4.dp
-                )
-        ) {
-            Text(
-                text = "획득 가능 스텟",
-                style = badge01TextStyle,
-                color = Color.White
-            )
-        }
-        ObtainableStatItemContent(quest.rewardList)
         Text(
             text = "퀘스트를 수행하셨나요?\n" +
                     "인증 후 포인트를 적립받으세요",
@@ -204,7 +144,7 @@ private fun QuestBottomSheetContent(
         )
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = onApproveButtonClick,
+            onClick = onClick,
             shape = RoundedCornerShape(12.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = primary
@@ -219,146 +159,12 @@ private fun QuestBottomSheetContent(
     }
 }
 
-@Composable
-private fun ObtainableStatItemContent(rewardList: List<Reward>) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(32.dp)
-    ) {
-        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            val strengthReward = rewardList.find { it.content == RewardType.STRENGTH.name }
-            val intellectReward = rewardList.find { it.content == RewardType.INTELLECT.name }
-            val charmReward = rewardList.find { it.content == RewardType.CHARM.name }
-
-            ObtainableStatItem(
-                rewardType = RewardType.STRENGTH,
-                xp = strengthReward?.quantity ?: 0
-            )
-            ObtainableStatItem(
-                rewardType = RewardType.INTELLECT,
-                xp = intellectReward?.quantity ?: 0
-            )
-            ObtainableStatItem(
-                rewardType = RewardType.CHARM,
-                xp = charmReward?.quantity ?: 0
-            )
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            val funReward = rewardList.find { it.content == RewardType.FUN.name }
-            val sociabilityReward =
-                rewardList.find { it.content == RewardType.SOCIABILITY.name }
-
-            ObtainableStatItem(
-                rewardType = RewardType.FUN,
-                xp = funReward?.quantity ?: 0
-            )
-            ObtainableStatItem(
-                rewardType = RewardType.SOCIABILITY,
-                xp = sociabilityReward?.quantity ?: 0
-            )
-        }
-    }
-}
-
-@Composable
-private fun ObtainableStatItem(rewardType: RewardType, xp: Int) {
-    val painterResource = painterResource(
-        when (rewardType) {
-            RewardType.STRENGTH -> {
-                R.drawable.strength
-            }
-
-            RewardType.INTELLECT -> {
-                R.drawable.bulb
-            }
-
-            RewardType.CHARM -> {
-                R.drawable.heart
-            }
-
-            RewardType.FUN -> {
-                R.drawable.`fun`
-            }
-
-            RewardType.SOCIABILITY -> {
-                R.drawable.social
-            }
-        }
-    )
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .width(60.dp)
-                .background(
-                    color = background,
-                    shape = RoundedCornerShape(12.dp)
-                )
-                .padding(vertical = 4.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = rewardType.title,
-                style = heading02,
-                color = gray500
-            )
-        }
-        Icon(
-            modifier = Modifier.size(48.dp),
-            painter = painterResource,
-            tint = Color.Unspecified,
-            contentDescription = "보상 타입 아이콘"
-        )
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = "${xp}P",
-                style = questBottomSheetRewardPointTextStyle
-            )
-            Icon(
-                modifier = Modifier.padding(
-                    vertical = 3.dp,
-                    horizontal = 4.dp
-                ),
-                painter = painterResource(R.drawable.quest_bottom_sheet_reward_xp_up),
-                tint = Color.Unspecified,
-                contentDescription = null
-            )
-        }
-    }
-}
-
 private val questBottomSheetTitleStyle = TextStyle(
     fontFamily = pretendardFontFamily,
     fontWeight = FontWeight.Bold,
     fontSize = 17.sp,
     lineHeight = 22.sp,
     color = gray500
-)
-
-private val questBottomSheetQuestTypeTextStyle = TextStyle(
-    fontFamily = FontFamily(Font(pretendard_regular)),
-    fontSize = 15.sp,
-    lineHeight = 30.sp,
-    letterSpacing = (-0.02).em,
-    color = gray500
-)
-
-private val questBottomSheetQuestTitleStyle = TextStyle(
-    fontFamily = FontFamily(Font(pretendard_bold)),
-    fontSize = 18.sp,
-    color = gray500,
-    lineHeight = 30.sp,
-    letterSpacing = (-0.02).sp,
-)
-
-private val questBottomSheetRewardPointTextStyle = TextStyle(
-    fontFamily = FontFamily(Font(pretendard_bold)),
-    fontSize = 13.sp,
-    lineHeight = 20.sp,
-    color = primary
 )
 
 private val questBottomSheetDescriptionTextStyle = TextStyle(
@@ -379,7 +185,7 @@ private val questBottomSheetApproveButtonTextStyle = TextStyle(
 @Preview(showBackground = true)
 @Composable
 private fun QuestBottomSheetPreview() {
-    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val bottomSheetState = rememberStandardBottomSheetState()
     QuestBottomSheet(
         quest = previewQuest,
         bottomSheetState = bottomSheetState,
@@ -392,7 +198,7 @@ private fun QuestBottomSheetPreview() {
 @Preview(showBackground = true)
 @Composable
 private fun QuestBottomSheetContentPreview() {
-    QuestBottomSheetContent(quest = previewQuest) {}
+    QuestBottomSheetContent(quest = previewQuest)
 }
 
 private val previewReward = Reward(
