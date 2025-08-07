963fed72a651fd62c8e1c0efbed3cd02cc48571c
swkim1 <swkim1@hds-secu.com>
2025-08-07 14:04:43 +0900

    [Feat#63] 내 일상존 화면 헤더 UI 적용

--- feature/myzone/src/main/java/com/ilsangtech/ilsang/feature/myzone/MyZoneScreen.kt
+++ feature/myzone/src/main/java/com/ilsangtech/ilsang/feature/myzone/MyZoneScreen.kt
@@ -1,15 +1,28 @@
 package com.ilsangtech.ilsang.feature.myzone
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.ilsangtech.ilsang.feature.myzone.component.MyZoneHeader
 
 @Composable
-fun MyZoneScreen() {
-
+fun MyZoneScreen(onBackButtonClick: () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Column {
+            MyZoneHeader(onBackButtonClick = onBackButtonClick)
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun MyZoneScreenPreview() {
-    MyZoneScreen()
+    MyZoneScreen {}
 }
