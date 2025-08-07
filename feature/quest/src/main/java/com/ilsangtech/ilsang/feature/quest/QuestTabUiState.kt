6f3a92b2bbd9cbdefd0b3da643928ec049e4c5e3
kimseongwoo <tjddn929@naver.com>
2025-08-07 09:47:46 +0900

    [Feat#60] 정렬 타입 enum class 생성

--- feature/quest/src/main/java/com/ilsangtech/ilsang/feature/quest/QuestTabUiState.kt
+++ feature/quest/src/main/java/com/ilsangtech/ilsang/feature/quest/QuestTabUiState.kt
@@ -10,3 +10,9 @@ sealed interface QuestTabUiState {
 
 data class QuestTabUiData(val questList: List<Quest>)
 
+enum class SortType(val title: String) {
+    FAVORITE("즐겨찾기만"),
+    POINT_DESC("포인트 높은 순"),
+    POINT_ASC("포인트 낮은 순"),
+    POPULAR("인기순")
+}
