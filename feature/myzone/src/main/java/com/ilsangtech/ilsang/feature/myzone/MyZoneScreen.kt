6ef2441452fe4c7d914c12fd04f6ace7f294f779
kimseongwoo <tjddn929@naver.com>
2025-08-07 12:59:29 +0900

    [Feat#63] MyZoneScreen 생성

---
+++ feature/myzone/src/main/java/com/ilsangtech/ilsang/feature/myzone/MyZoneScreen.kt
@@ -0,0 +1,15 @@
+package com.ilsangtech.ilsang.feature.myzone
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MyZoneScreen() {
+
+}
+
+@Preview
+@Composable
+private fun MyZoneScreenPreview() {
+    MyZoneScreen()
+}
