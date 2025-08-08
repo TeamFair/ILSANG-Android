e0e2f98338b38ec82593c99ac077ee57983d8544
kimseongwoo <tjddn929@naver.com>
2025-08-08 09:43:34 +0900

    [Feat#63] 내 일상존 선택 화면 UI 상태 클래스 구현

---
+++ feature/myzone/src/main/java/com/ilsangtech/ilsang/feature/myzone/MyZoneUiState.kt
@@ -0,0 +1,10 @@
+package com.ilsangtech.ilsang.feature.myzone
+
+import com.ilsangtech.ilsang.core.model.area.CommercialArea
+import com.ilsangtech.ilsang.core.model.area.MetroArea
+
+data class MyZoneUiState(
+    val selectedMetroArea: MetroArea?,
+    val selectedCommercialArea: CommercialArea?,
+    val areaList: List<MetroArea>
+)
