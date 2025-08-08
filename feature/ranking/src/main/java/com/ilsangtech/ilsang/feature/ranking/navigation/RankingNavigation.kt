a5dd79bfe0f0be95b9a2d1f57f4f6edee0f248d8
swkim1 <swkim1@hds-secu.com>
2025-08-08 17:58:16 +0900

    [Feat#68] Ranking 관련 라우트 생성 및 랭킹 navigation graph 구축

---
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/navigation/RankingNavigation.kt
@@ -0,0 +1,23 @@
+package com.ilsangtech.ilsang.feature.ranking.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.ilsangtech.ilsang.feature.ranking.RankingTabScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object RankingBaseRoute
+
+@Serializable
+data object RankingRoute
+
+fun NavGraphBuilder.rankingNavigation(
+    navigateToProfile: (String) -> Unit
+) {
+    navigation<RankingBaseRoute>(startDestination = RankingRoute) {
+        composable<RankingRoute> {
+            RankingTabScreen(navigateToProfile = navigateToProfile)
+        }
+    }
+}
