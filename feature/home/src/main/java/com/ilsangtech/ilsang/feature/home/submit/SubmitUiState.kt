a79dc6dcc52d8dff666b4812e06aca5b66495668
swkim1 <swkim1@hds-secu.com>
2025-08-08 16:42:29 +0900

    [Refactor#66] SubmitUiState submit 모듈로 이동, Success에 보상 리스트 프로퍼티 추가

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/submit/SubmitUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.ilsangtech.ilsang.feature.home.submit
-
-sealed interface SubmitUiState {
-    data object NotSubmitted : SubmitUiState
-    data object Loading : SubmitUiState
-    data object Success : SubmitUiState
-    data object Error : SubmitUiState
-}
