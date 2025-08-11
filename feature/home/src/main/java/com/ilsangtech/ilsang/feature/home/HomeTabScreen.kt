1db4f90fee1fc0a322cd2aa3ef1406ac8bef1883
swkim1 <swkim1@hds-secu.com>
2025-08-11 15:53:07 +0900

    [Refactor#63] 홈 화면 배너 컴포넌트 분리

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeTabScreen.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeTabScreen.kt
@@ -1,16 +1,10 @@
 package com.ilsangtech.ilsang.feature.home
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -18,15 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil3.compose.AsyncImage
-import com.ilsangtech.ilsang.core.model.Banner
 import com.ilsangtech.ilsang.core.ui.quest.bottomsheet.QuestBottomSheet
+import com.ilsangtech.ilsang.feature.home.component.BannerContent
 import com.ilsangtech.ilsang.feature.home.component.HomeTabHeader
 import com.ilsangtech.ilsang.feature.home.component.LargeRewardQuestsContent
 import com.ilsangtech.ilsang.feature.home.component.PopularQuestsContent
@@ -89,7 +80,7 @@ fun HomeTabScreen(
                     )
                 }
                 item {
-                    BannerView(
+                    BannerContent(
                         banners = banners,
                         onClick = navigateToQuestTab
                     )
@@ -127,36 +118,6 @@ fun HomeTabScreen(
                 item { Spacer(Modifier.height(84.dp)) }
             }
         }
-    }
-}
-
-@Composable
-fun BannerView(
-    banners: List<Banner>,
-    onClick: () -> Unit
-) {
-    val pagerState = rememberPagerState { banners.size }
-    HorizontalPager(
-        state = pagerState
-    ) { page ->
-        AsyncImage(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(11 / 10f)
-                .clip(RoundedCornerShape(12.dp))
-                .clickable(
-                    onClick = {
-                        if (banners[page].description.contains("quest")) {
-                            onClick()
-                        }
-                    },
-                    indication = null,
-                    interactionSource = null
-                ),
-            contentScale = ContentScale.Crop,
-            model = BuildConfig.IMAGE_URL + banners[page].imageId,
-            contentDescription = banners[page].description
-        )
     }
 }
 
