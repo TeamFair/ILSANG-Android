b0a20149ecf19b5deb1582ee90e3edd72a3830b1
swkim1 <swkim1@hds-secu.com>
2025-08-11 14:00:30 +0900

    [Feat#70] 퀘스트 라우트 직렬화 객체 생성 및 적용

--- feature/quest/src/main/java/com/ilsangtech/ilsang/feature/quest/navigation/QuestNavigation.kt
+++ feature/quest/src/main/java/com/ilsangtech/ilsang/feature/quest/navigation/QuestNavigation.kt
@@ -4,15 +4,19 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.ilsangtech.ilsang.feature.quest.QuestTabScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object QuestBaseRoute
+
+@Serializable
+data object QuestRoute
 
 fun NavGraphBuilder.questNavigation(
     onNavigateToSubmit: (String) -> Unit
 ) {
-    navigation(
-        route = "QuestBaseRoute",
-        startDestination = "Quest"
-    ) {
-        composable("Quest") {
+    navigation<QuestBaseRoute>(startDestination = QuestRoute) {
+        composable<QuestRoute> {
             QuestTabScreen(navigateToSubmit = onNavigateToSubmit)
         }
     }
