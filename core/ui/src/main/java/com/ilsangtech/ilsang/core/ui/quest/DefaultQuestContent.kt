5a010d883891bda8cfba8e7254288f70688040a0
kimseongwoo <tjddn929@naver.com>
2025-08-13 08:45:07 +0900

    [Feat#73] 새로 보상 포인트 칩이 적용된 DefaultQuestContent 구현

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/DefaultQuestContent.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/DefaultQuestContent.kt
@@ -17,8 +17,42 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ilsangtech.ilsang.core.model.Quest
 import com.ilsangtech.ilsang.core.model.Reward
+import com.ilsangtech.ilsang.core.model.RewardPoint
 import com.ilsangtech.ilsang.designsystem.theme.gray400
 import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
+
+@Composable
+fun DefaultQuestContent(
+    modifier: Modifier = Modifier,
+    title: String,
+    writer: String,
+    rewardPoints: List<RewardPoint>,
+    questImage: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        questImage()
+        Spacer(Modifier.width(20.dp))
+        Column {
+            Text(
+                text = title,
+                maxLines = 1,
+                style = defaultQuestContentTitleStyle,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = writer,
+                maxLines = 1,
+                style = defaultQuestContentWriterStyle,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(Modifier.height(4.dp))
+            RewardPointChips(rewardPoints)
+        }
+    }
+}
 
 @Composable
 fun DefaultQuestContent(
@@ -122,3 +156,26 @@ private fun DefaultQuestContentPreview() {
     )
 }
 
+@Preview(showBackground = true)
+@Composable
+fun DefaultQuestContentPreview2() {
+    val title = "퀘스트 타이틀 타이틀"
+    val writer = "퀘스트 작성자 작성자"
+    val rewardPoints = listOf(
+        RewardPoint.Metro(2),
+        RewardPoint.Commerical(5),
+        RewardPoint.Contribute(10)
+    )
+
+    DefaultQuestContent(
+        title = title,
+        writer = writer,
+        rewardPoints = rewardPoints,
+        questImage = {
+            DefaultQuestImage(
+                imageId = "sample_image_id",
+                contentDescription = title
+            )
+        }
+    )
+}
