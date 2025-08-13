31a266597a53cfdfa42be05cb154295764c2ee85
kimseongwoo <tjddn929@naver.com>
2025-08-14 08:57:49 +0900

    [Feat#73] 배너 상세화면 ViewModel 클래스 생성

---
+++ feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/BannerDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.ilsangtech.ilsang.feature.banner
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BannerDetailViewModel @Inject constructor(
+) : ViewModel() {
+}
