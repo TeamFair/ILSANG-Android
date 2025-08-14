699e18efb728ab66c640eb42e99ec4891c38972e
swkim1 <swkim1@hds-secu.com>
2025-08-14 16:18:53 +0900

    [Design#75] 홈탭 배너 모서리 둥글게 처리 제거

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/component/BannerContent.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/component/BannerContent.kt
@@ -5,12 +5,9 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.ilsangtech.ilsang.core.model.Banner
 import com.ilsangtech.ilsang.feature.home.BuildConfig
@@ -30,7 +27,6 @@ internal fun BannerContent(
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(11 / 10f)
-                .clip(RoundedCornerShape(12.dp))
                 .clickable(
                     onClick = { onClick(banners[page]) },
                     indication = null,
