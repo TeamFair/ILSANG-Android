5fac0c03b864c026c324810571dcc57e9b501e85
swkim1 <swkim1@hds-secu.com>
2025-08-14 13:56:50 +0900

    [Fix#75] 배너 상세 화면 헤더에 statusBarPadding 추가

--- feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/component/BannerDetailHeader.kt
+++ feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/component/BannerDetailHeader.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +29,7 @@ internal fun BannerDetailHeader(
     Box(
         modifier = modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .padding(top = 12.dp, bottom = 11.dp)
             .padding(start = 15.dp)
     ) {
