bc09510bd88357c33b028f7bbe871e0664efff07
swkim1 <swkim1@hds-secu.com>
2025-08-14 15:47:25 +0900

    [Feat#75] 파라미터로 Banner를 받아 정보를 추출하여 BannerDetailRoute로 이동할 수 있도록 하는 확장 함수 구현

--- feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/navigation/BannerNavigation.kt
+++ feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/navigation/BannerNavigation.kt
@@ -1,8 +1,10 @@
 package com.ilsangtech.ilsang.feature.banner.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.ilsangtech.ilsang.core.model.Banner
 import com.ilsangtech.ilsang.feature.banner.BannerDetailScreen
 import kotlinx.serialization.Serializable
 
@@ -16,6 +18,17 @@ data class BannerDetailRoute(
     val description: String,
     val imageId: String
 )
+
+fun NavHostController.navigateToBannerDetail(banner: Banner) {
+    navigate(
+        BannerDetailRoute(
+            id = banner.id,
+            title = banner.title,
+            description = banner.description,
+            imageId = banner.imageId
+        )
+    )
+}
 
 fun NavGraphBuilder.bannerNavigation(
     onBackButtonClick: () -> Unit
