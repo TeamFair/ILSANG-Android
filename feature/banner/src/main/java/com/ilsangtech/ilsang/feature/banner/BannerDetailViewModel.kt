83ebe38d9676d66c70b7814b408b6d6231d4019a
kimseongwoo <tjddn929@naver.com>
2025-08-15 16:44:27 +0900

    [Feat#75] 배너 상세 화면 관련 추가된 UI 상태 제어 로직 구현

--- feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/BannerDetailViewModel.kt
+++ feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/BannerDetailViewModel.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import com.ilsangtech.ilsang.core.model.quest.BannerQuest
 import com.ilsangtech.ilsang.feature.banner.navigation.BannerDetailRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,17 +22,28 @@ class BannerDetailViewModel @Inject constructor(
 ) : ViewModel() {
     private val bannerDetailInfo = savedStateHandle.toRoute<BannerDetailRoute>()
 
-    val uiState = flow {
-        emit(
-            BannerDetailUiState(
-                id = bannerDetailInfo.id,
-                title = bannerDetailInfo.title,
-                description = bannerDetailInfo.description,
-                imageId = bannerDetailInfo.imageId,
-                bannerQuestUiState = BannerQuestUiState.Success(
-                    onGoingQuests = emptyList(),
-                    completedQuests = emptyList()
-                )
+    private val _selectedQuestType = MutableStateFlow(BannerDetailQuestType.OnGoing)
+    private val selectedQuestType = _selectedQuestType.asStateFlow()
+
+    private val _selectedSortType = MutableStateFlow(BannerDetailSortType.ExpiredDate)
+    private val selectedSortType = _selectedSortType.asStateFlow()
+
+    val uiState = selectedSortType.map { sortType ->
+        //TODO 배너 퀘스트 API 적용
+        val onGoingQuests = emptyList<BannerQuest>()
+        val completedQuests = emptyList<BannerQuest>()
+        Triple(sortType, onGoingQuests, completedQuests)
+    }.combine(selectedQuestType) { (sortType, onGoingQuests, completedQuests), questType ->
+        BannerDetailUiState(
+            id = bannerDetailInfo.id,
+            title = bannerDetailInfo.title,
+            description = bannerDetailInfo.description,
+            imageId = bannerDetailInfo.imageId,
+            selectedQuestType = questType,
+            selectedSortType = sortType,
+            bannerQuestUiState = BannerQuestUiState.Success(
+                onGoingQuests = onGoingQuests,
+                completedQuests = completedQuests
             )
         )
     }.stateIn(
@@ -38,7 +54,17 @@ class BannerDetailViewModel @Inject constructor(
             title = bannerDetailInfo.title,
             description = bannerDetailInfo.description,
             imageId = bannerDetailInfo.imageId,
+            selectedQuestType = BannerDetailQuestType.OnGoing,
+            selectedSortType = BannerDetailSortType.ExpiredDate,
             bannerQuestUiState = BannerQuestUiState.Loading
         )
     )
+
+    fun onQuestTypeChanged(questType: BannerDetailQuestType) {
+        _selectedQuestType.update { questType }
+    }
+
+    fun onSortTypeChanged(sortType: BannerDetailSortType) {
+        _selectedSortType.update { sortType }
+    }
 }
