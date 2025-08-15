2c63ef34d4058adb0ae002ce755c7d1227d7bb15
kimseongwoo <tjddn929@naver.com>
2025-08-15 16:42:36 +0900

    [Feat#75] 배너 상세화면 UI 상태 추가

--- feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/BannerDetailUiState.kt
+++ feature/banner/src/main/java/com/ilsangtech/ilsang/feature/banner/BannerDetailUiState.kt
@@ -7,6 +7,8 @@ data class BannerDetailUiState(
     val title: String,
     val description: String,
     val imageId: String,
+    val selectedQuestType: BannerDetailQuestType,
+    val selectedSortType: BannerDetailSortType,
     val bannerQuestUiState: BannerQuestUiState
 )
 
@@ -18,4 +20,43 @@ sealed interface BannerQuestUiState {
     ) : BannerQuestUiState
 
     data object Error : BannerQuestUiState
+}
+
+enum class BannerDetailQuestType {
+    OnGoing {
+        override fun toString(): String {
+            return "진행중"
+        }
+    },
+    Completed {
+        override fun toString(): String {
+            return "완료"
+        }
+    }
+}
+
+enum class BannerDetailSortType {
+    PointAsc {
+        override fun toString(): String {
+            return "포인트 낮은순"
+        }
+    },
+
+    PointDesc {
+        override fun toString(): String {
+            return "포인트 높은순"
+        }
+    },
+
+    ExpiredDate {
+        override fun toString(): String {
+            return "임박순"
+        }
+    },
+
+    Popular {
+        override fun toString(): String {
+            return "인기순"
+        }
+    }
 }
