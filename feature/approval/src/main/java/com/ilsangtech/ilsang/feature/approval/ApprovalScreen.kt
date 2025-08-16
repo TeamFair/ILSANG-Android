91f1d9d4fdfbf54d79d6b0caf0d4174d3694bf9e
kimseongwoo <tjddn929@naver.com>
2025-08-16 10:28:24 +0900

    [Feat#77] 인증 아이템이 없을 경우에 보여지는 UI 적용

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalScreen.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalScreen.kt
@@ -1,6 +1,8 @@
 package com.ilsangtech.ilsang.feature.approval
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,20 +10,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.ilsangtech.ilsang.designsystem.R
 import com.ilsangtech.ilsang.designsystem.theme.background
+import com.ilsangtech.ilsang.designsystem.theme.gray400
+import com.ilsangtech.ilsang.designsystem.theme.gray500
+import com.ilsangtech.ilsang.designsystem.theme.heading01
+import com.ilsangtech.ilsang.designsystem.theme.tapRegularTextStyle
 import com.ilsangtech.ilsang.feature.approval.component.ApprovalItem
 
 @Composable
-fun ApprovalScreen(
+internal fun ApprovalScreen(
     approvalViewModel: ApprovalViewModel = hiltViewModel(),
     navigateToProfile: (String) -> Unit
 ) {
@@ -50,19 +62,55 @@ fun ApprovalScreen(
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             item { Spacer(Modifier.statusBarsPadding()) }
-            items(randomChallenges.itemCount) {
-                randomChallenges[it]?.let { randomChallenge ->
-                    ApprovalItem(
-                        challenge = randomChallenge,
-                        onProfileClick = { navigateToProfile(randomChallenge.customerId) },
-                        onLikeButtonClick = {
-                            approvalViewModel.likeChallenge(randomChallenge)
-                        },
-                        onHateButtonClick = {
-                            approvalViewModel.hateChallenge(randomChallenge)
-                        },
-                        onReportButtonClick = { approvalViewModel.reportChallenge(randomChallenge) }
-                    )
+            if (randomChallenges.loadState.refresh is LoadState.NotLoading
+                && randomChallenges.itemCount == 0
+            ) {
+                item {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.icon_approval_empty),
+                                contentDescription = null
+                            )
+                            Text(
+                                text = "기록을 만들어 볼까요?",
+                                style = heading01,
+                                color = gray500
+                            )
+                            Text(
+                                text = "아직 유저들의 인증사진이\n" +
+                                        "등록되지 않았어요!",
+                                style = tapRegularTextStyle,
+                                color = gray400
+                            )
+                        }
+                    }
+                }
+            } else {
+                items(randomChallenges.itemCount) {
+                    randomChallenges[it]?.let { randomChallenge ->
+                        ApprovalItem(
+                            challenge = randomChallenge,
+                            onProfileClick = { navigateToProfile(randomChallenge.customerId) },
+                            onLikeButtonClick = {
+                                approvalViewModel.likeChallenge(randomChallenge)
+                            },
+                            onHateButtonClick = {
+                                approvalViewModel.hateChallenge(randomChallenge)
+                            },
+                            onReportButtonClick = {
+                                approvalViewModel.reportChallenge(
+                                    randomChallenge
+                                )
+                            }
+                        )
+                    }
                 }
             }
         }
