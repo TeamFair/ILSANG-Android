78d4d2f0ecaced1a68bc1739e1ccb01dca704ac7
kimseongwoo <tjddn929@naver.com>
2025-08-18 22:20:44 +0900

    [Feat#81] 지역 랭킹 UI 모델 클래스 구현

---
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/AreaRankUiModel.kt
@@ -0,0 +1,8 @@
+package com.ilsangtech.ilsang.feature.ranking.model
+
+data class AreaRankUiModel(
+    val areaName: String,
+    val rank: Int,
+    val point: Int,
+    val images: List<String>
+)
