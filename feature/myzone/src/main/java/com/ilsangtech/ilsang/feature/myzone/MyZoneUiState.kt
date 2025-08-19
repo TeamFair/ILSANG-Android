657c5765c0fa3d83d0d80833fb5c58fb41db53b4
swkim1 <swkim1@hds-secu.com>
2025-08-19 15:59:08 +0900

    [Feat#83] MyZoneUiState에 내 지역 업데이트 결과 프로퍼티 추가

--- feature/myzone/src/main/java/com/ilsangtech/ilsang/feature/myzone/MyZoneUiState.kt
+++ feature/myzone/src/main/java/com/ilsangtech/ilsang/feature/myzone/MyZoneUiState.kt
@@ -6,5 +6,6 @@ import com.ilsangtech.ilsang.core.model.area.MetroArea
 data class MyZoneUiState(
     val selectedMetroArea: MetroArea?,
     val selectedCommercialArea: CommercialArea?,
-    val areaList: List<MetroArea>
+    val areaList: List<MetroArea>,
+    val isMyZoneUpdateSuccess: Boolean? = null
 )
