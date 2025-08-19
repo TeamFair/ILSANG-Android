c090a6b32f8992151a136dc5721ffeedee17ebaa
swkim1 <swkim1@hds-secu.com>
2025-08-19 17:37:34 +0900

    [Fix#85] HomeTabSuccessData의 largeRewardQuests(큰 보상 퀘스트) 타입 변경

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeTabUiState.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeTabUiState.kt
@@ -3,6 +3,7 @@ package com.ilsangtech.ilsang.feature.home
 import com.ilsangtech.ilsang.core.model.Banner
 import com.ilsangtech.ilsang.core.model.Quest
 import com.ilsangtech.ilsang.core.model.UserRank
+import com.ilsangtech.ilsang.core.model.quest.LargeRewardQuest
 
 sealed interface HomeTabUiState {
     data object Loading : HomeTabUiState
@@ -14,6 +15,6 @@ data class HomeTabSuccessData(
     val banners: List<Banner>,
     val popularQuests: List<Quest>,
     val recommendedQuests: List<Quest>,
-    val largeRewardQuests: Map<String, List<Quest>>,
+    val largeRewardQuests: List<LargeRewardQuest>,
     val topRankUsers: List<UserRank>,
 )
