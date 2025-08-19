132e1e882ccf32f37731fe06ac214ea9a4362d6c
swkim1 <swkim1@hds-secu.com>
2025-08-19 17:21:59 +0900

    [Feat#85] 큰 보상 퀘스트 조회 API DTO 클래스 생성

---
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/Pageable.kt
@@ -0,0 +1,13 @@
+package com.ilsangtech.ilsang.core.network.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pageable(
+    val offset: Int,
+    val pageNumber: Int,
+    val pageSize: Int,
+    val paged: Boolean,
+    val sort: Sort,
+    val unpaged: Boolean
+)
---
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/quest/LargeRewardQuestNetworkModel.kt
@@ -0,0 +1,14 @@
+package com.ilsangtech.ilsang.core.network.model.quest
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LargeRewardQuestNetworkModel(
+    val expireDate: String,
+    val imageId: String,
+    val mainImageId: String,
+    val questId: Int,
+    val rewardPointNetworkModels: List<RewardPointNetworkModel>,
+    val title: String,
+    val writerName: String
+)
