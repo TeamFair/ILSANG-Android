14155eba525568a3ee327e000430b41aa1dba8dc
kimseongwoo <tjddn929@naver.com>
2025-08-21 01:31:36 +0900

    [Fix#85] UserInfoMapper mapper 패키지로 이동, 매핑 구조 수정

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/mapper/UserInfoMapper.kt
@@ -0,0 +1,17 @@
+package com.ilsangtech.ilsang.core.data.user.mapper
+
+import com.ilsangtech.ilsang.core.model.UserInfo
+import com.ilsangtech.ilsang.core.network.model.user.UserInfoResponse
+
+internal fun UserInfoResponse.toUserInfo(): UserInfo {
+    return UserInfo(
+        id = id,
+        channel = channel,
+        commercialAreaCode = commercialAreaCode,
+        email = email,
+        nickname = nickname,
+        profileImageId = profileImageId,
+        status = status,
+        statusUpdatedAt = statusUpdatedAt
+    )
+}
