7804d68ef91ef59ebd178f9428543358d8eba097
swkim1 <swkim1@hds-secu.com>
2025-08-21 10:33:17 +0900

    [Feat#85] RankDataSource 내 전체(포인트 합산) 상위 랭크 유저 조회 함수 생성, 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/datasource/RankDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/datasource/RankDataSource.kt
@@ -1,9 +1,12 @@
 package com.ilsangtech.ilsang.core.data.rank.datasource
 
 import com.ilsangtech.ilsang.core.network.model.rank.TopUsersResponse
+import com.ilsangtech.ilsang.core.network.model.rank.TotalTopRankUsersResponse
 import com.ilsangtech.ilsang.core.network.model.rank.XpTypeRankResponse
 
 interface RankDataSource {
+    suspend fun getTotalTopRankUsers(commercialAreaCode: String): TotalTopRankUsersResponse
+
     suspend fun getTopRankUsers(): TopUsersResponse
     suspend fun getXpTypeRank(
         xpType: String,
--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/datasource/RankDataSourceImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/datasource/RankDataSourceImpl.kt
@@ -2,12 +2,17 @@ package com.ilsangtech.ilsang.core.data.rank.datasource
 
 import com.ilsangtech.ilsang.core.network.api.RankApiService
 import com.ilsangtech.ilsang.core.network.model.rank.TopUsersResponse
+import com.ilsangtech.ilsang.core.network.model.rank.TotalTopRankUsersResponse
 import com.ilsangtech.ilsang.core.network.model.rank.XpTypeRankResponse
 import javax.inject.Inject
 
 class RankDataSourceImpl @Inject constructor(
     private val rankApiService: RankApiService
 ) : RankDataSource {
+    override suspend fun getTotalTopRankUsers(commercialAreaCode: String): TotalTopRankUsersResponse {
+        return rankApiService.getTotalTopRankUsers(commercialAreaCode)
+    }
+
     override suspend fun getTopRankUsers(): TopUsersResponse {
         return rankApiService.getTopRankUsers()
     }
