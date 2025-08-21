cd809597b9598c3d40b34d941c37536618010ada
swkim1 <swkim1@hds-secu.com>
2025-08-21 11:43:08 +0900

    [Fix#85] BannerRepository 내 배너 조회 함수 Flow 타입으로 수정

--- core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/BannerRepository.kt
+++ core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/BannerRepository.kt
@@ -1,7 +1,8 @@
 package com.ilsangtech.ilsang.core.domain
 
 import com.ilsangtech.ilsang.core.model.banner.Banner
+import kotlinx.coroutines.flow.Flow
 
 interface BannerRepository {
-    suspend fun getBanners(): List<Banner>
+    suspend fun getBanners(): Flow<List<Banner>>
 }
