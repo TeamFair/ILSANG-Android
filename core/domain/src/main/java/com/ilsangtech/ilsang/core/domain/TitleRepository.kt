5fa0dbb0be3882c95119cf3306e487fcc3a1d13b
swkim1 <swkim1@hds-secu.com>
2025-08-21 10:51:56 +0900

    [Refactor#85] Title 클래스 title 패키지로 이동, 구조 변경

--- core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/TitleRepository.kt
+++ core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/TitleRepository.kt
@@ -1,6 +1,6 @@
 package com.ilsangtech.ilsang.core.domain
 
-import com.ilsangtech.ilsang.core.model.Title
+import com.ilsangtech.ilsang.core.model.title.Title
 
 interface TitleRepository {
     suspend fun getTitleList(): List<Title>
---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/title/Title.kt
@@ -0,0 +1,7 @@
+package com.ilsangtech.ilsang.core.model.title
+
+data class Title(
+    val name: String,
+    val grade: TitleGrade,
+    val type: TitleType,
+)
