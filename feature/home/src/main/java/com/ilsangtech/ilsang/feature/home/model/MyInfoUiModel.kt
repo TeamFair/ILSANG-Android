c62722edd2ef151fadef9153dffed86a76c6d4ed
swkim1 <swkim1@hds-secu.com>
2025-08-21 13:39:52 +0900

    [Feat#85] 내 정보를 나타내는 MyInfoUiModel 클래스 생성

---
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/model/MyInfoUiModel.kt
@@ -0,0 +1,8 @@
+package com.ilsangtech.ilsang.feature.home.model
+
+data class MyInfoUiModel(
+    val nickname: String,
+    val profileImageId: String,
+    val myCommercialAreaName: String,
+    val isCommericalAreaName: String
+)
