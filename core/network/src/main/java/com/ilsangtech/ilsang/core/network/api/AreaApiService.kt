a13580fcc1fdf67560dc22daee57fa3aea737a22
swkim1 <swkim1@hds-secu.com>
2025-08-21 17:53:10 +0900

    [Fix/#89] 광역 지역 전체 조회 API 시그니처 수정 및 라우트 변경

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/AreaApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/AreaApiService.kt
@@ -1,9 +1,9 @@
 package com.ilsangtech.ilsang.core.network.api
 
-import com.ilsangtech.ilsang.core.network.model.area.AreaListResponse
+import com.ilsangtech.ilsang.core.network.model.area.MetroAreaNetworkModel
 import retrofit2.http.GET
 
 interface AreaApiService {
-    @GET("api/v1/area")
-    suspend fun getAreaList(): AreaListResponse
+    @GET("api/v1/area/metro")
+    suspend fun getMetroAreaList(): List<MetroAreaNetworkModel>
 }
