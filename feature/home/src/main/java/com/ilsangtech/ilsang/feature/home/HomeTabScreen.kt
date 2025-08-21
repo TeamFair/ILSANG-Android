7fc6c8833722160f1009925b136f572b2c3889a7
swkim1 <swkim1@hds-secu.com>
2025-08-21 14:11:16 +0900

    [Fix#85] HomeTabScreen Preview 함수 적용 가능한 구조로 수정

--- feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeTabScreen.kt
+++ feature/home/src/main/java/com/ilsangtech/ilsang/feature/home/HomeTabScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
@@ -16,20 +17,33 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ilsangtech.ilsang.core.model.NewQuestType
+import com.ilsangtech.ilsang.core.model.RewardPoint
+import com.ilsangtech.ilsang.core.model.UserRank
 import com.ilsangtech.ilsang.core.model.banner.Banner
+import com.ilsangtech.ilsang.core.model.quest.LargeRewardQuest
+import com.ilsangtech.ilsang.core.model.quest.PopularQuest
+import com.ilsangtech.ilsang.core.model.quest.QuestDetail
+import com.ilsangtech.ilsang.core.model.quest.RecommendedQuest
+import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.TitleGrade
+import com.ilsangtech.ilsang.core.model.title.TitleType
 import com.ilsangtech.ilsang.core.ui.quest.bottomsheet.QuestBottomSheet
+import com.ilsangtech.ilsang.designsystem.theme.background
 import com.ilsangtech.ilsang.feature.home.component.BannerContent
 import com.ilsangtech.ilsang.feature.home.component.HomeTabHeader
 import com.ilsangtech.ilsang.feature.home.component.LargeRewardQuestContent
 import com.ilsangtech.ilsang.feature.home.component.PopularQuestsContent
 import com.ilsangtech.ilsang.feature.home.component.RecommendedQuestsContent
 import com.ilsangtech.ilsang.feature.home.component.UserRankContent
+import com.ilsangtech.ilsang.feature.home.model.HomeTabSuccessData
 import com.ilsangtech.ilsang.feature.home.model.HomeTabUiState
+import com.ilsangtech.ilsang.feature.home.model.MyInfoUiModel
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeTabScreen(
+internal fun HomeTabScreen(
     homeViewModel: HomeViewModel = hiltViewModel(),
     navigateToQuestTab: () -> Unit,
     navigateToMyTab: () -> Unit,
@@ -40,9 +54,43 @@ fun HomeTabScreen(
     onMyZoneClick: () -> Unit,
     onIsZoneClick: () -> Unit
 ) {
-    val selectedQuest by homeViewModel.selectedQuest.collectAsStateWithLifecycle()
     val homeTabUiState by homeViewModel.homeTabUiState.collectAsStateWithLifecycle()
+    val selectedQuest by homeViewModel.selectedQuest.collectAsStateWithLifecycle()
 
+    HomeTabScreen(
+        homeTabUiState = homeTabUiState,
+        selectedQuest = selectedQuest,
+        navigateToQuestTab = navigateToQuestTab,
+        navigateToMyTab = navigateToMyTab,
+        navigateToSubmit = navigateToSubmit,
+        navigateToRankingTab = navigateToRankingTab,
+        navigateToProfile = navigateToProfile,
+        onBannerClick = onBannerClick,
+        onMyZoneClick = onMyZoneClick,
+        onIsZoneClick = onIsZoneClick,
+        onSelectQuest = homeViewModel::selectQuest,
+        onUnselectQuest = homeViewModel::unselectQuest,
+        onFavoriteClick = homeViewModel::updateQuestFavoriteStatus
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeTabScreen(
+    homeTabUiState: HomeTabUiState,
+    selectedQuest: QuestDetail?,
+    navigateToQuestTab: () -> Unit,
+    navigateToMyTab: () -> Unit,
+    navigateToSubmit: (String) -> Unit,
+    navigateToRankingTab: () -> Unit,
+    navigateToProfile: (String) -> Unit,
+    onBannerClick: (Banner) -> Unit,
+    onMyZoneClick: () -> Unit,
+    onIsZoneClick: () -> Unit,
+    onSelectQuest: (Int) -> Unit,
+    onUnselectQuest: () -> Unit,
+    onFavoriteClick: () -> Unit
+) {
     val coroutineScope = rememberCoroutineScope()
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
@@ -51,15 +99,15 @@ fun HomeTabScreen(
             QuestBottomSheet(
                 quest = quest,
                 bottomSheetState = bottomSheetState,
-                onDismiss = homeViewModel::unselectQuest,
-                onFavoriteClick = homeViewModel::updateQuestFavoriteStatus,
+                onDismiss = onUnselectQuest,
+                onFavoriteClick = onFavoriteClick,
                 onMissionImageClick = {
                     //TODO 퀘스트 인증 예시 화면으로 이동
                 },
                 onApproveButtonClick = {
                     coroutineScope.launch {
                         bottomSheetState.hide()
-                        homeViewModel.unselectQuest()
+                        onUnselectQuest()
                         //TODO 제출 화면으로 이동 구현
                     }
                 }
@@ -70,11 +118,11 @@ fun HomeTabScreen(
     Surface(
         modifier = Modifier
             .fillMaxSize()
-            .navigationBarsPadding()
+            .navigationBarsPadding(),
+        color = background
     ) {
         if (homeTabUiState is HomeTabUiState.Success) {
-            val (userInfo, banners, popularQuests, recommendedQuests, largeRewardQuests, topRankUsers) =
-                (homeTabUiState as HomeTabUiState.Success).data
+            val (userInfo, banners, popularQuests, recommendedQuests, largeRewardQuests, topRankUsers) = homeTabUiState.data
             LazyColumn {
                 item {
                     HomeTabHeader(
@@ -96,7 +144,7 @@ fun HomeTabScreen(
                 item {
                     PopularQuestsContent(
                         popularQuests = popularQuests,
-                        onPopularQuestClick = homeViewModel::selectQuest,
+                        onPopularQuestClick = onSelectQuest,
                     )
                 }
                 item { Spacer(Modifier.height(36.dp)) }
@@ -104,14 +152,15 @@ fun HomeTabScreen(
                     RecommendedQuestsContent(
                         userNickname = userInfo.nickname,
                         recommendedQuests = recommendedQuests,
-                        onRecommendedQuestClick = homeViewModel::selectQuest
+                        onRecommendedQuestClick = onSelectQuest,
                     )
                 }
                 item { Spacer(Modifier.height(36.dp)) }
                 item {
                     LargeRewardQuestContent(
+                        modifier = Modifier.padding(horizontal = 20.dp),
                         largeRewardQuests = largeRewardQuests,
-                        onQuestClick = homeViewModel::selectQuest,
+                        onQuestClick = onSelectQuest,
                         onMoreButtonClick = navigateToQuestTab
                     )
                 }
@@ -129,11 +178,169 @@ fun HomeTabScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 private fun HomeTabScreenPreview() {
+    val homeTabUiState = HomeTabUiState.Success(
+        HomeTabSuccessData(
+            myInfo = MyInfoUiModel(
+                nickname = "User",
+                profileImageId = "default_profile",
+                myCommercialAreaName = "서현역",
+                isCommericalAreaName = "서현역"
+            ),
+            banners = listOf(
+                Banner(
+                    id = 1,
+                    bannerImageId = "sample_banner_1.png",
+                    description = "Sample Banner 1 Description",
+                    navigationTitle = "Go to Sample 1",
+                    title = "Sample Banner 1",
+                    useYn = true
+                ),
+                Banner(
+                    id = 2,
+                    bannerImageId = "sample_banner_2.png",
+                    description = "Sample Banner 2 Description",
+                    navigationTitle = "Go to Sample 2",
+                    title = "Sample Banner 2",
+                    useYn = true
+                )
+            ),
+            popularQuests = listOf(
+                PopularQuest(
+                    questId = 1,
+                    expireDate = "2023-12-31",
+                    imageId = "imageId1",
+                    mainImageId = "mainImageId1",
+                    questType = NewQuestType.Event,
+                    title = "첫 번째 인기 퀘스트",
+                    writerName = "작가 1"
+                ),
+                PopularQuest(
+                    questId = 2,
+                    expireDate = "2024-01-15",
+                    imageId = "imageId2",
+                    mainImageId = "mainImageId2",
+                    questType = NewQuestType.Repeat.Daily,
+                    title = "두 번째 인기 퀘스트 - 이것은 매우 긴 제목으로 두 줄을 넘을 수 있습니다. 확인해 보세요.",
+                    writerName = "작가 2"
+                ),
+                PopularQuest(
+                    questId = 3,
+                    expireDate = "2024-02-28",
+                    imageId = "imageId3",
+                    mainImageId = "mainImageId3",
+                    questType = NewQuestType.Normal,
+                    title = "세 번째 인기 퀘스트",
+                    writerName = "작가 3"
+                ),
+                PopularQuest(
+                    questId = 4,
+                    expireDate = "2024-03-10",
+                    imageId = "imageId4",
+                    mainImageId = "mainImageId4",
+                    questType = NewQuestType.Event,
+                    title = "네 번째 인기 퀘스트",
+                    writerName = "작가 4"
+                )
+            ),
+            recommendedQuests = List(8) {
+                RecommendedQuest(
+                    questId = 1,
+                    imageId = "sample_image_id",
+                    mainImageId = "sample_main_image_id",
+                    title = "Sample Quest Title",
+                    writerName = "Sample Writer"
+                )
+            },
+            largeRewardQuests = listOf(
+                LargeRewardQuest(
+                    questId = 1,
+                    expireDate = "2023-12-31",
+                    imageId = "sample_image_1",
+                    mainImageId = "sample_main_image_1",
+                    rewards = listOf(
+                        RewardPoint.Metro(5),
+                        RewardPoint.Commercial(10),
+                        RewardPoint.Contribute(15)
+                    ),
+                    title = "Sample Quest 1",
+                    writerName = "Writer 1"
+                ),
+                LargeRewardQuest(
+                    questId = 2,
+                    expireDate = "2024-01-15",
+                    imageId = "sample_image_2",
+                    mainImageId = "sample_main_image_2",
+                    rewards = listOf(
+                        RewardPoint.Metro(3),
+                        RewardPoint.Commercial(8),
+                        RewardPoint.Contribute(12)
+                    ),
+                    title = "Sample Quest 2",
+                    writerName = "Writer 2"
+                ),
+                LargeRewardQuest(
+                    questId = 3,
+                    expireDate = "2024-01-15",
+                    imageId = "sample_image_3",
+                    mainImageId = "sample_main_image_3",
+                    rewards = listOf(
+                        RewardPoint.Metro(3),
+                        RewardPoint.Commercial(8),
+                        RewardPoint.Contribute(12)
+                    ),
+                    title = "Sample Quest 2",
+                    writerName = "Writer 2"
+                )
+            ),
+            topRankUsers = listOf(
+                UserRank(
+                    userId = "1",
+                    nickName = "User 1",
+                    point = 100,
+                    profileImageId = "image1",
+                    rank = 1,
+                    title = Title(
+                        name = "모두의 시선을 받는 자",
+                        grade = TitleGrade.Legend,
+                        type = TitleType.Metro
+                    )
+                ),
+                UserRank(
+                    userId = "2",
+                    nickName = "User 2",
+                    point = 90,
+                    profileImageId = "image2",
+                    rank = 2,
+                    title = Title(
+                        name = "모두의 시선을 받는 자자자자 ",
+                        grade = TitleGrade.Rare,
+                        type = TitleType.Commercial
+                    )
+                ),
+                UserRank(
+                    userId = "3",
+                    nickName = "User 3",
+                    point = 80,
+                    profileImageId = "image3",
+                    rank = 3,
+                    title = Title(
+                        name = "Title 3",
+                        grade = TitleGrade.Standard,
+                        type = TitleType.Contribution
+                    )
+                )
+            )
+        )
+    )
+
+
     HomeTabScreen(
-        homeViewModel = hiltViewModel(),
+        homeTabUiState = homeTabUiState,
+        selectedQuest = null,
         navigateToQuestTab = {},
         navigateToMyTab = {},
         navigateToSubmit = {},
@@ -141,6 +348,9 @@ private fun HomeTabScreenPreview() {
         navigateToProfile = {},
         onBannerClick = {},
         onMyZoneClick = {},
-        onIsZoneClick = {}
+        onIsZoneClick = {},
+        onSelectQuest = {},
+        onUnselectQuest = {},
+        onFavoriteClick = {}
     )
 }
