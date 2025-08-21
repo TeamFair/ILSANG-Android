1713975c1fe7c4ef591dd239b88cf60e09a15509
swkim1 <swkim1@hds-secu.com>
2025-08-21 17:08:03 +0900

    [Fix#87] UserDataSource에 있는 로그인, 로그아웃, 회원탈퇴 함수 제거

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/datasource/UserDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/datasource/UserDataSource.kt
@@ -1,9 +1,5 @@
 package com.ilsangtech.ilsang.core.data.user.datasource
 
-import com.ilsangtech.ilsang.core.network.model.auth.LogoutResponse
-import com.ilsangtech.ilsang.core.network.model.auth.OAuthLoginRequest
-import com.ilsangtech.ilsang.core.network.model.auth.OAuthLoginResponse
-import com.ilsangtech.ilsang.core.network.model.auth.WithdrawalResponse
 import com.ilsangtech.ilsang.core.network.model.user.NicknameUpdateResponse
 import com.ilsangtech.ilsang.core.network.model.user.UserImageDeleteResponse
 import com.ilsangtech.ilsang.core.network.model.user.UserImageUpdateResponse
@@ -12,13 +8,7 @@ import com.ilsangtech.ilsang.core.network.model.user.UserTitleUpdateResponse
 import com.ilsangtech.ilsang.core.network.model.user.UserXpStatsResponse
 
 interface UserDataSource {
-    suspend fun login(loginRequest: OAuthLoginRequest): OAuthLoginResponse
-
     suspend fun getUserInfo(userId: String?): UserInfoResponse
-
-    suspend fun logout(): LogoutResponse
-
-    suspend fun withdraw(): WithdrawalResponse
 
     suspend fun getUserXpStats(customerId: String?): UserXpStatsResponse
 
--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/datasource/UserDataSourceImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/datasource/UserDataSourceImpl.kt
@@ -1,11 +1,6 @@
 package com.ilsangtech.ilsang.core.data.user.datasource
 
-import com.ilsangtech.ilsang.core.network.api.AuthApiService
 import com.ilsangtech.ilsang.core.network.api.UserApiService
-import com.ilsangtech.ilsang.core.network.model.auth.LogoutResponse
-import com.ilsangtech.ilsang.core.network.model.auth.OAuthLoginRequest
-import com.ilsangtech.ilsang.core.network.model.auth.OAuthLoginResponse
-import com.ilsangtech.ilsang.core.network.model.auth.WithdrawalResponse
 import com.ilsangtech.ilsang.core.network.model.user.NicknameUpdateRequest
 import com.ilsangtech.ilsang.core.network.model.user.NicknameUpdateResponse
 import com.ilsangtech.ilsang.core.network.model.user.UserImageDeleteResponse
@@ -18,23 +13,10 @@ import com.ilsangtech.ilsang.core.network.model.user.UserXpStatsResponse
 import javax.inject.Inject
 
 class UserDataSourceImpl @Inject constructor(
-    private val authApiService: AuthApiService,
     private val userApiService: UserApiService
 ) : UserDataSource {
-    override suspend fun login(loginRequest: OAuthLoginRequest): OAuthLoginResponse {
-        return authApiService.oAuthLogin(loginRequest)
-    }
-
     override suspend fun getUserInfo(userId: String?): UserInfoResponse {
         return userApiService.getUserInfo(userId)
-    }
-
-    override suspend fun logout(): LogoutResponse {
-        return authApiService.logout()
-    }
-
-    override suspend fun withdraw(): WithdrawalResponse {
-        return authApiService.withdraw()
     }
 
     override suspend fun getUserXpStats(customerId: String?): UserXpStatsResponse {
