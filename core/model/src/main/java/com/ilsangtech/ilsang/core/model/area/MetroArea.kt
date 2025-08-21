67f9d6e43037c999b097b9d165de4508a4cdb768
kimseongwoo <tjddn929@naver.com>
2025-08-21 21:37:15 +0900

    [Fix/#89] 광역, 상권 지역 model 클래스 프로퍼티에 images(List<String>) 추가

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/area/MetroArea.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/area/MetroArea.kt
@@ -3,5 +3,6 @@ package com.ilsangtech.ilsang.core.model.area
 data class MetroArea(
     val code: String,
     val areaName: String,
-    val commercialAreaList: List<CommercialArea>
+    val commercialAreaList: List<CommercialArea>,
+    val images: List<String>
 )
