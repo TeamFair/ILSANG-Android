126b80b3d1da253f42f6175282f76fdf650f132c
swkim1 <swkim1@hds-secu.com>
2025-08-21 11:26:31 +0900

    [Fix#85] BannerApiService 내 배너 리스트 조회 API 요청 함수 변경

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/BannerApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/BannerApiService.kt
@@ -1,15 +1,17 @@
 package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.banner.BannerResponse
-import com.ilsangtech.ilsang.core.network.model.banner.BannerSearchRequest
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BannerApiService {
-    @GET("open/v1/banners")
+    @GET("api/v1/banner")
     suspend fun getBanners(
-        @Query("searchRequest") searchRequest: BannerSearchRequest,
-        @Query("page") page: Int,
-        @Query("size") size: Int
+        @Query("title") title: String? = null,
+        @Query("description") description: String? = null,
+        @Query("useYn") userYn: Boolean? = null,
+        @Query("page") page: Int? = null,
+        @Query("size") size: Int? = null,
+        @Query("sort") sort: List<String>? = null
     ): BannerResponse
 }
