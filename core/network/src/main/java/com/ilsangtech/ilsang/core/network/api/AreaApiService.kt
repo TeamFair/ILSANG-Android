0d949a709aeaecbd8082bb16b092b5242b501c90
kimseongwoo <tjddn929@naver.com>
2025-08-21 21:20:33 +0900

    [Feat/#89] 광역 지역 단일 조회 API 요청 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/AreaApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/AreaApiService.kt
@@ -2,8 +2,14 @@ package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.area.MetroAreaNetworkModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface AreaApiService {
     @GET("api/v1/area/metro")
     suspend fun getMetroAreaList(): List<MetroAreaNetworkModel>
+
+    @GET("api/v1/area/metro/{metroAreaCode}")
+    suspend fun getMetroAreaDetail(
+        @Path("metroAreaCode") metroAreaCode: String
+    ): MetroAreaNetworkModel
 }
