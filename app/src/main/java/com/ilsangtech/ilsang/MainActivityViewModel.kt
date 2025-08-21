d87f536e1191b567704773370ce63141753a6381
swkim1 <swkim1@hds-secu.com>
2025-08-21 15:28:04 +0900

    [Fix#87] authRepository를 통해 로그인을 수행할 수 있도록 MainActivityViewModel 클래스 수정

--- app/src/main/java/com/ilsangtech/ilsang/MainActivityViewModel.kt
+++ app/src/main/java/com/ilsangtech/ilsang/MainActivityViewModel.kt
@@ -2,22 +2,40 @@ package com.ilsangtech.ilsang
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ilsangtech.ilsang.core.domain.AuthRepository
 import com.ilsangtech.ilsang.core.domain.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    private val userRepository: UserRepository,
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository
 ) : ViewModel() {
-    private val _isLoggedIn = MutableStateFlow<Boolean?>(null)
-    val isLoggedIn = _isLoggedIn.asStateFlow()
+    private val loginTrigger = MutableSharedFlow<Unit>(replay = 1)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val isLoggedIn = loginTrigger.flatMapLatest {
+        userRepository.getMyInfo().map { true }
+            .catch { emit(false) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = null
+            )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = null
+    )
 
     val shouldShowOnBoarding = userRepository.shouldShowOnBoarding.stateIn(
         scope = viewModelScope,
@@ -27,23 +45,14 @@ class MainActivityViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            try {
-                userRepository.updateMyInfo()
-                _isLoggedIn.update { true }
-            } catch (_: Exception) {
-                _isLoggedIn.update { false }
-            }
+            loginTrigger.emit(Unit)
         }
     }
 
     fun login(idToken: String) {
         viewModelScope.launch {
-            runCatching {
-                userRepository.login(idToken)
-            }.onSuccess {
-                _isLoggedIn.update { true }
-            }.onFailure {
-                _isLoggedIn.update { false }
+            authRepository.login(idToken).onSuccess {
+                loginTrigger.emit(Unit)
             }
         }
     }
