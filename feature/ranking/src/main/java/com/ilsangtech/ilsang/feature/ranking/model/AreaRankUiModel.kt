f3c02692e29b04844b0c8379dccb0a93fa3df760
swkim1 <swkim1@hds-secu.com>
2025-08-22 11:25:24 +0900

    [Feat/#93] 일상지역, 일상존 랭킹 Model 클래스를 AreaRankUiModel로 매핑하는 함수 구현

--- feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/AreaRankUiModel.kt
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/AreaRankUiModel.kt
@@ -1,8 +1,29 @@
 package com.ilsangtech.ilsang.feature.ranking.model
 
+import com.ilsangtech.ilsang.core.model.rank.CommercialAreaRank
+import com.ilsangtech.ilsang.core.model.rank.MetroAreaRank
+
 data class AreaRankUiModel(
     val areaName: String,
     val rank: Int,
     val point: Int,
     val images: List<String>
 )
+
+internal fun MetroAreaRank.toAreaRankUiModel(): AreaRankUiModel {
+    return AreaRankUiModel(
+        areaName = areaName,
+        rank = 0, //TODO 수정 필요
+        point = point,
+        images = images
+    )
+}
+
+internal fun CommercialAreaRank.toAreaRankUiModel(): AreaRankUiModel {
+    return AreaRankUiModel(
+        areaName = areaName,
+        rank = 0, //TODO 수정 필요
+        point = point,
+        images = images
+    )
+}
