697cefb03293a13457123f987a32a1fc37b3c21d
swkim1 <swkim1@hds-secu.com>
2025-08-22 11:24:33 +0900

    [Feat/#93] UserRank를 UserRankUiModel로 매핑하는 함수 구현

--- feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/UserRankUiModel.kt
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/UserRankUiModel.kt
@@ -1,5 +1,6 @@
 package com.ilsangtech.ilsang.feature.ranking.model
 
+import com.ilsangtech.ilsang.core.model.rank.UserRank
 import com.ilsangtech.ilsang.core.model.title.TitleGrade
 
 data class UserRankUiModel(
@@ -11,3 +12,15 @@ data class UserRankUiModel(
     val titleName: String,
     val titleGrade: TitleGrade
 )
+
+internal fun UserRank.toUserRankUiModel(): UserRankUiModel {
+    return UserRankUiModel(
+        userId = userId,
+        profileImageId = profileImageId,
+        nickname = nickName,
+        point = point,
+        rank = rank,
+        titleName = title.name,
+        titleGrade = title.grade
+    )
+}
