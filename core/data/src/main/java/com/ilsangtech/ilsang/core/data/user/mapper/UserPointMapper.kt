b1f888d76a8b50b80e0a01d980c1313d09f209fd
kimseongwoo <tjddn929@naver.com>
2025-08-24 00:51:06 +0900

    [Feat/#97] 많이 기여한 일상존 포인트 모델 클래스로 매핑하는 함수 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/mapper/UserPointMapper.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/mapper/UserPointMapper.kt
@@ -1,7 +1,10 @@
 package com.ilsangtech.ilsang.core.data.user.mapper
 
+import com.ilsangtech.ilsang.core.model.user.TopCommercialArea
+import com.ilsangtech.ilsang.core.model.user.UserCommercialPoint
 import com.ilsangtech.ilsang.core.model.user.UserPoint
 import com.ilsangtech.ilsang.core.model.user.UserPointSummary
+import com.ilsangtech.ilsang.core.network.model.user.UserCommercialPointResponse
 import com.ilsangtech.ilsang.core.network.model.user.UserPointResponse
 import com.ilsangtech.ilsang.core.network.model.user.UserPointSummaryResponse
 
@@ -19,5 +22,21 @@ internal fun UserPointSummaryResponse.toUserPointSummary(): UserPointSummary {
         topCommercialAreaCode = topCommercialAreaCode,
         topContributionPoint = topContributionPoint,
         topMetroAreaCode = topMetroAreaCode
+    )
+}
+
+internal fun UserCommercialPointResponse.toUserCommercialPoint(): UserCommercialPoint {
+    return UserCommercialPoint(
+        topCommercialArea = TopCommercialArea(
+            commercialAreaCode = topCommercialArea.commercialAreaCode,
+            ownerContributionPercent = topCommercialArea.ownerContributionPercent,
+            point = topCommercialArea.point
+        ),
+        totalOwnerContributions = totalOwnerContributions.map { contribution ->
+            com.ilsangtech.ilsang.core.model.user.TotalOwnerContribution(
+                commercialAreaCode = contribution.commercialAreaCode,
+                point = contribution.point
+            )
+        }
     )
 }
