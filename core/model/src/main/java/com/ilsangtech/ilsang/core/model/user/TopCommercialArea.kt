090478ade9f065807f76c0e9e7209a443ae525d6
kimseongwoo <tjddn929@naver.com>
2025-08-24 00:47:57 +0900

    [Feat/#97] 많이 기여한 일상존 포인트 모델 클래스 구현

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/user/TopCommercialArea.kt
@@ -0,0 +1,7 @@
+package com.ilsangtech.ilsang.core.model.user
+
+data class TopCommercialArea(
+    val commercialAreaCode: String,
+    val ownerContributionPercent: Int,
+    val point: Int
+)
---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/user/TotalOwnerContribution.kt
@@ -0,0 +1,6 @@
+package com.ilsangtech.ilsang.core.model.user
+
+data class TotalOwnerContribution(
+    val commercialAreaCode: String,
+    val point: Int
+)
