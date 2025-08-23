1bf48174ccbdab0ec7c0b3bea90b1dbc354cae19
kimseongwoo <tjddn929@naver.com>
2025-08-23 20:07:23 +0900

    [Fix/#95] 큰 보상 퀘스트, 추천 퀘스트, 인기 퀘스트 조회 API 응답 DTO 클래스 수정

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/quest/PopularQuestResponse.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/quest/PopularQuestResponse.kt
@@ -1,20 +1,13 @@
 package com.ilsangtech.ilsang.core.network.model.quest
 
-import com.ilsangtech.ilsang.core.network.model.Pageable
-import com.ilsangtech.ilsang.core.network.model.Sort
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PopularQuestResponse(
     val content: List<PopularQuestNetworkModel>,
-    val first: Boolean,
-    val last: Boolean,
-    val number: Int,
+    val isLast: Boolean,
+    val page: Int,
     val size: Int,
     val totalElements: Int,
-    val totalPages: Int,
-    val sort: Sort,
-    val pageable: Pageable,
-    val numberOfElements: Int,
-    val empty: Boolean
+    val totalPages: Int
 )
