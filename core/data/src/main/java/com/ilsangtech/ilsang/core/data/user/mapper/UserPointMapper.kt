b87b48c9769bfaadc7f5f51e9e8375cacb18fbf8
kimseongwoo <tjddn929@naver.com>
2025-08-24 00:29:17 +0900

    [Feat/#97] UserPointSummaryResponse를 UserPointSummary로 매핑하는 함수 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/mapper/UserPointMapper.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/user/mapper/UserPointMapper.kt
@@ -1,7 +1,9 @@
 package com.ilsangtech.ilsang.core.data.user.mapper
 
 import com.ilsangtech.ilsang.core.model.user.UserPoint
+import com.ilsangtech.ilsang.core.model.user.UserPointSummary
 import com.ilsangtech.ilsang.core.network.model.user.UserPointResponse
+import com.ilsangtech.ilsang.core.network.model.user.UserPointSummaryResponse
 
 internal fun UserPointResponse.toUserPoint(): UserPoint {
     return UserPoint(
@@ -9,5 +11,13 @@ internal fun UserPointResponse.toUserPoint(): UserPoint {
         completedQuestCount = completedQuestCount,
         contributionPoint = contributionPoint,
         metroAreaPoint = metroAreaPoint
+    )
+}
+
+internal fun UserPointSummaryResponse.toUserPointSummary(): UserPointSummary {
+    return UserPointSummary(
+        topCommercialAreaCode = topCommercialAreaCode,
+        topContributionPoint = topContributionPoint,
+        topMetroAreaCode = topMetroAreaCode
     )
 }
