b9020f02962326844582da725bee72fe5a845bd2
kimseongwoo <tjddn929@naver.com>
2025-08-23 09:27:16 +0900

    [Feat/#95] 유형별 퀘스트 페이징 소스 클래스 구현

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/quest/TypedQuestPagingSource.kt
@@ -0,0 +1,49 @@
+package com.ilsangtech.ilsang.core.data.quest
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.ilsangtech.ilsang.core.network.api.QuestApiService
+import com.ilsangtech.ilsang.core.network.model.quest.TypedQuestNetworkModel
+
+class TypedQuestPagingSource(
+    private val questApiService: QuestApiService,
+    private val commercialAreaCode: String,
+    private val type: String?,
+    private val repeatFrequency: String?,
+    private val orderRewardDesc: Boolean?,
+    private val favoriteYn: Boolean?,
+    private val completeYn: Boolean
+) : PagingSource<Int, TypedQuestNetworkModel>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TypedQuestNetworkModel> {
+        return try {
+            val pageNumber = params.key ?: 0
+            val response = questApiService.getTypedQuests(
+                commercialAreaCode = commercialAreaCode,
+                type = type,
+                repeatFrequency = repeatFrequency,
+                orderRewardDesc = orderRewardDesc,
+                favoriteYn = favoriteYn,
+                completeYn = completeYn,
+                page = pageNumber,
+                size = params.loadSize,
+            )
+            val prevKey = if (pageNumber == 0) null else pageNumber - 1
+            val nextKey = if (response.isLast) null else pageNumber + 1
+
+            LoadResult.Page(
+                data = response.content,
+                prevKey = prevKey,
+                nextKey = nextKey
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, TypedQuestNetworkModel>): Int? {
+        return state.anchorPosition?.let {
+            state.closestPageToPosition(it)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(it)?.nextKey?.minus(1)
+        }
+    }
+}
