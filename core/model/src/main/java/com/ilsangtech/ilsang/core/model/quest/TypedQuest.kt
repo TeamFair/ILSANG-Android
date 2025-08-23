548ccab191aed980e8ac4c3a12111b28d8fe1f05
kimseongwoo <tjddn929@naver.com>
2025-08-23 09:35:55 +0900

    [Feat/#95] TypedQuest(유형별 퀘스트) 도메인 클래스 구현

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/quest/TypedQuest.kt
@@ -0,0 +1,16 @@
+package com.ilsangtech.ilsang.core.model.quest
+
+import com.ilsangtech.ilsang.core.model.NewQuestType
+import com.ilsangtech.ilsang.core.model.RewardPoint
+
+data class TypedQuest(
+    val questId: Int,
+    val expireDate: String,
+    val favoriteYn: Boolean,
+    val imageId: String,
+    val mainImageId: String,
+    val rewards: List<RewardPoint>,
+    val title: String,
+    val writerName: String,
+    val questType: NewQuestType
+)
