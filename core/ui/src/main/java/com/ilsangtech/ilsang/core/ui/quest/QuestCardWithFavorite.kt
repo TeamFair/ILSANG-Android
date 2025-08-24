34552b4ffb9811a5ea5cbc552e126135429d6ea7
kimseongwoo <tjddn929@naver.com>
2025-08-24 16:35:45 +0900

    [Fix/#103] 퀘스트 탭에 사용되는 퀘스트 카드 UI에 보상 포인트가 잘리지 않도록 수정

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/QuestCardWithFavorite.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/QuestCardWithFavorite.kt
@@ -1,11 +1,13 @@
 package com.ilsangtech.ilsang.core.ui.quest
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,10 +32,10 @@ fun QuestCardWithFavorite(
         modifier = modifier.fillMaxWidth(),
         onClick = onClick
     ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Box(modifier = Modifier.fillMaxWidth()) {
             DefaultQuestContent(
                 modifier = Modifier
-                    .weight(1f)
+                    .fillMaxWidth()
                     .padding(
                         horizontal = 23.dp,
                         vertical = 20.dp
@@ -50,6 +52,7 @@ fun QuestCardWithFavorite(
             )
             Icon(
                 modifier = modifier
+                    .align(Alignment.TopEnd)
                     .padding(
                         top = 16.dp,
                         end = 16.dp
