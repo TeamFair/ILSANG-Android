92a31f750550d994513c0ea68bdc9aed7355892d
kimseongwoo <tjddn929@naver.com>
2025-08-25 23:16:43 +0900

    [Fix/#110] 홈 탭 배너 조회 API 요청 형태(엔드포인트, 쿼리 파라미터), 응답 DTO 수정

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/BannerApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/BannerApiService.kt
@@ -1,17 +1,9 @@
 package com.ilsangtech.ilsang.core.network.api
 
-import com.ilsangtech.ilsang.core.network.model.banner.BannerResponse
+import com.ilsangtech.ilsang.core.network.model.banner.BannerNetworkModel
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface BannerApiService {
-    @GET("api/v1/banner")
-    suspend fun getBanners(
-        @Query("title") title: String? = null,
-        @Query("description") description: String? = null,
-        @Query("useYn") userYn: Boolean? = null,
-        @Query("page") page: Int? = null,
-        @Query("size") size: Int? = null,
-        @Query("sort") sort: List<String>? = null
-    ): BannerResponse
+    @GET("api/v1/banner/user")
+    suspend fun getBanners(): List<BannerNetworkModel>
 }
