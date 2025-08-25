998a56f5c2b7e74d06ce176e3be85af202f7efa3
swkim1 <swkim1@hds-secu.com>
2025-08-25 17:06:57 +0900

    [Feat/#109] ApprovalViewModel 클래스에 미션 이력에 대한 좋아요, 싫어요, 신고하기 기능 함수 적용

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalViewModel.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalViewModel.kt
@@ -5,122 +5,73 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import androidx.paging.map
 import com.ilsangtech.ilsang.core.domain.AreaRepository
-import com.ilsangtech.ilsang.core.domain.ChallengeRepository
-import com.ilsangtech.ilsang.core.domain.EmojiRepository
 import com.ilsangtech.ilsang.core.domain.MissionRepository
-import com.ilsangtech.ilsang.core.model.Emoji
-import com.ilsangtech.ilsang.core.model.EmojiType
-import com.ilsangtech.ilsang.core.model.RandomChallenge
+import com.ilsangtech.ilsang.feature.approval.model.RandomMissionHistoryUiModel
 import com.ilsangtech.ilsang.feature.approval.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ApprovalViewModel @Inject constructor(
-    missionRepository: MissionRepository,
     areaRepository: AreaRepository,
-    private val challengeRepository: ChallengeRepository,
-    private val emojiRepository: EmojiRepository
+    private val missionRepository: MissionRepository
 ) : ViewModel() {
-    private val emojiMap = MutableStateFlow<Map<String, Emoji>>(mapOf())
+    private val _missionHistoryRefreshTrigger = MutableSharedFlow<Unit>(replay = 1)
+    val missionHistoryRefreshTrigger = _missionHistoryRefreshTrigger.asSharedFlow()
 
-    val randomMissionHistories =
-        missionRepository.getRandomMissionHistory()
-            .map { pagingData ->
-                pagingData.map { randomMissionHistory ->
-                    val commercialArea =
-                        areaRepository.getCommercialArea(randomMissionHistory.commercialAreaCode)
-                    randomMissionHistory.toUiModel(commercialArea.areaName)
-                }
-            }.cachedIn(viewModelScope)
+    val randomMissionHistories = missionRepository.getRandomMissionHistory().map { pagingData ->
+        pagingData.map { randomMissionHistory ->
+            val commercialArea =
+                areaRepository.getCommercialArea(randomMissionHistory.commercialAreaCode)
+            randomMissionHistory.toUiModel(commercialArea.areaName)
+        }
+    }.cachedIn(viewModelScope)
 
-    private val _isReportSuccess = MutableStateFlow<Boolean?>(null)
-    val isReportSuccess = _isReportSuccess.asStateFlow()
-
-    fun likeChallenge(challenge: RandomChallenge) {
+    fun likeChallenge(missionHistory: RandomMissionHistoryUiModel) {
         viewModelScope.launch {
-            val emoji = emojiMap.value[challenge.challengeId] ?: challenge.emoji
-            if (emoji?.isLike == true) {
-                runCatching {
-                    emojiRepository.deleteEmoji(challenge.emoji!!.likeId!!)
-                }.onSuccess {
-                    emojiMap.update {
-                        it + (challenge.challengeId to emoji.copy(
-                            isLike = false,
-                            likeId = null
-                        ))
-                    }
-                }
+            val missionHistoryId = missionHistory.missionHistoryId
+            val emojiTypes = missionHistory.currentUserEmojis
+
+            val result = if (!emojiTypes.contains("LIKE")) {
+                missionRepository.likeMissionHistory(missionHistoryId)
             } else {
-                runCatching {
-                    emojiRepository.registerEmoji(
-                        targetId = challenge.challengeId,
-                        targetType = "challenge",
-                        emojiType = EmojiType.LIKE
-                    )
-                }.onSuccess { emojiId ->
-                    emojiMap.update {
-                        it + (challenge.challengeId to emoji!!.copy(
-                            isLike = true,
-                            likeId = emojiId
-                        ))
-                    }
-                }
+                missionRepository.unlikeMissionHistory(missionHistoryId)
+            }
+
+            result.onSuccess {
+                _missionHistoryRefreshTrigger.emit(Unit)
             }
         }
     }
 
-    fun hateChallenge(challenge: RandomChallenge) {
+    fun hateChallenge(missionHistory: RandomMissionHistoryUiModel) {
         viewModelScope.launch {
-            val emoji = emojiMap.value[challenge.challengeId] ?: challenge.emoji
-            if (emoji?.isHate == true) {
-                runCatching {
-                    emojiRepository.deleteEmoji(emoji.hateId!!)
-                }.onSuccess {
-                    emojiMap.update {
-                        it + (challenge.challengeId to emoji.copy(
-                            isHate = false,
-                            hateId = null
-                        ))
-                    }
-                }
+            val missionHistoryId = missionHistory.missionHistoryId
+            val emojiTypes = missionHistory.currentUserEmojis
+
+            val result = if (!emojiTypes.contains("HATE")) {
+                missionRepository.hateMissionHistory(missionHistoryId)
             } else {
-                runCatching {
-                    emojiRepository.registerEmoji(
-                        targetId = challenge.challengeId,
-                        targetType = "challenge",
-                        emojiType = EmojiType.HATE
-                    )
-                }.onSuccess { emojiId ->
-                    emojiMap.update {
-                        it + (challenge.challengeId to emoji!!.copy(
-                            isHate = true,
-                            hateId = emojiId
-                        ))
-                    }
-                }
+                missionRepository.unhateMissionHistory(missionHistoryId)
+            }
+
+            result.onSuccess {
+                _missionHistoryRefreshTrigger.emit(Unit)
             }
         }
     }
 
-    fun reportChallenge(challenge: RandomChallenge) {
+    fun reportMissionHistory(missionHistory: RandomMissionHistoryUiModel) {
         viewModelScope.launch {
             runCatching {
-                challengeRepository.reportChallenge(challenge.challengeId)
+                missionRepository.reportMissionHistory(missionHistory.missionHistoryId)
             }.onSuccess {
-                _isReportSuccess.update { true }
-            }.onFailure {
-                _isReportSuccess.update { false }
+                _missionHistoryRefreshTrigger.emit(Unit)
             }
         }
-    }
-
-    fun resetReportStatus() {
-        _isReportSuccess.update { null }
     }
 }
