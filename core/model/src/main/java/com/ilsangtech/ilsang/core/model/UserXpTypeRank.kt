b2870e115b15866f9bf8bf49956abfcf82939c35
swkim1 <swkim1@hds-secu.com>
2025-08-25 10:46:21 +0900

    [Refactor/#107] 스텟 랭킹 관련 네트워크, 도메인, UI 클래스, 파일 제거

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/UserXpTypeRank.kt
+++
@@ -1,9 +0,0 @@
-package com.ilsangtech.ilsang.core.model
-
-data class UserXpTypeRank(
-    val customerId: String,
-    val nickname: String,
-    val profileImage: String?,
-    val xpPoint: Int,
-    val xpType: String
-)
