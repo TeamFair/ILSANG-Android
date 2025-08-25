b4f633dfb5db7468e1bdbbf9155cfca114e43c19
swkim1 <swkim1@hds-secu.com>
2025-08-25 13:26:57 +0900

    [Feat/#109] MissionApiService 내 사용자가 수행한 미션 이력 조회 API 요청 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/MissionApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/MissionApiService.kt
@@ -1,12 +1,22 @@
 package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.mission.MissionDetailResponse
+import com.ilsangtech.ilsang.core.network.model.mission.UserMissionHistoryResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MissionApiService {
     @GET("api/v1/mission/{id}")
     suspend fun getMissionById(
         @Path("id") id: Int
     ): MissionDetailResponse
+
+    @GET("api/v1/mission/user/history")
+    suspend fun getUserMissionHistory(
+        @Query("userId") userId: String?,
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("sort") sort: List<String> = emptyList()
+    ): UserMissionHistoryResponse
 }
