833c21f757509ef51facaa1bfcb622b1366061dc
swkim1 <swkim1@hds-secu.com>
2025-08-25 13:37:35 +0900

    [Feat/#109] MissionApiService 내 랜덤 미션 이력 조회 API 요청 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/MissionApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/MissionApiService.kt
@@ -1,6 +1,7 @@
 package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.mission.MissionDetailResponse
+import com.ilsangtech.ilsang.core.network.model.mission.RandomMissionHistoryResponse
 import com.ilsangtech.ilsang.core.network.model.mission.UserMissionHistoryResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -19,4 +20,11 @@ interface MissionApiService {
         @Query("size") size: Int,
         @Query("sort") sort: List<String> = emptyList()
     ): UserMissionHistoryResponse
+
+    @GET("api/v1/mission/user/history/random")
+    suspend fun getRandomMissionHistory(
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("sort") sort: List<String> = emptyList()
+    ): RandomMissionHistoryResponse
 }
