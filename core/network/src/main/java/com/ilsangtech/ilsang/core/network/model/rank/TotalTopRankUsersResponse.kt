02f3d7e1fc449c183497872398bc4d2ce3f426bb
swkim1 <swkim1@hds-secu.com>
2025-08-25 10:37:39 +0900

    [Fix/#107] 사용하지 않은 DTO 클래스 제거

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/rank/TotalTopRankUsersResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.ilsangtech.ilsang.core.network.model.rank
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class TotalTopRankUsersResponse : ArrayList<UserRankNetworkModel>()
