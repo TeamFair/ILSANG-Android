c9093e07faf2f6ac6c3e6d0a0541d2de3d3e049b
swkim1 <swkim1@hds-secu.com>
2025-08-27 15:01:32 +0900

    [Feat/#122] 미션 인증 예시 도메인 클래스 매핑 로직 추가

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/mission/mapper/MissionHistoryMapper.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/mission/mapper/MissionHistoryMapper.kt
@@ -1,13 +1,30 @@
 package com.ilsangtech.ilsang.core.data.mission.mapper
 
 import com.ilsangtech.ilsang.core.data.title.mapper.toTitle
+import com.ilsangtech.ilsang.core.model.mission.ExampleMissionHistory
 import com.ilsangtech.ilsang.core.model.mission.MissionHistoryUser
 import com.ilsangtech.ilsang.core.model.mission.RandomMissionHistory
+import com.ilsangtech.ilsang.core.network.model.mission.ExampleMissionHistoryNetworkModel
 import com.ilsangtech.ilsang.core.network.model.mission.MissionHistoryUserNetworkModel
 import com.ilsangtech.ilsang.core.network.model.mission.RandomMissionHistoryNetworkModel
 
 internal fun RandomMissionHistoryNetworkModel.toRandomMissionHistory(): RandomMissionHistory {
     return RandomMissionHistory(
+        commercialAreaCode = commercialAreaCode,
+        createdAt = createdAt,
+        currentUserEmojis = currentUserEmojis,
+        hateCount = hateCount,
+        likeCount = likeCount,
+        missionHistoryId = missionHistoryId,
+        submitImageId = submitImageId,
+        title = title,
+        user = user.toMissionHistoryUser(),
+        viewCount = viewCount
+    )
+}
+
+internal fun ExampleMissionHistoryNetworkModel.toExampleMissionHistory(): ExampleMissionHistory {
+    return ExampleMissionHistory(
         commercialAreaCode = commercialAreaCode,
         createdAt = createdAt,
         currentUserEmojis = currentUserEmojis,
