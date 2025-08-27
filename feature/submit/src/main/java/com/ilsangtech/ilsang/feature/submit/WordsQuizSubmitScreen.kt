bd4bac02d9ddfaf6b454f610b030b8e4a2374a19
swkim1 <swkim1@hds-secu.com>
2025-08-27 13:35:58 +0900

    [Feat/#115] 구현한 뷰모델 상태를 기반으로 WordsQuizSubmitScreen 함수 구성, 적용

--- feature/submit/src/main/java/com/ilsangtech/ilsang/feature/submit/WordsQuizSubmitScreen.kt
+++ feature/submit/src/main/java/com/ilsangtech/ilsang/feature/submit/WordsQuizSubmitScreen.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -22,20 +24,81 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ilsangtech.ilsang.core.model.NewQuestType
+import com.ilsangtech.ilsang.core.model.RewardPoint
 import com.ilsangtech.ilsang.designsystem.theme.background
 import com.ilsangtech.ilsang.designsystem.theme.gray300
 import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
 import com.ilsangtech.ilsang.designsystem.theme.primary
+import com.ilsangtech.ilsang.feature.submit.component.InCorrectAnswerDialog
 import com.ilsangtech.ilsang.feature.submit.component.QuizQuestInfoCard
 import com.ilsangtech.ilsang.feature.submit.component.QuizScreenHeader
+import com.ilsangtech.ilsang.feature.submit.component.SubmitErrorDialog
+import com.ilsangtech.ilsang.feature.submit.component.SubmitLoadingDialog
+import com.ilsangtech.ilsang.feature.submit.component.SubmitSuccessDialog
 import com.ilsangtech.ilsang.feature.submit.component.WordsQuizGuideCard
 import com.ilsangtech.ilsang.feature.submit.component.WordsQuizSubmitCard
 
 @Composable
+internal fun WordsQuizSubmitScreen(
+    viewModel: WordsQuizSubmitViewModel,
+    onBackButtonClick: () -> Unit
+) {
+    val wordsQuizUiState by viewModel.wordsQuizUiState.collectAsStateWithLifecycle()
+    val submitResultUiState by viewModel.submitResultUiState.collectAsStateWithLifecycle()
+    val submitAnswerState = viewModel.submitAnswerState
+
+    when (val result = submitResultUiState) {
+        SubmitResultUiState.Loading -> {
+            SubmitLoadingDialog()
+        }
+
+        is SubmitResultUiState.Success -> {
+            SubmitSuccessDialog(
+                rewardPoints = result.rewardPoints,
+                onDismissRequest = {
+                    viewModel.resetResultUiState()
+                    onBackButtonClick()
+                }
+            )
+        }
+
+        SubmitResultUiState.WrongAnswer -> {
+            InCorrectAnswerDialog {
+                viewModel.resetResultUiState()
+            }
+        }
+
+        SubmitResultUiState.Error -> {
+            SubmitErrorDialog {
+                viewModel.resetResultUiState()
+                onBackButtonClick()
+            }
+        }
+
+        else -> {}
+    }
+
+    if (wordsQuizUiState is WordsQuizUiState.Success) {
+        val wordsQuizUiState = wordsQuizUiState as WordsQuizUiState.Success
+        WordsQuizSubmitScreen(
+            question = wordsQuizUiState.question,
+            hint = wordsQuizUiState.hint,
+            submitQuestUiState = wordsQuizUiState.submitQuestUiState,
+            submitAnswer = submitAnswerState,
+            onSubmitButtonClick = viewModel::submitMission,
+            onBackButtonClick = onBackButtonClick
+        )
+    }
+}
+
+@Composable
 private fun WordsQuizSubmitScreen(
+    question: String,
+    hint: String?,
     submitQuestUiState: SubmitQuestUiState,
-    quizUiState: QuizUiState.WordsQuizUiState,
+    submitAnswer: TextFieldState,
     onSubmitButtonClick: () -> Unit,
     onBackButtonClick: () -> Unit
 ) {
@@ -51,15 +114,21 @@ private fun WordsQuizSubmitScreen(
                     QuizQuestInfoCard(
                         questImageId = submitQuestUiState.questImageId,
                         title = submitQuestUiState.title,
-                        locationName = submitQuestUiState.locationName,
+                        writerName = submitQuestUiState.writerName,
                         questType = submitQuestUiState.questType,
-                        point = submitQuestUiState.point
+                        point = submitQuestUiState.rewards.sumOf { it.point }
                     )
                 }
                 item { Spacer(Modifier.height(16.dp)) }
                 item { WordsQuizGuideCard() }
                 item { Spacer(Modifier.height(16.dp)) }
-                item { WordsQuizSubmitCard(wordsQuizUiState = quizUiState) }
+                item {
+                    WordsQuizSubmitCard(
+                        question = question,
+                        hint = hint,
+                        answer = submitAnswer
+                    )
+                }
                 item { Spacer(Modifier.height(40.dp)) }
                 item {
                     Button(
@@ -74,7 +143,7 @@ private fun WordsQuizSubmitScreen(
                             disabledContentColor = Color.White
                         ),
                         shape = RoundedCornerShape(12.dp),
-                        enabled = quizUiState.answer.text.isNotEmpty(),
+                        enabled = submitAnswer.text.isNotEmpty(),
                         onClick = onSubmitButtonClick
                     ) {
                         Text(
@@ -97,24 +166,28 @@ private fun WordsQuizSubmitScreen(
 @Composable
 private fun WordsQuizScreenPreview() {
     val answerTextFieldState = rememberTextFieldState()
-    val submitQuestUiState = SubmitQuestUiState(
-        questImageId = "",
-        title = "정자동 최고의 돈까스 가게 가기",
-        locationName = "야미돈까스 정자동점",
-        questType = NewQuestType.Event,
-        point = 10
-    )
-
-    val quizUiState = QuizUiState.WordsQuizUiState(
+    val quizUiState = WordsQuizUiState.Success(
         quizId = 0,
         question = "야미돈까스 정자동점의 베스트셀러 메뉴는?",
         hint = "등심",
-        answer = answerTextFieldState
+        submitQuestUiState = SubmitQuestUiState(
+            questImageId = "",
+            title = "정자동 최고의 돈까스 가게 가기",
+            writerName = "야미돈까스 정자동점",
+            questType = NewQuestType.Event,
+            rewards = listOf(
+                RewardPoint.Metro(5),
+                RewardPoint.Commercial(10),
+                RewardPoint.Contribute(15)
+            )
+        )
     )
 
     WordsQuizSubmitScreen(
-        submitQuestUiState = submitQuestUiState,
-        quizUiState = quizUiState,
+        question = quizUiState.question,
+        hint = quizUiState.hint,
+        submitQuestUiState = quizUiState.submitQuestUiState,
+        submitAnswer = answerTextFieldState,
         onSubmitButtonClick = {},
         onBackButtonClick = {}
     )
