8c32ca58981f1920433f41a0f5a317b371d6e97c
swkim1 <swkim1@hds-secu.com>
2025-08-27 17:49:46 +0900

    [Design/#118] 사용자 프로필 정보 UI 함수 디자인 변경

--- feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/component/UserProfileInfoCard.kt
+++ feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/component/UserProfileInfoCard.kt
@@ -1,11 +1,15 @@
 package com.ilsangtech.ilsang.feature.profile.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -13,11 +17,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -25,21 +29,24 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.TitleGrade
+import com.ilsangtech.ilsang.core.model.title.TitleType
+import com.ilsangtech.ilsang.core.ui.title.TitleGradeIcon
 import com.ilsangtech.ilsang.core.util.XpLevelCalculator
 import com.ilsangtech.ilsang.designsystem.R
-import com.ilsangtech.ilsang.designsystem.theme.gray100
-import com.ilsangtech.ilsang.designsystem.theme.gray400
+import com.ilsangtech.ilsang.designsystem.theme.badge01TextStyle
 import com.ilsangtech.ilsang.designsystem.theme.gray500
 import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
 import com.ilsangtech.ilsang.designsystem.theme.primary
-import com.ilsangtech.ilsang.designsystem.theme.primary500
-import java.util.Locale
+import com.ilsangtech.ilsang.designsystem.theme.toSp
 
 @Composable
 internal fun UserProfileInfoCard(
     modifier: Modifier = Modifier,
     nickname: String,
-    xpPoint: Int
+    title: Title?,
+    point: Int
 ) {
     Card(
         modifier = modifier.fillMaxWidth(),
@@ -47,76 +54,112 @@ internal fun UserProfileInfoCard(
         shape = RoundedCornerShape(12.dp)
     ) {
         Row(
-            modifier = Modifier.padding(
-                vertical = 18.dp,
-                horizontal = 16.dp
-            ),
+            modifier = Modifier
+                .padding(vertical = 15.dp)
+                .padding(start = 16.dp, end = 33.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                modifier = Modifier.size(57.dp),
-                tint = Color.Unspecified,
-                painter = painterResource(R.drawable.default_user_profile),
-                contentDescription = null
-            )
+            Box(modifier = Modifier.padding(bottom = 7.dp)) {
+                Icon(
+                    modifier = Modifier.size(57.dp),
+                    tint = Color.Unspecified,
+                    painter = painterResource(R.drawable.default_user_profile),
+                    contentDescription = null
+                )
+                Box(
+                    modifier = Modifier
+                        .offset(y = 7.dp)
+                        .align(Alignment.BottomCenter)
+                        .width(57.dp)
+                        .clip(RoundedCornerShape(20.dp))
+                        .border(
+                            width = 1.dp,
+                            color = primary,
+                            shape = RoundedCornerShape(20.dp)
+                        )
+                        .background(Color.White)
+                        .padding(vertical = 3.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Lv. ${XpLevelCalculator.getCurrentLevel(point)}",
+                        style = TextStyle(
+                            fontFamily = pretendardFontFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 13.dp.toSp(),
+                            lineHeight = 12.dp.toSp()
+                        ),
+                        color = primary
+                    )
+                }
+            }
             Spacer(Modifier.width(16.dp))
             Column(
                 modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
             ) {
                 Text(
                     text = nickname,
                     style = TextStyle(
                         fontFamily = pretendardFontFamily,
                         fontWeight = FontWeight.Bold,
-                        fontSize = 15.sp,
+                        fontSize = 16.sp,
+                        lineHeight = 18.sp,
                         color = gray500
                     )
                 )
-                Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
-                    Text(
-                        text = "LV.${XpLevelCalculator.getCurrentLevel(xpPoint)}",
-                        style = TextStyle(
-                            fontSize = 13.sp,
-                            fontFamily = pretendardFontFamily,
-                            fontWeight = FontWeight.SemiBold,
-                            color = primary500
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    title?.let {
+                        TitleGradeIcon(
+                            modifier = Modifier.size(20.dp),
+                            titleGrade = title.grade
                         )
-                    )
-                    Text(
-                        text = "|",
-                        style = TextStyle(
-                            fontSize = 13.sp,
-                            fontFamily = pretendardFontFamily,
-                            fontWeight = FontWeight.Normal,
-
-                            color = gray100
+                        Text(
+                            text = title.name,
+                            style = badge01TextStyle.copy(
+                                fontSize = 11.dp.toSp(),
+                                lineHeight = 12.dp.toSp()
+                            ),
+                            color = gray500
                         )
-                    )
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(8.dp)
+                            .clip(RoundedCornerShape(6.dp))
+                            .background(Color(0xFFEDEBEB))
+                    ) {
+                        val pointProgress = XpLevelCalculator.getLevelProgress(point)
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(pointProgress)
+                                .height(8.dp)
+                                .clip(RoundedCornerShape(6.dp))
+                                .background(primary)
+                        )
+                    }
                     Text(
-                        text = String.format(
-                            locale = Locale.KOREA,
-                            format = "%,d",
-                            xpPoint
-                        ) + "XP",
+                        modifier = Modifier.padding(start = 7.dp),
+                        text = "${point}P",
                         style = TextStyle(
-                            fontSize = 13.sp,
                             fontFamily = pretendardFontFamily,
-                            fontWeight = FontWeight.SemiBold,
-                            color = gray400
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 13.dp.toSp(),
+                            lineHeight = 12.dp.toSp(),
+                            color = primary
                         )
                     )
                 }
-                LinearProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(6.dp),
-                    progress = { XpLevelCalculator.getLevelProgress(xpPoint) },
-                    color = primary,
-                    trackColor = gray100,
-                    gapSize = (-5).dp,
-                    drawStopIndicator = {}
-                )
             }
         }
     }
@@ -127,7 +170,12 @@ internal fun UserProfileInfoCard(
 private fun UserProfileInfoCardPreview() {
     UserProfileInfoCard(
         nickname = "김일상123",
-        xpPoint = 16300
+        point = 650,
+        title = Title(
+            name = "세상을 움직이는 자",
+            grade = TitleGrade.Rare,
+            type = TitleType.Metro
+        )
     )
 }
 
