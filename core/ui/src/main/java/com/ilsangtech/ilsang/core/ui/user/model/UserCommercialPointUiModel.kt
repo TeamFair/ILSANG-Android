aa6c35784b201f6c87de12844315435b97563356
swkim1 <swkim1@hds-secu.com>
2025-08-28 17:50:04 +0900

    [Feat/#118] 사용자 일상존(Commercial) 포인트 UI 모델 클래스 구현

---
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/user/model/UserCommercialPointUiModel.kt
@@ -0,0 +1,36 @@
+package com.ilsangtech.ilsang.core.ui.user.model
+
+import com.ilsangtech.ilsang.core.model.user.TopCommercialArea
+import com.ilsangtech.ilsang.core.model.user.TotalOwnerContribution
+
+data class UserCommercialPointUiModel(
+    val nickname: String,
+    val topCommercialArea: TopCommercialAreaUiModel,
+    val totalOwnerContributions: List<TotalOwnerContributionUiModel>
+)
+
+data class TopCommercialAreaUiModel(
+    val commercialAreaName: String,
+    val contributionPercent: Int,
+    val point: Int
+)
+
+data class TotalOwnerContributionUiModel(
+    val commercialAreaName: String,
+    val point: Int
+)
+
+internal fun TopCommercialArea.toUiModel(areaName: String): TopCommercialAreaUiModel {
+    return TopCommercialAreaUiModel(
+        commercialAreaName = areaName,
+        contributionPercent = ownerContributionPercent,
+        point = point
+    )
+}
+
+internal fun TotalOwnerContribution.toUiModel(areaName: String): TotalOwnerContributionUiModel {
+    return TotalOwnerContributionUiModel(
+        commercialAreaName = areaName,
+        point = point
+    )
+}
