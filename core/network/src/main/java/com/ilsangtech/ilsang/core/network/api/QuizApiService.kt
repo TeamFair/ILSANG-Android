4f85c66f931a8a769d4a6473ca6a9747c71aa876
swkim1 <swkim1@hds-secu.com>
2025-08-28 10:21:36 +0900

    [Fix/#115] QuizApiService 퀴즈 조회 함수에 Query 어노테이션 적용

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/QuizApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/QuizApiService.kt
@@ -2,8 +2,11 @@ package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.quiz.RandomQuizResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface QuizApiService {
     @GET("api/v1/challenge/random-quiz")
-    suspend fun getRandomQuiz(missionId: Int): RandomQuizResponse
+    suspend fun getRandomQuiz(
+        @Query("missionId") missionId: Int
+    ): RandomQuizResponse
 }
