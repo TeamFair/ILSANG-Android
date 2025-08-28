25954c6f9f7e502adc315f9cf207303c9c885cf4
swkim1 <swkim1@hds-secu.com>
2025-08-28 17:58:54 +0900

    [Feat/#118] 사용자가 수행한 미션 이력 도메인 클래스 구현

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/UserMissionHistory.kt
@@ -0,0 +1,8 @@
+package com.ilsangtech.ilsang.core.model.mission
+
+data class UserMissionHistory(
+    val missionHistoryId: Int,
+    val title: String,
+    val submitImageId: String,
+    val createdAt: String
+)
