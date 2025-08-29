6c398af17c8619ad61f2b9c612ecfaed7c163816
swkim1 <swkim1@hds-secu.com>
2025-08-29 13:06:42 +0900

    [Fix/#118] ProfileUiState.Success 타입에 포함하는 데이터에 UserPoint 대신 UI 모델 클래스 적용

--- feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/model/ProfileUiState.kt
+++ feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/model/ProfileUiState.kt
@@ -1,14 +1,14 @@
 package com.ilsangtech.ilsang.feature.profile.model
 
-import com.ilsangtech.ilsang.core.model.user.UserPoint
 import com.ilsangtech.ilsang.core.ui.user.model.UserCommercialPointUiModel
+import com.ilsangtech.ilsang.core.ui.user.model.UserObtainedPointUiModel
 
 sealed interface ProfileUiState {
     data object Loading : ProfileUiState
     data class Success(
         val userProfileInfo: UserProfileInfoUiModel,
         val userCommercialPoint: UserCommercialPointUiModel,
-        val userPoint: UserPoint
+        val userPoint: UserObtainedPointUiModel
     ) : ProfileUiState
 
     data object Error : ProfileUiState
