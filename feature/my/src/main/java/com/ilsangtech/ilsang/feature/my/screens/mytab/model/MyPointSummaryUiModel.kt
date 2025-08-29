636b3d688cc05912afa408aacbde76942d4e7ced
swkim1 <swkim1@hds-secu.com>
2025-08-29 17:16:49 +0900

    [Feat/#128] 내 시즌 요약 UI 모델 클래스 생성, 매핑 함수 구현

---
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/mytab/model/MyPointSummaryUiModel.kt
@@ -0,0 +1,30 @@
+package com.ilsangtech.ilsang.feature.my.screens.mytab.model
+
+import com.ilsangtech.ilsang.core.model.season.Season
+import com.ilsangtech.ilsang.core.model.user.UserPointSummary
+
+data class MyPointSummaryUiModel(
+    val nickName: String,
+    val topCommercialAreaName: String,
+    val topMetroAreaName: String,
+    val topContributionPoint: Int,
+    val seasonNumber: Int,
+    val seasonStartDate: String,
+    val seasonEndDate: String
+)
+
+internal fun UserPointSummary.toMyPointSummaryUiModel(
+    nickName: String,
+    season: Season,
+    changeAreaCodeToName: (String) -> String
+): MyPointSummaryUiModel {
+    return MyPointSummaryUiModel(
+        nickName = nickName,
+        topCommercialAreaName = changeAreaCodeToName(topCommercialAreaCode),
+        topMetroAreaName = changeAreaCodeToName(topMetroAreaCode),
+        topContributionPoint = topContributionPoint,
+        seasonNumber = season.seasonNumber,
+        seasonStartDate = season.startDate,
+        seasonEndDate = season.endDate
+    )
+}
