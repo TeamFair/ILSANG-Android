6b7723917e71c8228faa880aad32c54804b380cd
swkim1 <swkim1@hds-secu.com>
2025-08-29 13:02:59 +0900

    [Fix/#118] 사용자가 수행한 미션 이력 아이템에 사진이 꽉 차도록 수정

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/mission/UserMissionHistoryItem.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/mission/UserMissionHistoryItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -41,6 +42,7 @@ fun UserMissionHistoryItem(
             AsyncImage(
                 modifier = Modifier.fillMaxSize(),
                 model = BuildConfig.IMAGE_URL + userMissionHistory.submitImageId,
+                contentScale = ContentScale.Crop,
                 contentDescription = null
             )
             Column(
