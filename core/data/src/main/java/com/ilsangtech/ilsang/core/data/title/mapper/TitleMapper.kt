5980dcbee7602d6d89e1c55205fadd567986b6bf
swkim1 <swkim1@hds-secu.com>
2025-09-01 15:32:13 +0900

    [Feat/#142] TitleDetailNetworkModel을 TitleDetail로 매핑하는 로직 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/mapper/TitleMapper.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/mapper/TitleMapper.kt
@@ -1,9 +1,11 @@
 package com.ilsangtech.ilsang.core.data.title.mapper
 
 import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.TitleDetail
 import com.ilsangtech.ilsang.core.model.title.TitleGrade
 import com.ilsangtech.ilsang.core.model.title.TitleType
 import com.ilsangtech.ilsang.core.model.title.UserTitle
+import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
 import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 
 internal fun UserTitleNetworkModel.toUserTitle(): UserTitle {
@@ -28,5 +30,27 @@ internal fun UserTitleNetworkModel.toTitle(): Title {
             "CONTRIBUTION" -> TitleType.Contribution
             else -> throw IllegalArgumentException("Unknown title type: $type")
         }
+    )
+}
+
+internal fun TitleDetailNetworkModel.toTitleDetail(): TitleDetail {
+    return TitleDetail(
+        id = id,
+        title = Title(
+            name = name,
+            grade = when (grade) {
+                "STANDARD" -> TitleGrade.Standard
+                "RARE" -> TitleGrade.Rare
+                "LEGEND" -> TitleGrade.Legend
+                else -> throw IllegalArgumentException("Unknown title grade: $grade")
+            },
+            type = when (type) {
+                "METRO" -> TitleType.Metro
+                "COMMERCIAL" -> TitleType.Commercial
+                "CONTRIBUTION" -> TitleType.Contribution
+                else -> throw IllegalArgumentException("Unknown title type: $type")
+            }
+        ),
+        condition = condition
     )
 }
