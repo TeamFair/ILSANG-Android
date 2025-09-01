75732b022865888ceca87caa791c6cdf78dfded7
swkim1 <swkim1@hds-secu.com>
2025-09-01 15:34:45 +0900

    [Fix/#142] getTitleList 함수 타입을 List<TitleDetail>로 변경하여 구현 적용

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/repository/TitleRepositoryImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/repository/TitleRepositoryImpl.kt
@@ -1,33 +1,19 @@
 package com.ilsangtech.ilsang.core.data.title.repository
 
 import com.ilsangtech.ilsang.core.data.title.datasource.TitleDataSource
+import com.ilsangtech.ilsang.core.data.title.mapper.toTitleDetail
 import com.ilsangtech.ilsang.core.data.title.mapper.toUserTitle
 import com.ilsangtech.ilsang.core.domain.TitleRepository
-import com.ilsangtech.ilsang.core.model.title.Title
-import com.ilsangtech.ilsang.core.model.title.TitleGrade
-import com.ilsangtech.ilsang.core.model.title.TitleType
+import com.ilsangtech.ilsang.core.model.title.TitleDetail
 import com.ilsangtech.ilsang.core.model.title.UserTitle
+import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
 import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 
 class TitleRepositoryImpl(
     private val titleDataSource: TitleDataSource
 ) : TitleRepository {
-    override suspend fun getTitleList(): List<Title> {
-        return titleDataSource.getTitleList().map {
-            Title(
-                name = it.name,
-                grade = when (it.grade) {
-                    "STANDARD" -> TitleGrade.Standard
-                    "RARE" -> TitleGrade.Rare
-                    else -> TitleGrade.Legend
-                },
-                type = when (it.type) {
-                    "METRO" -> TitleType.Metro
-                    "COMMERCIAL" -> TitleType.Commercial
-                    else -> TitleType.Contribution
-                }
-            )
-        }
+    override suspend fun getTitleList(): List<TitleDetail> {
+        return titleDataSource.getTitleList().map(TitleDetailNetworkModel::toTitleDetail)
     }
 
     override suspend fun getUserTitleList(): List<UserTitle> {
--- core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/TitleRepository.kt
+++ core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/TitleRepository.kt
@@ -1,10 +1,10 @@
 package com.ilsangtech.ilsang.core.domain
 
-import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.TitleDetail
 import com.ilsangtech.ilsang.core.model.title.UserTitle
 
 interface TitleRepository {
-    suspend fun getTitleList(): List<Title>
+    suspend fun getTitleList(): List<TitleDetail>
 
     suspend fun getUserTitleList(): List<UserTitle>
 }
