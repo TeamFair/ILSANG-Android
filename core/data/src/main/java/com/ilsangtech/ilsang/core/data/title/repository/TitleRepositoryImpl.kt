4300d528c30ce1b1f9fc33daac4be66a8edd50dd
swkim1 <swkim1@hds-secu.com>
2025-09-01 15:03:55 +0900

    [Feat/#142] TitleRepositoryImpl의 전체 칭호 조회 함수를 새롭게 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/repository/TitleRepositoryImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/repository/TitleRepositoryImpl.kt
@@ -2,20 +2,27 @@ package com.ilsangtech.ilsang.core.data.title.repository
 
 import com.ilsangtech.ilsang.core.data.title.datasource.TitleDataSource
 import com.ilsangtech.ilsang.core.domain.TitleRepository
-import com.ilsangtech.ilsang.core.model.Title
+import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.TitleGrade
+import com.ilsangtech.ilsang.core.model.title.TitleType
 
 class TitleRepositoryImpl(
     private val titleDataSource: TitleDataSource
 ) : TitleRepository {
     override suspend fun getTitleList(): List<Title> {
-        return titleDataSource.getTitleList().data.map { (title, history) ->
+        return titleDataSource.getTitleList().map {
             Title(
-                id = title.id,
-                historyId = history?.id,
-                name = title.name,
-                type = title.type,
-                condition = title.condition,
-                createdAt = title.createdAt
+                name = it.name,
+                grade = when (it.grade) {
+                    "STANDARD" -> TitleGrade.Standard
+                    "RARE" -> TitleGrade.Rare
+                    else -> TitleGrade.Legend
+                },
+                type = when (it.type) {
+                    "METRO" -> TitleType.Metro
+                    "COMMERCIAL" -> TitleType.Commercial
+                    else -> TitleType.Contribution
+                }
             )
         }
     }
