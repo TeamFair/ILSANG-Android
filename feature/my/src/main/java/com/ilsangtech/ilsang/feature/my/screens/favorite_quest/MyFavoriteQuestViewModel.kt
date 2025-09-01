5bf471d0cf650d739443f653297f0ddd52de24fd
swkim1 <swkim1@hds-secu.com>
2025-09-01 17:44:52 +0900

    [Feat/#139] 즐겨찾기 퀘스트 화면 뷰모델 클래스 생성

---
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestViewModel.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.feature.my.screens.favorite_quest
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyFavoriteQuestViewModel @Inject constructor() : ViewModel() {
+}
