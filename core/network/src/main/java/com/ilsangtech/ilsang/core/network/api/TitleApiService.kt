8de2ced326db4b2b3a313ab23b3694062963bb69
swkim1 <swkim1@hds-secu.com>
2025-09-01 14:39:33 +0900

    [Fix/#142] TitleApiService 의 칭호 전체 조회 엔드포인트, 반환 타입 변경

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/TitleApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/TitleApiService.kt
@@ -1,9 +1,9 @@
 package com.ilsangtech.ilsang.core.network.api
 
-import com.ilsangtech.ilsang.core.network.model.title.TitleListResponse
+import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
 import retrofit2.http.GET
 
 interface TitleApiService {
-    @GET("customer/title/history")
-    suspend fun getTitleList(): TitleListResponse
+    @GET("api/v1/title")
+    suspend fun getTitleList(): List<TitleDetailNetworkModel>
 }
