44a29ddbbb223c76a14109497e613032a92410aa
swkim1 <swkim1@hds-secu.com>
2025-09-01 15:27:23 +0900

    [Feat/#142] TitleRepository 내 사용자 칭호 조회 함수 생성 및 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/repository/TitleRepositoryImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/repository/TitleRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package com.ilsangtech.ilsang.core.data.title.repository
 
 import com.ilsangtech.ilsang.core.data.title.datasource.TitleDataSource
+import com.ilsangtech.ilsang.core.data.title.mapper.toUserTitle
 import com.ilsangtech.ilsang.core.domain.TitleRepository
 import com.ilsangtech.ilsang.core.model.title.Title
 import com.ilsangtech.ilsang.core.model.title.TitleGrade
 import com.ilsangtech.ilsang.core.model.title.TitleType
+import com.ilsangtech.ilsang.core.model.title.UserTitle
+import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 
 class TitleRepositoryImpl(
     private val titleDataSource: TitleDataSource
@@ -25,5 +28,9 @@ class TitleRepositoryImpl(
                 }
             )
         }
+    }
+
+    override suspend fun getUserTitleList(): List<UserTitle> {
+        return titleDataSource.getUserTitleList().map(UserTitleNetworkModel::toUserTitle)
     }
 }
--- core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/TitleRepository.kt
+++ core/domain/src/main/java/com/ilsangtech/ilsang/core/domain/TitleRepository.kt
@@ -1,7 +1,10 @@
 package com.ilsangtech.ilsang.core.domain
 
 import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.UserTitle
 
 interface TitleRepository {
     suspend fun getTitleList(): List<Title>
+
+    suspend fun getUserTitleList(): List<UserTitle>
 }
