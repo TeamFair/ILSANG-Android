e5b8c600c9674913d1b461d46597e710af1962d0
kimseongwoo <tjddn929@naver.com>
2025-09-01 21:47:36 +0900

    [Feat/#139] MyInfo Flow를 기반으로 지역 이름 UI 상태 추가

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestViewModel.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestViewModel.kt
@@ -3,20 +3,37 @@ package com.ilsangtech.ilsang.feature.my.screens.favorite_quest
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import com.ilsangtech.ilsang.core.domain.AreaRepository
 import com.ilsangtech.ilsang.core.domain.QuestRepository
 import com.ilsangtech.ilsang.core.domain.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MyFavoriteQuestViewModel @Inject constructor(
     userRepository: UserRepository,
+    areaRepository: AreaRepository,
     questRepository: QuestRepository
 ) : ViewModel() {
+    private val myInfo = userRepository.getMyInfo()
+
+    val commercialAreaName = myInfo.map {
+        areaRepository
+            .getCommercialArea(it.myCommericalAreaCode)
+            .areaName
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = null
+    )
+
     @OptIn(ExperimentalCoroutinesApi::class)
-    val favoriteQuestList = userRepository.getMyInfo().flatMapLatest { myInfo ->
+    val favoriteQuestList = myInfo.flatMapLatest { myInfo ->
         questRepository.getTypedQuests(
             commercialAreaCode = myInfo.myCommericalAreaCode,
             favoriteYn = true
