593179dcd0e3f12393901c0ca8bfb6c8c97d1866
swkim1 <swkim1@hds-secu.com>
2025-09-01 15:23:19 +0900

    [Feat/#142] TitleDataSource 내 사용자 칭호 조회 함수 생성 및 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/datasource/TitleDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/datasource/TitleDataSource.kt
@@ -1,7 +1,10 @@
 package com.ilsangtech.ilsang.core.data.title.datasource
 
 import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
+import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 
 interface TitleDataSource {
     suspend fun getTitleList(): List<TitleDetailNetworkModel>
+
+    suspend fun getUserTitleList(): List<UserTitleNetworkModel>
 }
--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/datasource/TitleDataSourceImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/datasource/TitleDataSourceImpl.kt
@@ -2,9 +2,14 @@ package com.ilsangtech.ilsang.core.data.title.datasource
 
 import com.ilsangtech.ilsang.core.network.api.TitleApiService
 import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
+import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 
 class TitleDataSourceImpl(private val titleApiService: TitleApiService) : TitleDataSource {
     override suspend fun getTitleList(): List<TitleDetailNetworkModel> {
         return titleApiService.getTitleList()
+    }
+
+    override suspend fun getUserTitleList(): List<UserTitleNetworkModel> {
+        return titleApiService.getUserTitleList()
     }
 }
