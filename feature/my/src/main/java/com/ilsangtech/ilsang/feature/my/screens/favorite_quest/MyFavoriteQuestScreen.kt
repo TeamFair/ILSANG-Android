b55694785c1ac1c07bb2fb51a272b9b3c8469815
swkim1 <swkim1@hds-secu.com>
2025-09-01 17:58:59 +0900

    [Feat/#139] 즐겨찾기 퀘스트 페이징 데이터 관리, MyFavoriteQuestScreen에 적용

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestScreen.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestScreen.kt
@@ -1,7 +1,21 @@
 package com.ilsangtech.ilsang.feature.my.screens.favorite_quest
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.ilsangtech.ilsang.core.model.quest.TypedQuest
 
 @Composable
-private fun MyFavoriteQuestScreen() {
+internal fun MyFavoriteQuestScreen(
+    viewModel: MyFavoriteQuestViewModel = hiltViewModel()
+) {
+    val favoriteQuestList = viewModel.favoriteQuestList.collectAsLazyPagingItems()
+    MyFavoriteQuestScreen(favoriteQuestList)
+}
+
+@Composable
+private fun MyFavoriteQuestScreen(
+    favoriteQuestList: LazyPagingItems<TypedQuest>
+) {
 }
--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestViewModel.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/MyFavoriteQuestViewModel.kt
@@ -1,9 +1,25 @@
 package com.ilsangtech.ilsang.feature.my.screens.favorite_quest
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.ilsangtech.ilsang.core.domain.QuestRepository
+import com.ilsangtech.ilsang.core.domain.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
-class MyFavoriteQuestViewModel @Inject constructor() : ViewModel() {
+class MyFavoriteQuestViewModel @Inject constructor(
+    userRepository: UserRepository,
+    questRepository: QuestRepository
+) : ViewModel() {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val favoriteQuestList = userRepository.getMyInfo().flatMapLatest { myInfo ->
+        questRepository.getTypedQuests(
+            commercialAreaCode = myInfo.myCommericalAreaCode,
+            favoriteYn = true
+        )
+    }.cachedIn(viewModelScope)
 }
