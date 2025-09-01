7bd7edbbd9a2c14b1d761afdc6dd17a7892a9103
swkim1 <swkim1@hds-secu.com>
2025-09-01 15:18:57 +0900

    [Feat/#142] TitleApiService 내 사용자 칭호 조회 API 요청 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/TitleApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/TitleApiService.kt
@@ -1,9 +1,13 @@
 package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
+import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 import retrofit2.http.GET
 
 interface TitleApiService {
     @GET("api/v1/title")
     suspend fun getTitleList(): List<TitleDetailNetworkModel>
+
+    @GET("api/v1/user/title")
+    suspend fun getUserTitleList(): List<UserTitleNetworkModel>
 }
