8693f0e2b5c6551c798eb0934f82a6d55fc809e5
swkim1 <swkim1@hds-secu.com>
2025-09-01 17:50:23 +0900

    [Feat/#139] 즐겨찾기 퀘스트 화면 헤더 UI 구현

---
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/favorite_quest/component/MyFavoriteQuestHeader.kt
@@ -0,0 +1,63 @@
+package com.ilsangtech.ilsang.feature.my.screens.favorite_quest.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ilsangtech.ilsang.designsystem.R
+import com.ilsangtech.ilsang.designsystem.theme.gray500
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
+
+@Composable
+internal fun MyFavoriteQuestHeader(onBackButtonClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(vertical = 12.dp),
+    ) {
+        Icon(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 16.dp)
+                .clickable(
+                    onClick = onBackButtonClick,
+                    indication = null,
+                    interactionSource = null
+                ),
+            painter = painterResource(id = R.drawable.icon_back),
+            tint = gray500,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "즐겨찾기 퀘스트",
+            style = TextStyle(
+                fontFamily = pretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 17.sp,
+                lineHeight = 22.sp,
+                color = Color.Black
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MyFavoriteQuestHeaderPreview() {
+    MyFavoriteQuestHeader(onBackButtonClick = {})
+}
