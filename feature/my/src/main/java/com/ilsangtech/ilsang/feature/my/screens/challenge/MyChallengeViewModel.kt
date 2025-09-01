8ec5b5ec1b571214eca07c950b68a947d114ff92
swkim1 <swkim1@hds-secu.com>
2025-09-01 11:47:11 +0900

    [Feat/#138] 내가 수행한 챌린지 뷰모델 클래스 생성

---
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge/MyChallengeViewModel.kt
@@ -0,0 +1,10 @@
+package com.ilsangtech.ilsang.feature.my.screens.challenge
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyChallengeViewModel @Inject constructor(
+) : ViewModel() {
+}
