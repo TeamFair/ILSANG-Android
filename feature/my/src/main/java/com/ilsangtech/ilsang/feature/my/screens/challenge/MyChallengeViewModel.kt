ef36df9f8056a6b7caf507953b376fcbdab86174
swkim1 <swkim1@hds-secu.com>
2025-09-01 12:59:41 +0900

    [Feat/#138] MyChallengeViewModel에 페이징 데이터를 노출하도록 구현

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge/MyChallengeViewModel.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge/MyChallengeViewModel.kt
@@ -1,10 +1,23 @@
 package com.ilsangtech.ilsang.feature.my.screens.challenge
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.ilsangtech.ilsang.core.domain.MissionRepository
+import com.ilsangtech.ilsang.core.model.mission.UserMissionHistory
+import com.ilsangtech.ilsang.core.ui.mission.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class MyChallengeViewModel @Inject constructor(
+    missionRepository: MissionRepository
 ) : ViewModel() {
+    val myMissionHistories = missionRepository.getUserMissionHistory()
+        .cachedIn(viewModelScope)
+        .map { pagingData ->
+            pagingData.map(UserMissionHistory::toUiModel)
+        }
 }
