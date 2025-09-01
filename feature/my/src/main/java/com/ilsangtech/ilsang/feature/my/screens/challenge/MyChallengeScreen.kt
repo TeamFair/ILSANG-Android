e6e56861d7fd17412c26324d50dd83f657317e89
swkim1 <swkim1@hds-secu.com>
2025-09-01 14:14:48 +0900

    [Fix/#138] 수행한 퀘스트가 없을 때 나타나는 UI가 제대로 나타나도록 수정

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge/MyChallengeScreen.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge/MyChallengeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -34,6 +35,11 @@ internal fun MyChallengeScreen(
     onBackButtonClick: () -> Unit
 ) {
     val myMissionHistoryItems = viewModel.myMissionHistories.collectAsLazyPagingItems()
+
+    LaunchedEffect(Unit) {
+        myMissionHistoryItems.refresh()
+    }
+
     MyChallengeScreen(
         myMissionHistoryItems = myMissionHistoryItems,
         onMissionHistoryClick = onMissionHistoryClick,
@@ -55,43 +61,44 @@ private fun MyChallengeScreen(
     ) {
         Column {
             MyChallengeHeader(onBackButtonClick = onBackButtonClick)
-            LazyColumn(
-                contentPadding = PaddingValues(start = 20.dp, end = 20.dp, bottom = 72.dp)
+
+            if (myMissionHistoryItems.loadState.refresh is LoadState.NotLoading
+                && myMissionHistoryItems.itemCount == 0
             ) {
-                item {
-                    Text(
-                        modifier = Modifier.padding(top = 16.dp, bottom = 20.dp),
-                        text = "수행한 챌린지",
-                        style = TextStyle(
-                            fontFamily = pretendardFontFamily,
-                            fontWeight = FontWeight.Medium,
-                            fontSize = 14.sp,
-                            lineHeight = 16.sp,
-                            color = gray400
-                        )
-                    )
-                }
-                if (myMissionHistoryItems.loadState.refresh is LoadState.NotLoading
-                    && myMissionHistoryItems.itemCount == 0
+                MyChallengeEmptyContent(
+                    modifier = Modifier.weight(1f),
+                    onQuestNavButtonClick = onQuestNavButtonClick
+                )
+            } else {
+                LazyColumn(
+                    contentPadding = PaddingValues(start = 20.dp, end = 20.dp, bottom = 72.dp)
                 ) {
                     item {
-                        MyChallengeEmptyContent(
-                            modifier = Modifier.weight(1f),
-                            onQuestNavButtonClick = onQuestNavButtonClick
+                        Text(
+                            modifier = Modifier.padding(top = 16.dp, bottom = 20.dp),
+                            text = "수행한 챌린지",
+                            style = TextStyle(
+                                fontFamily = pretendardFontFamily,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 14.sp,
+                                lineHeight = 16.sp,
+                                color = gray400
+                            )
                         )
                     }
-                }
-                items(myMissionHistoryItems.itemCount) { index ->
-                    val item = myMissionHistoryItems[index]
-                    item?.let {
-                        UserMissionHistoryItem(
-                            modifier = Modifier.padding(
-                                bottom =
-                                    if (myMissionHistoryItems.itemCount - 1 != index) 9.dp else 0.dp
-                            ),
-                            userMissionHistory = item,
-                            onClick = { onMissionHistoryClick(item) }
-                        )
+
+                    items(myMissionHistoryItems.itemCount) { index ->
+                        val item = myMissionHistoryItems[index]
+                        item?.let {
+                            UserMissionHistoryItem(
+                                modifier = Modifier.padding(
+                                    bottom =
+                                        if (myMissionHistoryItems.itemCount - 1 != index) 9.dp else 0.dp
+                                ),
+                                userMissionHistory = item,
+                                onClick = { onMissionHistoryClick(item) }
+                            )
+                        }
                     }
                 }
             }
