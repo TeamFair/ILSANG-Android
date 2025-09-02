1f171c9ca1a4eeaf1f31ec67ffebd35b918becb0
swkim1 <swkim1@hds-secu.com>
2025-09-02 15:00:11 +0900

    [Feat/#156] 유저 랭킹 정보가 없을 때 나타나는 텍스트 UI 구현

--- feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/RankingDetailScreen.kt
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/RankingDetailScreen.kt
@@ -1,9 +1,12 @@
 package com.ilsangtech.ilsang.feature.ranking
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
@@ -14,15 +17,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ilsangtech.ilsang.core.model.title.TitleGrade
 import com.ilsangtech.ilsang.designsystem.theme.background
+import com.ilsangtech.ilsang.designsystem.theme.gray400
+import com.ilsangtech.ilsang.designsystem.theme.gray500
 import com.ilsangtech.ilsang.designsystem.theme.heading02
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
 import com.ilsangtech.ilsang.feature.ranking.component.BoxWithOverlay
 import com.ilsangtech.ilsang.feature.ranking.component.MyRankCard
 import com.ilsangtech.ilsang.feature.ranking.component.RankingDetailAreaInfoCard
@@ -122,30 +133,68 @@ private fun RankingDetailScreen(
                             color = Color.Black
                         )
                     }
-                    item {
-                        MyRankCard(
-                            imageId = myRankUiModel.profileImageId,
-                            nickname = myRankUiModel.nickname,
-                            titleName = myRankUiModel.titleName,
-                            titleGrade = myRankUiModel.titleGrade,
-                            point = myRankUiModel.point,
-                            rank = myRankUiModel.rank,
-                            requiredPoint = myRankUiModel.pointGap
-                        )
-                    }
-                    item { Spacer(Modifier.height(12.dp)) }
-                    itemsIndexed(userRankList) { index, item ->
-                        UserRankItem(
-                            imageId = item.profileImageId,
-                            nickname = item.nickname,
-                            titleName = item.titleName,
-                            titleGrade = item.titleGrade,
-                            point = item.point,
-                            rank = item.rank,
-                            onClick = { onUserItemClick(item) }
-                        )
-                        if (index != userRankList.lastIndex) {
-                            Spacer(Modifier.height(12.dp))
+                    if (userRankList.isEmpty()) {
+                        item {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(230.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Column(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.spacedBy(6.dp)
+                                ) {
+                                    Text(
+                                        text = "퀘스트를 수행한 유저가 없어요",
+                                        style = TextStyle(
+                                            fontFamily = pretendardFontFamily,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 21.sp,
+                                            lineHeight = 1.5.em
+                                        ),
+                                        color = gray500
+                                    )
+                                    Text(
+                                        text = "랭킹에 가장 먼저 이름을 올려보세요!",
+                                        style = TextStyle(
+                                            fontFamily = pretendardFontFamily,
+                                            fontWeight = FontWeight.Medium,
+                                            fontSize = 17.sp,
+                                            lineHeight = 1.5.em
+                                        ),
+                                        color = gray400
+                                    )
+                                }
+                            }
+                        }
+                    } else {
+                        item {
+                            MyRankCard(
+                                imageId = myRankUiModel.profileImageId,
+                                nickname = myRankUiModel.nickname,
+                                titleName = myRankUiModel.titleName,
+                                titleGrade = myRankUiModel.titleGrade,
+                                point = myRankUiModel.point,
+                                rank = myRankUiModel.rank,
+                                requiredPoint = myRankUiModel.pointGap
+                            )
+                        }
+                        item { Spacer(Modifier.height(12.dp)) }
+                        itemsIndexed(userRankList) { index, item ->
+                            UserRankItem(
+                                imageId = item.profileImageId,
+                                nickname = item.nickname,
+                                titleName = item.titleName,
+                                titleGrade = item.titleGrade,
+                                point = item.point,
+                                rank = item.rank,
+                                onClick = { onUserItemClick(item) }
+                            )
+                            if (index != userRankList.lastIndex) {
+                                Spacer(Modifier.height(12.dp))
+                            }
                         }
                     }
                 }
