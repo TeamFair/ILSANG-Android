c84cff94a6dd8ded27882dff679d2396a59e501b
swkim1 <swkim1@hds-secu.com>
2025-09-02 09:14:01 +0900

    [Feat/#151] MyAreaRank 도메인 모델로 매핑하는 로직 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/mapper/RankMapper.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/rank/mapper/RankMapper.kt
@@ -1,7 +1,9 @@
 package com.ilsangtech.ilsang.core.data.rank.mapper
 
 import com.ilsangtech.ilsang.core.data.title.mapper.toTitle
+import com.ilsangtech.ilsang.core.model.rank.MyAreaRank
 import com.ilsangtech.ilsang.core.model.rank.UserRank
+import com.ilsangtech.ilsang.core.network.model.rank.MyAreaRankNetworkModel
 import com.ilsangtech.ilsang.core.network.model.rank.UserRankNetworkModel
 
 internal fun UserRankNetworkModel.toUserRank(): UserRank {
@@ -12,5 +14,17 @@ internal fun UserRankNetworkModel.toUserRank(): UserRank {
         profileImageId = profileImageId,
         rank = rank,
         title = title?.toTitle()
+    )
+}
+
+internal fun MyAreaRankNetworkModel.toMyAreaRank(): MyAreaRank {
+    return MyAreaRank(
+        userId = userId,
+        nickName = nickName,
+        point = point,
+        profileImageId = profileImageId,
+        rank = rank,
+        title = title?.toTitle(),
+        pointGap = pointGap
     )
 }
