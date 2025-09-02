2a882feea14e4d5460620fe1d942fd18a1adfe9f
kimseongwoo <tjddn929@naver.com>
2025-09-02 21:18:58 +0900

    [Feat/#160] DateConverter.kt에서 timeAgoSinceDate 함수 삭제, formatDate 함수에서 타임존을 Asia/Seoul로 변경

--- core/util/src/main/java/com/ilsangtech/ilsang/core/util/DateConverter.kt
+++ core/util/src/main/java/com/ilsangtech/ilsang/core/util/DateConverter.kt
@@ -1,47 +1,18 @@
 package com.ilsangtech.ilsang.core.util
 
 import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
 object DateConverter {
-    fun timeAgoSinceDate(dateString: String, withYear: Boolean = true): String {
-        val datePart = dateString.split(".").firstOrNull() ?: return ""
-
-        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
-        inputFormat.timeZone = TimeZone.getTimeZone("UTC")
-
-        val date: Date = try {
-            inputFormat.parse(datePart) ?: return ""
-        } catch (e: Exception) {
-            return ""
-        }
-
-        val calendar = Calendar.getInstance()
-        val currentYear = calendar.get(Calendar.YEAR)
-
-        calendar.time = date
-        val dateYear = calendar.get(Calendar.YEAR)
-
-        val outputFormat = when {
-            withYear && currentYear != dateYear -> SimpleDateFormat(
-                "yyyy년 M월 d일",
-                Locale.getDefault()
-            )
-
-            else -> SimpleDateFormat("M월 d일", Locale.getDefault())
-        }
-        return outputFormat.format(date)
-    }
-
     fun formatDate(input: String, outputPattern: String = "yyyy.MM.dd"): String {
         return try {
             val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault()).apply {
-                timeZone = TimeZone.getTimeZone("UTC")
+                timeZone = TimeZone.getTimeZone("Asia/Seoul")
             }
-            val outputFormat = SimpleDateFormat(outputPattern, Locale.getDefault())
+            val outputFormat = SimpleDateFormat(outputPattern, Locale.getDefault()).apply {
+                timeZone = TimeZone.getTimeZone("Asia/Seoul")
+            }
             val date = inputFormat.parse(input)
             outputFormat.format(date!!)
         } catch (_: Exception) {
