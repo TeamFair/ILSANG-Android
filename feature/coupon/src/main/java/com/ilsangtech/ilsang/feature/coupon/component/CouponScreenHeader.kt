bacb990063942046887a1a6b2b1de7dc21f49039
swkim1 <swkim1@hds-secu.com>
2025-09-03 09:48:04 +0900

    [Feat/#141] 쿠폰 화면 헤더 UI 구현

---
+++ feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/component/CouponScreenHeader.kt
@@ -0,0 +1,66 @@
+package com.ilsangtech.ilsang.feature.coupon.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ilsangtech.ilsang.designsystem.R
+import com.ilsangtech.ilsang.designsystem.theme.gray500
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
+import com.ilsangtech.ilsang.designsystem.theme.toSp
+
+@Composable
+internal fun CouponScreenHeader(
+    modifier: Modifier = Modifier,
+    onBackButtonClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(top = 12.dp, bottom = 11.dp)
+    ) {
+        Icon(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 15.dp)
+                .clickable(
+                    onClick = onBackButtonClick,
+                    indication = null,
+                    interactionSource = null
+                ),
+            tint = gray500,
+            painter = painterResource(R.drawable.icon_chevron_back),
+            contentDescription = "뒤로 가기"
+        )
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "쿠폰",
+            style = TextStyle(
+                fontFamily = pretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 17.dp.toSp(),
+                lineHeight = 22.dp.toSp()
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CouponScreenHeaderPreview() {
+    CouponScreenHeader(
+        onBackButtonClick = {}
+    )
+}
