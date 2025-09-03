deb72f7f66f3beed0d286814eb418c78406ed3dc
swkim1 <swkim1@hds-secu.com>
2025-09-03 15:50:01 +0900

    [Feat/#141] 뷰모델의 쿠폰 탭 상태를 기반으로 Screen에 헤더와 탭 UI 적용

--- feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/CouponScreen.kt
+++ feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/CouponScreen.kt
@@ -1,7 +1,17 @@
 package com.ilsangtech.ilsang.feature.coupon
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ilsangtech.ilsang.designsystem.theme.background
+import com.ilsangtech.ilsang.feature.coupon.component.CouponScreenHeader
+import com.ilsangtech.ilsang.feature.coupon.component.CouponTabRow
+import com.ilsangtech.ilsang.feature.coupon.model.CouponTab
 
 @Composable
 internal fun CouponScreen(
@@ -9,7 +19,11 @@ internal fun CouponScreen(
     onQuestNavButtonClick: () -> Unit,
     onBackButtonClick: () -> Unit
 ) {
+    val selectedCouponTab by viewModel.selectedCouponTab.collectAsStateWithLifecycle()
+
     CouponScreen(
+        selectedCouponTab = selectedCouponTab,
+        onCouponTabSelected = viewModel::updateSelectedCouponTab,
         onQuestNavButtonClick = onQuestNavButtonClick,
         onBackButtonClick = onBackButtonClick
     )
@@ -17,7 +31,21 @@ internal fun CouponScreen(
 
 @Composable
 private fun CouponScreen(
+    selectedCouponTab: CouponTab,
+    onCouponTabSelected: (CouponTab) -> Unit,
     onQuestNavButtonClick: () -> Unit,
     onBackButtonClick: () -> Unit
 ) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = background
+    ) {
+        Column {
+            CouponScreenHeader(onBackButtonClick = onBackButtonClick)
+            CouponTabRow(
+                selectedTab = selectedCouponTab,
+                onTabSelected = onCouponTabSelected
+            )
+        }
+    }
 }
