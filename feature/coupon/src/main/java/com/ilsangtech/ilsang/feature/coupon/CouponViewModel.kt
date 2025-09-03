2e3db9882df328782a499c771862730f98c96ad4
swkim1 <swkim1@hds-secu.com>
2025-09-03 14:05:16 +0900

    [Feat/#141] CouponViewModel 클래스 생성

---
+++ feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/CouponViewModel.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.feature.coupon
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CouponViewModel @Inject constructor() : ViewModel() {
+}
