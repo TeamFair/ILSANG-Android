ad0cc533cf07772022474e2881a81b1e92725546
swkim1 <swkim1@hds-secu.com>
2025-09-03 15:29:54 +0900

    [Feat/#141] 선택한 쿠폰 탭 UI 상태 생성, 변경 함수 구현

--- feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/CouponViewModel.kt
+++ feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/CouponViewModel.kt
@@ -1,9 +1,19 @@
 package com.ilsangtech.ilsang.feature.coupon
 
 import androidx.lifecycle.ViewModel
+import com.ilsangtech.ilsang.feature.coupon.model.CouponTab
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class CouponViewModel @Inject constructor() : ViewModel() {
+    private val _selectedCouponTab = MutableStateFlow(CouponTab.Available)
+    val selectedCouponTab = _selectedCouponTab.asStateFlow()
+
+    fun updateSelectedCouponTab(tab: CouponTab) {
+        _selectedCouponTab.update { tab }
+    }
 }
