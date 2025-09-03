ef996eb3ea0f2a3bb30df5b33a948359c4e41808
swkim1 <swkim1@hds-secu.com>
2025-09-03 14:05:28 +0900

    [Feat/#141] CouponNavigation 파일 생성, 네비게이션 라우트 구현

---
+++ feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/navigation/CouponNavigation.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.feature.coupon.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object CouponBaseRoute
+
+@Serializable
+data object CouponRoute
