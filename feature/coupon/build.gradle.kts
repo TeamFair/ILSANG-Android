6baa55c3b38e15e4ea760bcdc22e55a9a5100b85
swkim1 <swkim1@hds-secu.com>
2025-09-03 16:04:33 +0900

    [Chore/#141] coupon 모듈에 paging compose 라이브러리 의존성 추가

--- feature/coupon/build.gradle.kts
+++ feature/coupon/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 
 dependencies {
     implementation(project(":core:designsystem"))
+    implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
