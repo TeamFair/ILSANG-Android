fed70a2f2f792441b40af8adeb610dc8ae046487
swkim1 <swkim1@hds-secu.com>
2025-09-04 16:24:50 +0900

    [Feat/#140] 쿠폰 비밀번호 검증 API 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CouponApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CouponApiService.kt
@@ -1,9 +1,20 @@
 package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.coupon.CouponDetailNetworkModel
+import com.ilsangtech.ilsang.core.network.model.coupon.CouponPasswordVerifyRequest
+import com.ilsangtech.ilsang.core.network.model.coupon.CouponPasswordVerifyResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface CouponApiService {
     @GET("api/v1/user/coupon")
     suspend fun getCouponList(page: Int, size: Int): List<CouponDetailNetworkModel>
+
+    @POST("api/v1/user/coupon/{id}/verify-password")
+    suspend fun verifyPassword(
+        @Path("id") id: Int,
+        @Body password: CouponPasswordVerifyRequest
+    ): CouponPasswordVerifyResponse
 }
