6f1d0506af74f3bf00d02ff0ef4c4725f80cf55b
swkim1 <swkim1@hds-secu.com>
2025-09-04 16:31:55 +0900

    [Feat/#140] 쿠폰 수정 API 요청 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CouponApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CouponApiService.kt
@@ -3,9 +3,11 @@ package com.ilsangtech.ilsang.core.network.api
 import com.ilsangtech.ilsang.core.network.model.coupon.CouponDetailNetworkModel
 import com.ilsangtech.ilsang.core.network.model.coupon.CouponPasswordVerifyRequest
 import com.ilsangtech.ilsang.core.network.model.coupon.CouponPasswordVerifyResponse
+import com.ilsangtech.ilsang.core.network.model.coupon.CouponUpdateRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface CouponApiService {
@@ -17,4 +19,10 @@ interface CouponApiService {
         @Path("id") id: Int,
         @Body password: CouponPasswordVerifyRequest
     ): CouponPasswordVerifyResponse
+
+    @PUT("api/v1/user/coupon/{id}")
+    suspend fun updateCoupon(
+        @Path("id") id: Int,
+        @Body updateRequest: CouponUpdateRequest
+    ): CouponDetailNetworkModel
 }
