2a0d026c4219724976f74d567541b11a53875e58
swkim1 <swkim1@hds-secu.com>
2025-09-04 15:49:31 +0900

    [Feat/#140] 쿠폰 리스트 조회 응답 DTO 클래스 생성

---
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/coupon/CouponNetworkModel.kt
@@ -0,0 +1,13 @@
+package com.ilsangtech.ilsang.core.network.model.coupon
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CouponNetworkModel(
+    val couponId: Int,
+    val couponType: String,
+    val storeName: String,
+    val imageId: String?,
+    val name: String,
+    val description: String?
+)
