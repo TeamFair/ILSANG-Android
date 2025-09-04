77ba522faf644e9efe90f43ccd814d4b555e9ef1
swkim1 <swkim1@hds-secu.com>
2025-09-04 15:54:14 +0900

    [Feat/#140] 쿠폰 리스트 조회 API 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CouponApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CouponApiService.kt
@@ -1,4 +1,9 @@
 package com.ilsangtech.ilsang.core.network.api
 
+import com.ilsangtech.ilsang.core.network.model.coupon.CouponDetailNetworkModel
+import retrofit2.http.GET
+
 interface CouponApiService {
+    @GET("api/v1/user/coupon")
+    suspend fun getCouponList(page: Int, size: Int): List<CouponDetailNetworkModel>
 }
