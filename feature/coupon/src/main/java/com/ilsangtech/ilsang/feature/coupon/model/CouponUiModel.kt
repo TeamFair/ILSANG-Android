8986b93b8bc173a137f0d18fab81465f2b106d10
swkim1 <swkim1@hds-secu.com>
2025-09-05 09:14:56 +0900

    [Feat/#141] 쿠폰 도메인 클래스를 UI 모델로 매핑하는 함수 구현

--- feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/model/CouponUiModel.kt
+++ feature/coupon/src/main/java/com/ilsangtech/ilsang/feature/coupon/model/CouponUiModel.kt
@@ -1,5 +1,8 @@
 package com.ilsangtech.ilsang.feature.coupon.model
 
+import com.ilsangtech.ilsang.core.model.coupon.Coupon
+import com.ilsangtech.ilsang.core.util.DateConverter
+
 data class CouponUiModel(
     val id: Int,
     val title: String,
@@ -11,4 +14,25 @@ data class CouponUiModel(
     val isExpired: Boolean
 ) {
     val isAvailable: Boolean = !isUsed && !isExpired
+}
+
+internal fun Coupon.toUiModel(): CouponUiModel {
+    return CouponUiModel(
+        id = id,
+        title = name,
+        writerName = storeName,
+        imageId = imageId,
+        expireDate = DateConverter.formatDate(
+            input = validTo,
+            outputPattern = "yyyy.MM.dd HH:mm"
+        ),
+        usedDate = usedAt?.let {
+            DateConverter.formatDate(
+                input = it,
+                outputPattern = "yyyy.MM.dd"
+            )
+        },
+        isUsed = couponUseYn,
+        isExpired = couponExpireYn
+    )
 }
