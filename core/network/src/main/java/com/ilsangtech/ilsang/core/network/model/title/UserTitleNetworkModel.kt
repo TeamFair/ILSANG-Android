d5e9fd0cf1fe7cc9f90e8608e7ac44637c87090f
kimseongwoo <tjddn929@naver.com>
2025-09-07 22:42:45 +0900

    [Fix/#193] 사용자 칭호 DTO 클래스에 userId 추가

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/title/UserTitleNetworkModel.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/model/title/UserTitleNetworkModel.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UserTitleNetworkModel(
     val titleHistoryId: Int,
+    val userId: String,
     val name: String,
     val grade: String,
     val type: String
