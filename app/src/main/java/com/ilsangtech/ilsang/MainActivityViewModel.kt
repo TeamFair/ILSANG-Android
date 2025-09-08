c4661d3e8895da614495769f88aafec6ed5d6bc6
swkim1 <swkim1@hds-secu.com>
2025-09-08 13:09:45 +0900

    [Feat/#197] MainActivityViewModel 내 읽지 않은 칭호 리스트 상태 관리 구현

--- app/src/main/java/com/ilsangtech/ilsang/MainActivityViewModel.kt
+++ app/src/main/java/com/ilsangtech/ilsang/MainActivityViewModel.kt
@@ -3,7 +3,9 @@ package com.ilsangtech.ilsang
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ilsangtech.ilsang.core.domain.AuthRepository
+import com.ilsangtech.ilsang.core.domain.TitleRepository
 import com.ilsangtech.ilsang.core.domain.UserRepository
+import com.ilsangtech.ilsang.core.model.title.UserTitle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -22,7 +24,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val authRepository: AuthRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val titleRepository: TitleRepository
 ) : ViewModel() {
     private val loginTrigger = MutableSharedFlow<Unit>(replay = 1)
     private val _shouldShowIsZoneDialog = MutableStateFlow(false)
@@ -35,6 +38,7 @@ class MainActivityViewModel @Inject constructor(
                 if (myInfo.isCommercialAreaCode == null && myInfo.showIsZoneDialogAgain) {
                     _shouldShowIsZoneDialog.update { true }
                 }
+                _unreadTitleList.update { titleRepository.getUnreadTitleList() }
             }
             .map { true }
             .catch { emit(false) }
@@ -49,6 +53,9 @@ class MainActivityViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = false
     )
+
+    private val _unreadTitleList = MutableStateFlow<List<UserTitle>>(emptyList())
+    val unreadTitleList = _unreadTitleList.asStateFlow()
 
     init {
         viewModelScope.launch {
@@ -82,6 +89,15 @@ class MainActivityViewModel @Inject constructor(
                 userRepository.updateShowIsZoneDialogAgain(false)
             }
             _shouldShowIsZoneDialog.update { false }
+        }
+    }
+
+    fun readTitle(titleHistoryId: Int) {
+        viewModelScope.launch {
+            titleRepository.readTitle(titleHistoryId)
+            _unreadTitleList.update { list ->
+                list.filter { it.titleHistoryId != titleHistoryId }
+            }
         }
     }
 }
