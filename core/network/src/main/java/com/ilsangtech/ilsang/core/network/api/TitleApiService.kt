56d46d3966e45d80713443ff8d1dd7654687c928
swkim1 <swkim1@hds-secu.com>
2025-09-08 11:45:21 +0900

    [Feat/#197] TitleApiService 내 칭호 읽음 처리 API 호출 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/TitleApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/TitleApiService.kt
@@ -3,6 +3,8 @@ package com.ilsangtech.ilsang.core.network.api
 import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
 import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
 import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface TitleApiService {
     @GET("api/v1/title")
@@ -13,4 +15,7 @@ interface TitleApiService {
 
     @GET("api/v1/user/title/unread")
     suspend fun getUnreadTitleList(): List<UserTitleNetworkModel>
+
+    @PUT("api/v1/user/title/{id}/read")
+    suspend fun readTitle(@Path("id") titleHistoryId: Int)
 }
