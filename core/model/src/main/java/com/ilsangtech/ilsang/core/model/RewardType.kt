71d031e2ac6f2b5266264283682d4758d356f44c
swkim1 <swkim1@hds-secu.com>
2025-09-08 16:55:58 +0900

    [Refactor/#203] 사용하지 않는 Reward, RewardType 도메인 모델, 참조하는 컴포저블 함수 제거

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/RewardType.kt
+++
@@ -1,9 +0,0 @@
-package com.ilsangtech.ilsang.core.model
-
-enum class RewardType(val title: String) {
-    STRENGTH("체력"),
-    INTELLECT("지능"),
-    CHARM("매력"),
-    FUN("재미"),
-    SOCIABILITY("사회성")
-}
