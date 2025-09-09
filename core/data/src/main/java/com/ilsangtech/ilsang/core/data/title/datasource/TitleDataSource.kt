fae9dbef755b44db1868a52b8a12deb35f236de4
kimseongwoo <tjddn929@naver.com>
2025-09-10 00:01:28 +0900

    [Feat/#199] PagingDataSource 내 전설 칭호 페이징 데이터 조회 함수 생성 및 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/datasource/TitleDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/datasource/TitleDataSource.kt
@@ -1,7 +1,10 @@
 package com.ilsangtech.ilsang.core.data.title.datasource
 
+import androidx.paging.PagingData
+import com.ilsangtech.ilsang.core.network.model.title.LegendTitleRankNetworkModel
 import com.ilsangtech.ilsang.core.network.model.title.TitleDetailNetworkModel
 import com.ilsangtech.ilsang.core.network.model.title.UserTitleNetworkModel
+import kotlinx.coroutines.flow.Flow
 
 interface TitleDataSource {
     suspend fun getTitleList(): List<TitleDetailNetworkModel>
@@ -11,4 +14,6 @@ interface TitleDataSource {
     suspend fun getUnreadTitleList(): List<UserTitleNetworkModel>
 
     suspend fun readTitle(titleHistoryId: Int)
+
+    fun getLegendTitleRankList(titleId: String): Flow<PagingData<LegendTitleRankNetworkModel>>
 }
