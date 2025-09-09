1b38f2c56b4df99e15b520820bea883498d0fac1
kimseongwoo <tjddn929@naver.com>
2025-09-09 23:38:52 +0900

    [Feat/#199] LegendTitleViewModel 클래스 생성

---
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/legend_title/LegendTitleViewModel.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.feature.my.screens.legend_title
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LegendTitleViewModel @Inject constructor() : ViewModel() {
+}
