1924841afc07a443c12c3ec3fcec777248c1e7c5
swkim1 <swkim1@hds-secu.com>
2025-09-10 17:51:08 +0900

    [Feat/#199] 전설 칭호 도메인 모델 매핑 함수 구현

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/title/mapper/LegendTitleMapper.kt
@@ -0,0 +1,32 @@
+package com.ilsangtech.ilsang.core.data.title.mapper
+
+import com.ilsangtech.ilsang.core.model.title.LegendTitle
+import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.model.title.TitleGrade
+import com.ilsangtech.ilsang.core.model.title.TitleType
+import com.ilsangtech.ilsang.core.network.model.title.LegendTitleRankNetworkModel
+
+internal fun LegendTitleRankNetworkModel.toLegendTitle(): LegendTitle {
+    return LegendTitle(
+        rank = rank,
+        nickname = nickName,
+        profileImageId = profileImageId,
+        userPoint = point,
+        title = Title(
+            name = title.name,
+            grade = when (title.grade) {
+                "STANDARD" -> TitleGrade.Standard
+                "RARE" -> TitleGrade.Rare
+                "LEGEND" -> TitleGrade.Legend
+                else -> throw IllegalArgumentException("Unknown title grade: ${title.grade}")
+            },
+            type = when (title.type) {
+                "METRO" -> TitleType.Metro
+                "COMMERCIAL" -> TitleType.Commercial
+                "CONTRIBUTION" -> TitleType.Contribution
+                else -> throw IllegalArgumentException("Unknown title type: ${title.grade}")
+            }
+        ),
+        createdAt = title.createdAt
+    )
+}
