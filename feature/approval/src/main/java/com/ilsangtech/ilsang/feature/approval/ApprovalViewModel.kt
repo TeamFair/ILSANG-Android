2e69a59e267200c5820853e01eec21e743ee4bff
kimseongwoo <tjddn929@naver.com>
2025-09-10 21:45:37 +0900

    [Fix/#218] 좋아요, 싫어요 등록 및 해제 성공 시 미션 이력 아이디를 추가, 삭제하여 UI 모델을 업데이트하도록 로직 변경

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalViewModel.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalViewModel.kt
@@ -10,8 +10,11 @@ import com.ilsangtech.ilsang.feature.approval.model.MissionHistoryUiModel
 import com.ilsangtech.ilsang.feature.approval.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,27 +26,87 @@ class ApprovalViewModel @Inject constructor(
     private val _missionHistoryRefreshTrigger = MutableSharedFlow<Unit>(replay = 1)
     val missionHistoryRefreshTrigger = _missionHistoryRefreshTrigger.asSharedFlow()
 
+    private val likeMissionHistorySet = MutableStateFlow(setOf<Int>())
+    private val hateMissionHistorySet = MutableStateFlow(setOf<Int>())
+
     val randomMissionHistories = missionRepository.getRandomMissionHistory().map { pagingData ->
         pagingData.map { randomMissionHistory ->
+            if (randomMissionHistory.currentUserEmojis.contains("LIKE")) {
+                likeMissionHistorySet.update { it + randomMissionHistory.missionHistoryId }
+            }
+            if (randomMissionHistory.currentUserEmojis.contains("HATE")) {
+                hateMissionHistorySet.update { it + randomMissionHistory.missionHistoryId }
+            }
+
             val commercialArea =
                 areaRepository.getCommercialArea(randomMissionHistory.commercialAreaCode)
             randomMissionHistory.toUiModel(commercialArea.areaName)
         }
     }.cachedIn(viewModelScope)
+        .combine(likeMissionHistorySet) { pagingData, likeMissionHistorySet ->
+            pagingData.map { missionHistory ->
+                when {
+                    likeMissionHistorySet.contains(missionHistory.missionHistoryId)
+                            && !missionHistory.currentUserEmojis.contains("LIKE") -> {
+                        missionHistory.copy(
+                            currentUserEmojis = missionHistory.currentUserEmojis + "LIKE",
+                            likeCount = missionHistory.likeCount + 1
+                        )
+                    }
+
+                    !likeMissionHistorySet.contains(missionHistory.missionHistoryId)
+                            && missionHistory.currentUserEmojis.contains("LIKE") -> {
+                        missionHistory.copy(
+                            currentUserEmojis = missionHistory.currentUserEmojis - "LIKE",
+                            likeCount = missionHistory.likeCount - 1
+                        )
+                    }
+
+                    else -> {
+                        missionHistory
+                    }
+                }
+            }
+        }
+        .combine(hateMissionHistorySet) { pagingData, hateMissionHistorySet ->
+            pagingData.map { missionHistory ->
+                when {
+                    hateMissionHistorySet.contains(missionHistory.missionHistoryId)
+                            && !missionHistory.currentUserEmojis.contains("HATE") -> {
+                        missionHistory.copy(
+                            currentUserEmojis = missionHistory.currentUserEmojis + "HATE",
+                            hateCount = missionHistory.hateCount + 1
+                        )
+                    }
+
+                    !hateMissionHistorySet.contains(missionHistory.missionHistoryId)
+                            && missionHistory.currentUserEmojis.contains("HATE") -> {
+                        missionHistory.copy(
+                            currentUserEmojis = missionHistory.currentUserEmojis - "HATE",
+                            hateCount = missionHistory.hateCount - 1
+                        )
+                    }
+
+                    else -> {
+                        missionHistory
+                    }
+                }
+            }
+        }
 
     fun likeChallenge(missionHistory: MissionHistoryUiModel) {
         viewModelScope.launch {
             val missionHistoryId = missionHistory.missionHistoryId
             val emojiTypes = missionHistory.currentUserEmojis
 
-            val result = if (!emojiTypes.contains("LIKE")) {
-                missionRepository.likeMissionHistory(missionHistoryId)
+            if (!emojiTypes.contains("LIKE")) {
+                missionRepository.likeMissionHistory(missionHistoryId).onSuccess {
+                    likeMissionHistorySet.update { it + missionHistoryId }
+                }
             } else {
-                missionRepository.unlikeMissionHistory(missionHistoryId)
-            }
-
-            result.onSuccess {
-                _missionHistoryRefreshTrigger.emit(Unit)
+                missionRepository.unlikeMissionHistory(missionHistoryId).onSuccess {
+                    likeMissionHistorySet.update { it - missionHistoryId }
+                }
             }
         }
     }
@@ -53,14 +116,14 @@ class ApprovalViewModel @Inject constructor(
             val missionHistoryId = missionHistory.missionHistoryId
             val emojiTypes = missionHistory.currentUserEmojis
 
-            val result = if (!emojiTypes.contains("HATE")) {
-                missionRepository.hateMissionHistory(missionHistoryId)
+            if (!emojiTypes.contains("HATE")) {
+                missionRepository.hateMissionHistory(missionHistoryId).onSuccess {
+                    hateMissionHistorySet.update { it + missionHistoryId }
+                }
             } else {
-                missionRepository.unhateMissionHistory(missionHistoryId)
-            }
-
-            result.onSuccess {
-                _missionHistoryRefreshTrigger.emit(Unit)
+                missionRepository.unhateMissionHistory(missionHistoryId).onSuccess {
+                    hateMissionHistorySet.update { it - missionHistoryId }
+                }
             }
         }
     }
