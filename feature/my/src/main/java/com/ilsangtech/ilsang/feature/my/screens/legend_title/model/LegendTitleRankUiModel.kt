8d46a8d7928d0dac74402b88edd3ff04e368ca1c
kimseongwoo <tjddn929@naver.com>
2025-09-11 22:56:47 +0900

    [Feat/#199] LegendTitle을 LegendTitleRankUiModel로 매핑하는 로직 구현

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/legend_title/model/LegendTitleRankUiModel.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/legend_title/model/LegendTitleRankUiModel.kt
@@ -1,6 +1,9 @@
 package com.ilsangtech.ilsang.feature.my.screens.legend_title.model
 
+import com.ilsangtech.ilsang.core.model.title.LegendTitle
 import com.ilsangtech.ilsang.core.model.title.Title
+import com.ilsangtech.ilsang.core.util.DateConverter
+import com.ilsangtech.ilsang.core.util.XpLevelCalculator
 
 data class LegendTitleRankUiModel(
     val rank: Int,
@@ -10,3 +13,14 @@ data class LegendTitleRankUiModel(
     val title: Title,
     val createdAt: String
 )
+
+internal fun LegendTitle.toUiModel(): LegendTitleRankUiModel {
+    return LegendTitleRankUiModel(
+        rank = rank,
+        nickname = nickname,
+        profileImageId = profileImageId,
+        level = XpLevelCalculator.getCurrentLevel(userPoint),
+        title = title,
+        createdAt = DateConverter.formatDate(createdAt)
+    )
+}
