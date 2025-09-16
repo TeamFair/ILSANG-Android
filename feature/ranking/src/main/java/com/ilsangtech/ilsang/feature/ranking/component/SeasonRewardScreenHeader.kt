f09881fcbfc5a3043dd176faafe1762784d771ff
kimseongwoo <tjddn929@naver.com>
2025-09-16 22:41:18 +0900

    [Feat/#240] 시즌 보상 화면 헤더 UI 구현

---
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/component/SeasonRewardScreenHeader.kt
@@ -0,0 +1,65 @@
+package com.ilsangtech.ilsang.feature.ranking.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ilsangtech.ilsang.designsystem.R
+import com.ilsangtech.ilsang.designsystem.theme.gray500
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
+
+@Composable
+internal fun SeasonRewardScreenHeader(
+    modifier: Modifier = Modifier,
+    onBackButtonClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(top = 12.dp, bottom = 11.dp)
+    ) {
+        Icon(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 15.dp)
+                .clickable(
+                    onClick = onBackButtonClick,
+                    indication = null,
+                    interactionSource = null
+                ),
+            painter = painterResource(R.drawable.icon_chevron_back),
+            tint = gray500,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "시즌 보상",
+            style = TextStyle(
+                fontFamily = pretendardFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 17.sp,
+                lineHeight = 22.sp
+            ),
+            color = gray500
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SeasonRewardScreenHeaderPreview() {
+    SeasonRewardScreenHeader(onBackButtonClick = {})
+}
