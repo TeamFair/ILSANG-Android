99b8dae48aa2022eeb3c4fab3a13a8d772b85d05
swkim1 <swkim1@hds-secu.com>
2025-09-16 17:56:36 +0900

    [Feat/#240] 시즌 보상 화면에 뷰모델 적용 및 Preview 함수 생성

--- feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/SeasonRewardScreen.kt
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/SeasonRewardScreen.kt
@@ -1,7 +1,21 @@
 package com.ilsangtech.ilsang.feature.ranking
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-internal fun SeasonRewardScreen() {
+internal fun SeasonRewardScreen(
+    viewModel: SeasonRewardViewModel = hiltViewModel()
+) {
+    SeasonRewardScreen()
+}
+
+@Composable
+private fun SeasonRewardScreen(
+) {
+}
+
+@Composable
+private fun SeasonRewardScreenPreview() {
+    SeasonRewardScreen()
 }
