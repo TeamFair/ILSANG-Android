d3b62788828d67adbbda764a139a1d274afb6408
swkim1 <swkim1@hds-secu.com>
2025-09-17 15:34:25 +0900

    [Feat/#240] 시즌 보상 칭호 도메인 모델을 UI 모델로 매핑하는 로직 구현

--- feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/SeasonRewardTitleUiModel.kt
+++ feature/ranking/src/main/java/com/ilsangtech/ilsang/feature/ranking/model/SeasonRewardTitleUiModel.kt
@@ -1,5 +1,6 @@
 package com.ilsangtech.ilsang.feature.ranking.model
 
+import com.ilsangtech.ilsang.core.model.title.SeasonRewardTitle
 import com.ilsangtech.ilsang.core.model.title.Title
 
 sealed class SeasonRewardTitleUiModel(
@@ -17,4 +18,34 @@ sealed class SeasonRewardTitleUiModel(
         val rankRange: IntRange,
         override val title: Title
     ) : SeasonRewardTitleUiModel(id, title)
+}
+
+internal fun SeasonRewardTitle.toUiModel(): SeasonRewardTitleUiModel {
+    val singleRankRegex = Regex("""(\d+)등""")
+    val rangeRankRegex = Regex("""(\d+)~(\d+)등""")
+
+    return when {
+        rangeRankRegex.find(condition) != null -> {
+            val match = rangeRankRegex.find(condition)!!
+            val start = match.groupValues[1].toInt()
+            val end = match.groupValues[2].toInt()
+            SeasonRewardTitleUiModel.OtherRewardTitle(
+                id = id,
+                rankRange = start..end,
+                title = title
+            )
+        }
+
+        singleRankRegex.find(condition) != null -> {
+            val match = singleRankRegex.find(condition)!!
+            val rank = match.groupValues[1].toInt()
+            SeasonRewardTitleUiModel.TopRewardTitle(
+                id = id,
+                rank = rank,
+                title = title
+            )
+        }
+
+        else -> throw IllegalArgumentException("Invalid condition: $condition")
+    }
 }
