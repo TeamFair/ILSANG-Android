104fd69ab54cd97c3823b39a8036e3d5998762a1
swkim1 <swkim1@hds-secu.com>
2025-10-20 13:56:18 +0900

    [Refactor/#270] Mission의 type 프로퍼티를 MissionType으로 변경, 매핑 함수 변경

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/mission/mapper/MissonMapper.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/mission/mapper/MissonMapper.kt
@@ -1,6 +1,7 @@
 package com.ilsangtech.ilsang.core.data.mission.mapper
 
 import com.ilsangtech.ilsang.core.model.mission.Mission
+import com.ilsangtech.ilsang.core.model.mission.MissionType
 import com.ilsangtech.ilsang.core.network.model.mission.MissionNetworkModel
 
 internal fun MissionNetworkModel.toMission(): Mission {
@@ -8,6 +9,11 @@ internal fun MissionNetworkModel.toMission(): Mission {
         id = id,
         exampleImageIds = exampleImageIds,
         title = title,
-        type = type
+        type = when (type) {
+            "PHOTO" -> MissionType.Photo
+            "OX" -> MissionType.Ox
+            "WORDS" -> MissionType.Words
+            else -> throw IllegalArgumentException("Unknown mission type")
+        }
     )
 }
