3d9e56071fad3396199697029e969c5dac80ecae
swkim1 <swkim1@hds-secu.com>
2025-10-20 13:55:15 +0900

    [Refactor/#270] MissionType에 Quiz 타입 제거

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/MissionType.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/MissionType.kt
@@ -2,8 +2,6 @@ package com.ilsangtech.ilsang.core.model.mission
 
 sealed interface MissionType {
     data object Photo : MissionType
-    sealed interface Quiz : MissionType {
-        data object Ox : Quiz
-        data object Words : Quiz
-    }
+    data object Ox : MissionType
+    data object Words : MissionType
 }
