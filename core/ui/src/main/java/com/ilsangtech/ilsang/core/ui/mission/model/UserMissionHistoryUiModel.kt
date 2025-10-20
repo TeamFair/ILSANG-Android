0a47a12215fb910151e3403b345a169c928e89a7
swkim1 <swkim1@hds-secu.com>
2025-10-20 17:48:07 +0900

    [Feat/#270] UserMissionHistoryUiModel에 Serializable 어노테이션 추가, NavType 타입 변수 구현

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/mission/model/UserMissionHistoryUiModel.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/mission/model/UserMissionHistoryUiModel.kt
@@ -1,10 +1,18 @@
 package com.ilsangtech.ilsang.core.ui.mission.model
 
+import android.os.Bundle
+import androidx.navigation.NavType
 import com.ilsangtech.ilsang.core.model.mission.MissionType
 import com.ilsangtech.ilsang.core.model.mission.UserMissionHistory
 import com.ilsangtech.ilsang.core.model.quest.QuestType
 import com.ilsangtech.ilsang.core.util.DateConverter
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
+@Serializable
 data class UserMissionHistoryUiModel(
     val missionHistoryId: Int,
     val title: String,
@@ -29,3 +37,34 @@ fun UserMissionHistory.toUiModel(): UserMissionHistoryUiModel {
         createdAt = DateConverter.formatDate(input = createdAt)
     )
 }
+
+val userMissionHistoryNavType =
+    object : NavType<UserMissionHistoryUiModel>(isNullableAllowed = false) {
+        override fun get(
+            bundle: Bundle,
+            key: String
+        ): UserMissionHistoryUiModel? {
+            return bundle.getString(key)?.let { Json.decodeFromString(it) }
+        }
+
+        override fun parseValue(value: String): UserMissionHistoryUiModel {
+            val decoded = URLDecoder.decode(value, StandardCharsets.UTF_8.toString())
+            return Json.decodeFromString(decoded)
+        }
+
+        override fun put(
+            bundle: Bundle,
+            key: String,
+            value: UserMissionHistoryUiModel
+        ) {
+            bundle.putString(
+                key,
+                Json.encodeToString(UserMissionHistoryUiModel.serializer(), value)
+            )
+        }
+
+        override fun serializeAsValue(value: UserMissionHistoryUiModel): String {
+            val json = Json.encodeToString(value)
+            return URLEncoder.encode(json, StandardCharsets.UTF_8.toString())
+        }
+    }
