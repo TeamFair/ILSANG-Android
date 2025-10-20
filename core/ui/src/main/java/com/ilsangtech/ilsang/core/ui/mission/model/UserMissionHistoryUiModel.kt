44bb67744e90fcbcd530c142668f363b2bf8ed93
swkim1 <swkim1@hds-secu.com>
2025-10-20 14:05:15 +0900

    [Feat/#270] UserMissionHistoryUiModel에 questType, missionType 프로퍼티 추가

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/mission/model/UserMissionHistoryUiModel.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/mission/model/UserMissionHistoryUiModel.kt
@@ -1,6 +1,8 @@
 package com.ilsangtech.ilsang.core.ui.mission.model
 
+import com.ilsangtech.ilsang.core.model.mission.MissionType
 import com.ilsangtech.ilsang.core.model.mission.UserMissionHistory
+import com.ilsangtech.ilsang.core.model.quest.QuestType
 import com.ilsangtech.ilsang.core.util.DateConverter
 
 data class UserMissionHistoryUiModel(
@@ -10,7 +12,10 @@ data class UserMissionHistoryUiModel(
     val submitImageId: String,
     val viewCount: Int,
     val likeCount: Int,
-    val createdAt: String
+    val createdAt: String,
+    //TODO QuestType, MissionType을 도메인 모델에서 받을 수 있도록 적용 필요
+    val questType: QuestType = QuestType.Repeat.Daily,
+    val missionType: MissionType = MissionType.Photo
 )
 
 fun UserMissionHistory.toUiModel(): UserMissionHistoryUiModel {
