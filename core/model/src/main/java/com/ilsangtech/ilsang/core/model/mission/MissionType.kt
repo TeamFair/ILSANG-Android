e8e5d0d5dfddcc11090db4b3567041ccb95a41b1
swkim1 <swkim1@hds-secu.com>
2025-10-21 12:57:46 +0900

    [Refactor/#270] MissionType에 Serializable 어노테이션 추가

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/MissionType.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/MissionType.kt
@@ -1,7 +1,15 @@
 package com.ilsangtech.ilsang.core.model.mission
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface MissionType {
+    @Serializable
     data object Photo : MissionType
+
+    @Serializable
     data object Ox : MissionType
+
+    @Serializable
     data object Words : MissionType
 }
