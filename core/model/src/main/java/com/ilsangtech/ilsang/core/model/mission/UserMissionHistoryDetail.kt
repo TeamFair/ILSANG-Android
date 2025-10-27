efe1723d9a99d28f96f07858e3b2f7dc0be32eae
swkim1 <swkim1@hds-secu.com>
2025-10-27 15:28:33 +0900

    [Feat/#270] 수행한 퀘스트 상세 이력(UserMissionHistoryDetail) 도메인 모델 클래스 생성

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/UserMissionHistoryDetail.kt
@@ -0,0 +1,22 @@
+package com.ilsangtech.ilsang.core.model.mission
+
+import com.ilsangtech.ilsang.core.model.quest.QuestType
+import com.ilsangtech.ilsang.core.model.quiz.CompletedQuiz
+
+data class UserMissionHistoryDetail(
+    val missionHistoryId: Int,
+    val title: String,
+    val submitImageId: String?,
+    val questImageId: String?,
+    val viewCount: Int,
+    val likeCount: Int,
+    val createdAt: String,
+    val commercialAreaCode: String,
+    val missionType: MissionType,
+    val writerName: String,
+    val commercialGainPoint: Int,
+    val metroGainPoint: Int,
+    val contributionGainPoint: Int,
+    val quiz: CompletedQuiz?,
+    val questType: QuestType
+)
