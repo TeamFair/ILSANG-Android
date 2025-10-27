daf01b8fd121405aa18fbd3bad1ebb52442d39b3
swkim1 <swkim1@hds-secu.com>
2025-10-27 15:59:18 +0900

    [Feat/#270] 수행한 퀘스트 상세 UI 모델 생성 및 MyChallengeDetailUiState 구조 변경

--- feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge_detail/model/MyChallengeDetailUiState.kt
+++ feature/my/src/main/java/com/ilsangtech/ilsang/feature/my/screens/challenge_detail/model/MyChallengeDetailUiState.kt
@@ -1,9 +1,49 @@
 package com.ilsangtech.ilsang.feature.my.screens.challenge_detail.model
 
-data class MyChallengeDetailUiState(
+import com.ilsangtech.ilsang.core.model.mission.MissionType
+import com.ilsangtech.ilsang.core.model.mission.UserMissionHistoryDetail
+import com.ilsangtech.ilsang.core.model.quest.QuestType
+import com.ilsangtech.ilsang.core.model.quiz.CompletedQuiz
+import com.ilsangtech.ilsang.core.util.DateConverter
+
+sealed interface MyChallengeDetailUiState {
+    data class Success(val data: MyChallengeDetailUiModel) : MyChallengeDetailUiState
+    data object Error : MyChallengeDetailUiState
+    object Loading : MyChallengeDetailUiState
+}
+
+data class MyChallengeDetailUiModel(
     val missionHistoryId: Int,
     val title: String,
     val submitImageId: String?,
-    val questImageId: String?,
-    val likeCount: Int
+    val viewCount: Int,
+    val likeCount: Int,
+    val createdAt: String,
+    val commercialAreaCode: String,
+    val missionType: MissionType,
+    val writerName: String,
+    val commercialGainPoint: Int,
+    val metroGainPoint: Int,
+    val contributionGainPoint: Int,
+    val quiz: CompletedQuiz?,
+    val questType: QuestType
 )
+
+internal fun UserMissionHistoryDetail.toUiModel(): MyChallengeDetailUiModel {
+    return MyChallengeDetailUiModel(
+        missionHistoryId = missionHistoryId,
+        title = title,
+        submitImageId = submitImageId,
+        viewCount = viewCount,
+        likeCount = likeCount,
+        createdAt = DateConverter.formatDate(createdAt),
+        commercialAreaCode = commercialAreaCode,
+        missionType = missionType,
+        writerName = writerName,
+        commercialGainPoint = commercialGainPoint,
+        metroGainPoint = metroGainPoint,
+        contributionGainPoint = contributionGainPoint,
+        quiz = quiz,
+        questType = questType
+    )
+}
