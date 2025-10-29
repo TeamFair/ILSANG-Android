69a91d36fed4555e26b679afb6f1715185670688
swkim1 <swkim1@hds-secu.com>
2025-10-29 17:23:08 +0900

    [Feat/#270] 첼린지 상세 화면 헤더에 공유하기 드롭다운이 나타나도록 적용

--- feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/ChallengeScreen.kt
+++ feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/ChallengeScreen.kt
@@ -10,16 +10,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,7 +32,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -73,29 +80,31 @@ private fun ChallengeScreen(
                 .fillMaxSize()
                 .statusBarsPadding()
         ) {
-            ChallengeScreenHeader(onBackButtonClick)
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .background(background),
-                contentPadding = PaddingValues(bottom = 72.dp)
-            ) {
-                when (uiState) {
-                    ChallengeDetailUiState.Loading -> {
-                        item(key = "로딩", contentType = "Loading") {
-                            Box(
-                                modifier = Modifier.fillParentMaxSize(),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                CircularProgressIndicator(color = gray200)
-                            }
-                        }
+            when (uiState) {
+                ChallengeDetailUiState.Loading -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator(color = gray200)
                     }
+                }
 
-                    ChallengeDetailUiState.Error -> {}
-                    is ChallengeDetailUiState.Success -> {
-                        val uiModel = uiState.data
+                ChallengeDetailUiState.Error -> {}
+                is ChallengeDetailUiState.Success -> {
+                    val uiModel = uiState.data
+                    ChallengeScreenHeader(
+                        missionType = uiModel.missionType,
+                        onShareButtonClick = {},
+                        onBackButtonClick = onBackButtonClick
+                    )
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .background(background),
+                        contentPadding = PaddingValues(bottom = 72.dp)
+                    ) {
                         if (uiModel.missionType is MissionType.Photo) {
                             item(key = "퀘스트 수행 이미지", contentType = "Image") {
                                 MissionHistoryDetailPhotoItem(imageId = uiModel.submitImageId)
@@ -136,38 +145,113 @@ private fun ChallengeScreen(
 
 @Composable
 private fun ChallengeScreenHeader(
+    missionType: MissionType,
+    onShareButtonClick: () -> Unit,
     onBackButtonClick: () -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(min = 45.dp)
-            .padding(
-                horizontal = 12.dp,
-                vertical = 7.dp
-            )
-    ) {
-        Icon(
+    var isExpanded by remember { mutableStateOf(false) }
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Box(
             modifier = Modifier
-                .align(Alignment.CenterStart)
-                .clickable(
-                    onClick = onBackButtonClick,
-                    indication = null,
-                    interactionSource = null
-                ),
-            painter = painterResource(R.drawable.icon_back),
-            contentDescription = null
-        )
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "챌린지 정보",
-            style = TextStyle(
-                fontFamily = pretendardFontFamily,
-                fontWeight = FontWeight.Bold,
-                fontSize = 17.sp,
-                color = gray500
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(
+                    horizontal = 12.dp,
+                    vertical = 7.dp
+                )
+        ) {
+            Icon(
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .clickable(
+                        onClick = onBackButtonClick,
+                        indication = null,
+                        interactionSource = null
+                    ),
+                painter = painterResource(R.drawable.icon_back),
+                contentDescription = null
             )
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "챌린지 정보",
+                style = TextStyle(
+                    fontFamily = pretendardFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    color = gray500
+                )
+            )
+            if (missionType == MissionType.Photo) {
+                Icon(
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .clickable(
+                            onClick = { isExpanded = true },
+                            indication = null,
+                            interactionSource = null
+                        ),
+                    painter = painterResource(R.drawable.icon_more_vertical),
+                    tint = gray500,
+                    contentDescription = null
+                )
+            }
+        }
+        ChallengeDropDownMenu(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            isExpanded = isExpanded,
+            onDismissRequest = { isExpanded = false },
+            onShareButtonClick = {
+                isExpanded = false
+                onShareButtonClick()
+            }
         )
+    }
+}
+
+@Composable
+private fun ChallengeDropDownMenu(
+    modifier: Modifier = Modifier,
+    isExpanded: Boolean,
+    onDismissRequest: () -> Unit,
+    onShareButtonClick: () -> Unit
+) {
+    DropdownMenu(
+        modifier = modifier.width(180.dp),
+        expanded = isExpanded,
+        offset = DpOffset(x = (-20).dp, y = 10.dp),
+        shape = RoundedCornerShape(8.dp),
+        onDismissRequest = onDismissRequest,
+        containerColor = Color.White
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(
+                    onClick = onShareButtonClick,
+                    interactionSource = null,
+                    indication = null
+                )
+                .padding(horizontal = 12.dp, vertical = 2.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterStart),
+                style = TextStyle(
+                    fontFamily = pretendardFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 15.sp,
+                    lineHeight = 1.3.em,
+                    color = gray500
+                ),
+                text = "공유하기"
+            )
+            Icon(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                painter = painterResource(R.drawable.share),
+                tint = gray500,
+                contentDescription = null
+            )
+        }
     }
 }
 
