b4fe2104b82bbe5619171cebce3cce998cabf1a6
swkim1 <swkim1@hds-secu.com>
2025-10-29 15:18:26 +0900

    [Feat/#270] 변경된 UI에 맞게 ChallengeScreen UI 구현

--- feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/ChallengeScreen.kt
+++ feature/profile/src/main/java/com/ilsangtech/ilsang/feature/profile/ChallengeScreen.kt
@@ -1,42 +1,68 @@
 package com.ilsangtech.ilsang.feature.profile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil3.compose.AsyncImage
-import com.ilsangtech.ilsang.core.model.Challenge
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ilsangtech.ilsang.core.model.mission.MissionType
+import com.ilsangtech.ilsang.core.model.quest.QuestType
+import com.ilsangtech.ilsang.core.ui.mission.MissionHistoryDetailDateItem
+import com.ilsangtech.ilsang.core.ui.mission.MissionHistoryDetailInfoItem
+import com.ilsangtech.ilsang.core.ui.mission.MissionHistoryDetailPhotoItem
+import com.ilsangtech.ilsang.core.ui.mission.MissionHistoryDetailPointItem
+import com.ilsangtech.ilsang.core.ui.mission.MissionHistoryDetailWriterItem
 import com.ilsangtech.ilsang.designsystem.R
+import com.ilsangtech.ilsang.designsystem.theme.background
+import com.ilsangtech.ilsang.designsystem.theme.gray200
 import com.ilsangtech.ilsang.designsystem.theme.gray500
 import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
-import com.ilsangtech.ilsang.feature.profile.component.UserChallengeInfoCard
+import com.ilsangtech.ilsang.feature.profile.model.ChallengeDetailUiModel
+import com.ilsangtech.ilsang.feature.profile.model.ChallengeDetailUiState
 
 @Composable
 internal fun ChallengeScreen(
-    receiptImageId: String?,
-    questImageId: String?,
-    likeCount: Int,
-    title: String,
+    viewModel: ChallengeViewModel = hiltViewModel(),
     popBackStack: () -> Unit
+) {
+    val challengeDetailUiState by viewModel.challengeDetailUiState.collectAsStateWithLifecycle()
+
+    ChallengeScreen(
+        uiState = challengeDetailUiState,
+        onBackButtonClick = popBackStack
+    )
+}
+
+@Composable
+private fun ChallengeScreen(
+    uiState: ChallengeDetailUiState,
+    onBackButtonClick: () -> Unit
 ) {
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -47,28 +73,62 @@ internal fun ChallengeScreen(
                 .fillMaxSize()
                 .statusBarsPadding()
         ) {
-            ChallengeScreenHeader(onBackButtonClick = popBackStack)
-            Box(
+            ChallengeScreenHeader(onBackButtonClick)
+            LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
+                    .background(background),
+                contentPadding = PaddingValues(bottom = 72.dp)
             ) {
-                AsyncImage(
-                    modifier = Modifier.fillMaxSize(),
-                    model = BuildConfig.IMAGE_URL + receiptImageId,
-                    contentScale = ContentScale.Fit,
-                    contentDescription = null
-                )
-                UserChallengeInfoCard(
-                    modifier = Modifier
-                        .padding(horizontal = 20.dp)
-                        .padding(bottom = 16.dp)
-                        .navigationBarsPadding()
-                        .align(Alignment.BottomCenter),
-                    questImageId = questImageId,
-                    likeCount = likeCount,
-                    title = title
-                )
+                when (uiState) {
+                    ChallengeDetailUiState.Loading -> {
+                        item(key = "로딩", contentType = "Loading") {
+                            Box(
+                                modifier = Modifier.fillParentMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator(color = gray200)
+                            }
+                        }
+                    }
+
+                    ChallengeDetailUiState.Error -> {}
+                    is ChallengeDetailUiState.Success -> {
+                        val uiModel = uiState.data
+                        if (uiModel.missionType is MissionType.Photo) {
+                            item(key = "퀘스트 수행 이미지", contentType = "Image") {
+                                MissionHistoryDetailPhotoItem(imageId = uiModel.submitImageId)
+                            }
+                        }
+                        item(key = "퀘스트 정보", contentType = "Info") {
+                            MissionHistoryDetailInfoItem(
+                                title = uiModel.title,
+                                questType = uiModel.questType,
+                                missionType = uiModel.missionType,
+                                likeCount = uiModel.likeCount,
+                                createdAt = uiModel.createdAt
+                            )
+                        }
+                        item(key = "퀘스트 정보 여백", contentType = "Spacer") {
+                            Spacer(Modifier.height(24.dp))
+                        }
+                        item(key = "퀘스트 상세 정보", contentType = "DetailInfo") {
+                            Column(
+                                modifier = Modifier.padding(horizontal = 20.dp),
+                                verticalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                MissionHistoryDetailPointItem(
+                                    metroGainPoint = uiModel.metroGainPoint,
+                                    commercialGainPoint = uiModel.commercialGainPoint,
+                                    contributionGainPoint = uiModel.contributionGainPoint
+                                )
+                                MissionHistoryDetailWriterItem(writerName = uiModel.writerName)
+                                MissionHistoryDetailDateItem(createdAt = uiModel.createdAt)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -113,25 +173,25 @@ private fun ChallengeScreenHeader(
 
 @Preview
 @Composable
-private fun ChallengeScreenPreview() {
-    val challenge = Challenge(
-        challengeId = "",
-        createdAt = "",
-        hateCnt = 12,
-        likeCnt = 13,
-        missionTitle = "겨울 간식 먹기",
-        questImage = "",
-        receiptImageId = "",
-        status = "",
-        userNickName = "",
-        viewCount = 1302
+private fun ChallengeScreenSuccessPreview() {
+    val uiModel = ChallengeDetailUiModel(
+        missionHistoryId = 1,
+        title = "맛있는 음식 사진 찍기",
+        submitImageId = "image_id_123",
+        viewCount = 120,
+        likeCount = 45,
+        createdAt = "2023.10.27",
+        commercialAreaCode = "12345",
+        missionType = MissionType.Photo,
+        writerName = "일상챌린저",
+        commercialGainPoint = 50,
+        metroGainPoint = 10,
+        contributionGainPoint = 5,
+        quiz = null,
+        questType = QuestType.Normal
     )
-
     ChallengeScreen(
-        receiptImageId = challenge.receiptImageId,
-        questImageId = challenge.questImage,
-        likeCount = challenge.likeCnt,
-        title = challenge.missionTitle,
-        popBackStack = {}
+        uiState = ChallengeDetailUiState.Success(uiModel),
+        onBackButtonClick = {}
     )
 }
