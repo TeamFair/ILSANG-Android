fda373e92b7910223bc67eb233bc79e3f9ea565a
swkim1 <swkim1@hds-secu.com>
2025-11-06 17:25:32 +0900

    [Feat/#277] 화면 크기에 따라 반복 타입 퀘스트 뱃지 크기 및 텍스트 스타일 조정

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/RepeatQuestTypeBadge.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/RepeatQuestTypeBadge.kt
@@ -12,69 +12,42 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ilsangtech.ilsang.core.model.quest.QuestType
 import com.ilsangtech.ilsang.designsystem.theme.badge02TextStyle
+import com.ilsangtech.ilsang.designsystem.theme.pretendardFontFamily
 import com.ilsangtech.ilsang.designsystem.theme.toSp
 
-@Composable
-fun RepeatQuestTypeBadge(
-    modifier: Modifier = Modifier,
-    repeatType: String
-) {
-    val badgeBrush = when (repeatType) {
-        "일간" -> Brush.horizontalGradient(
-            colors = listOf(
-                Color(0xFF7D17FF),
-                Color(0xFFA45DFF)
-            )
-        )
-
-        "주간" -> Brush.horizontalGradient(
-            colors = listOf(
-                Color(0xFF45009E),
-                Color(0xFF7000FF)
-            )
-        )
-
-        else -> Brush.horizontalGradient(
-            colors = listOf(
-                Color(0xFF00DA30),
-                Color(0xFFD1FF05)
-            )
-        )
-    }
-
-    Box(
-        modifier = modifier
-            .size(
-                width = 40.dp,
-                height = 20.dp
-            )
-            .background(
-                brush = badgeBrush,
-                shape = RoundedCornerShape(20.dp)
-            ),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = repeatType,
-            style = badge02TextStyle,
-            color = if (repeatType == "월간") {
-                monthlyBadgeTextColor
-            } else {
-                Color.White
-            }
-        )
-    }
-}
 
 @Composable
 fun RepeatQuestTypeBadge(
     modifier: Modifier = Modifier,
+    isSmallSize: Boolean = false,
     repeatType: QuestType.Repeat
 ) {
+    val badgeWidth = if (isSmallSize) 32.dp else 40.dp
+    val badgeHeight = if (isSmallSize) 16.dp else 20.dp
+
+    val textStyle = if (isSmallSize) {
+        TextStyle(
+            fontFamily = pretendardFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 8.dp.toSp(),
+            lineHeight = 9.6.dp.toSp(),
+            letterSpacing = (-0.24).dp.toSp()
+        )
+    } else {
+        badge02TextStyle.copy(
+            fontSize = 10.dp.toSp(),
+            lineHeight = 12.dp.toSp(),
+            letterSpacing = (-0.3).dp.toSp()
+        )
+    }
+
+
     val badgeBrush = when (repeatType) {
         QuestType.Repeat.Daily -> Brush.horizontalGradient(
             colors = listOf(
@@ -101,8 +74,8 @@ fun RepeatQuestTypeBadge(
     Box(
         modifier = modifier
             .size(
-                width = 40.dp,
-                height = 20.dp
+                width = badgeWidth,
+                height = badgeHeight
             )
             .background(
                 brush = badgeBrush,
@@ -116,11 +89,7 @@ fun RepeatQuestTypeBadge(
                 QuestType.Repeat.Weekly -> "주간"
                 QuestType.Repeat.Monthly -> "월간"
             },
-            style = badge02TextStyle.copy(
-                fontSize = 10.dp.toSp(),
-                lineHeight = 12.dp.toSp(),
-                letterSpacing = (-0.3).dp.toSp()
-            ),
+            style = textStyle,
             color = if (repeatType is QuestType.Repeat.Monthly) {
                 monthlyBadgeTextColor
             } else {
@@ -138,8 +107,8 @@ private fun RepeatQuestTypeBadgePreview() {
     Column(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        RepeatQuestTypeBadge(repeatType = "주간")
-        RepeatQuestTypeBadge(repeatType = "일간")
-        RepeatQuestTypeBadge(repeatType = "월간")
+        RepeatQuestTypeBadge(repeatType = QuestType.Repeat.Daily)
+        RepeatQuestTypeBadge(repeatType = QuestType.Repeat.Weekly)
+        RepeatQuestTypeBadge(repeatType = QuestType.Repeat.Monthly)
     }
 }
