f890056a427245ef97c1ba74799b1ee1ca911d72
kimseongwoo <tjddn929@naver.com>
2025-11-08 15:44:06 +0900

    [Chore] 버전 1.5.1 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ilsangtech.ilsang"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 16
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
