49d9ef573bad39d4da0ecb8748e51b769ddb61da
swkim1 <swkim1@hds-secu.com>
2025-11-10 12:47:11 +0900

    [Refactor/#281] 이모지(Emoji) 관련 파일 제거

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/Emoji.kt
+++
@@ -1,8 +0,0 @@
-package com.ilsangtech.ilsang.core.model
-
-data class Emoji(
-    val likeId: String?,
-    val isLike: Boolean,
-    val hateId: String?,
-    val isHate: Boolean
-)
--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/EmojiType.kt
+++
@@ -1,6 +0,0 @@
-package com.ilsangtech.ilsang.core.model
-
-enum class EmojiType {
-    LIKE,
-    HATE
-}
