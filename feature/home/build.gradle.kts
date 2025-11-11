705802dd8cec965c22eca2712666fee648691d75
swkim1 <swkim1@hds-secu.com>
2025-11-11 16:31:05 +0900

    [Chore/#284] home 모듈에 coroutine-test, turbine 라이브러리 의존성 추가

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
