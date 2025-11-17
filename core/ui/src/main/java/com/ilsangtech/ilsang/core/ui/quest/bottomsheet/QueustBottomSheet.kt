c34d60451287f37681dcb0040f38f239489ed1ae
swkim1 <swkim1@hds-secu.com>
2025-11-17 11:21:50 +0900

    [Design/#290] 퀘스트 하단 시트 영역이 너무 커질 경우를 대비해 컨텐츠 영역을 스크롤 가능하도록 적용

--- core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/bottomsheet/QueustBottomSheet.kt
+++ core/ui/src/main/java/com/ilsangtech/ilsang/core/ui/quest/bottomsheet/QueustBottomSheet.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -80,6 +82,9 @@ fun QuestBottomSheet(
             onFavoriteClick = onFavoriteClick
         )
         QuestBottomSheetContent(
+            modifier = Modifier
+                .weight(weight = 1f, fill = false)
+                .verticalScroll(rememberScrollState()),
             quest = quest,
             onImageClick = onMissionImageClick,
             onRewardButtonClick = { showQuestRewardCouponDialog = true }
