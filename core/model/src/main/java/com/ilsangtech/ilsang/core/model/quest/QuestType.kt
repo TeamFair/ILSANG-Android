cc19b9566adb264ab2d9a55e5803df55f68787e7
swkim1 <swkim1@hds-secu.com>
2025-12-10 11:47:36 +0900

    [Chore/#294] QuestType 도메인 모델에 Serialization 어노테이션 추가

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/quest/QuestType.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/quest/QuestType.kt
@@ -1,13 +1,25 @@
 package com.ilsangtech.ilsang.core.model.quest
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface QuestType {
+    @Serializable
     data object Normal : QuestType
+
+    @Serializable
     sealed interface Repeat : QuestType {
+        @Serializable
         data object Daily : Repeat
+
+        @Serializable
         data object Weekly : Repeat
+
+        @Serializable
         data object Monthly : Repeat
     }
 
+    @Serializable
     data object Event : QuestType
 
     companion object
