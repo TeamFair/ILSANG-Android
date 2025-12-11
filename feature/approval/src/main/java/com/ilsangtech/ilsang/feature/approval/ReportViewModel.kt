aa463bf94b7bdf98c3f8577f5dd1f17533f3c963
swkim1 <swkim1@hds-secu.com>
2025-12-11 17:20:15 +0900

    [Feat/#295] 신고하기 화면 뷰모델 클래스 상태 관리 및 미션 신고하기 기능 구현

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ReportViewModel.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ReportViewModel.kt
@@ -1,10 +1,55 @@
 package com.ilsangtech.ilsang.feature.approval
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.ilsangtech.ilsang.core.domain.MissionRepository
+import com.ilsangtech.ilsang.feature.approval.model.ReportResultUiState
+import com.ilsangtech.ilsang.feature.approval.model.ReportTypeUiModel
+import com.ilsangtech.ilsang.feature.approval.navigation.ReportRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ReportViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val missionRepository: MissionRepository
 ) : ViewModel() {
+    private val missionHistoryId = savedStateHandle.toRoute<ReportRoute>().missionHistoryId
+    private val commentId = savedStateHandle.toRoute<ReportRoute>().commentId
+
+    private val _selectedReportTypes = MutableStateFlow<List<ReportTypeUiModel>>(emptyList())
+    val selectedReportTypes = _selectedReportTypes.asStateFlow()
+
+    private val _reportResultUiState =
+        MutableStateFlow<ReportResultUiState>(ReportResultUiState.UnReported)
+    val reportResultUiState = _reportResultUiState.asStateFlow()
+
+    fun updateSelectedReportTypes(reportType: ReportTypeUiModel) {
+        _selectedReportTypes.update {
+            if (it.contains(reportType)) {
+                it.filter { type -> type != reportType }
+            } else {
+                it + reportType
+            }
+        }
+    }
+
+    fun report() {
+        check(missionHistoryId != null || commentId != null)
+        viewModelScope.launch {
+            val reason = selectedReportTypes.value.joinToString(",") { it.toString() }
+            missionHistoryId?.let { missionHistoryId ->
+                missionRepository.reportMissionHistory(
+                    missionHistoryId = missionHistoryId,
+                    reason = reason
+                )
+            }
+        }
+    }
 }
