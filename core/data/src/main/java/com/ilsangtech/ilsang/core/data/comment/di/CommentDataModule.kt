625b95c44d457c49683a62d31127808a28d6af98
swkim1 <swkim1@hds-secu.com>
2025-12-12 13:51:23 +0900

    [Chore/#296] CommentRepository 의존성 주입 함수 생성

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/di/CommentDataModule.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/di/CommentDataModule.kt
@@ -2,17 +2,28 @@ package com.ilsangtech.ilsang.core.data.comment.di
 
 import com.ilsangtech.ilsang.core.data.comment.datasource.CommentDataSource
 import com.ilsangtech.ilsang.core.data.comment.datasource.CommentDataSourceImpl
+import com.ilsangtech.ilsang.core.data.comment.repository.CommentRepositoryImpl
+import com.ilsangtech.ilsang.core.domain.CommentRepository
 import com.ilsangtech.ilsang.core.network.api.CommentApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class CommentDataModule {
     @Provides
+    @Singleton
     fun provideCommentDataSource(apiService: CommentApiService): CommentDataSource {
         return CommentDataSourceImpl(apiService)
     }
+
+    @Provides
+    @Singleton
+    fun provideCommentRepository(dataSource: CommentDataSource): CommentRepository {
+        return CommentRepositoryImpl(dataSource)
+    }
+
 }
