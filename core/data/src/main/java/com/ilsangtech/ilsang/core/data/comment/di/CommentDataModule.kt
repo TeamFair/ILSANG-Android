1bc1464eb2e79cee4c61c0ffe8ab507b277c4348
swkim1 <swkim1@hds-secu.com>
2025-12-12 13:39:19 +0900

    [Chore/#296] CommentDataSource 의존성 주입 함수 생성

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/di/CommentDataModule.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/di/CommentDataModule.kt
@@ -1,10 +1,18 @@
 package com.ilsangtech.ilsang.core.data.comment.di
 
+import com.ilsangtech.ilsang.core.data.comment.datasource.CommentDataSource
+import com.ilsangtech.ilsang.core.data.comment.datasource.CommentDataSourceImpl
+import com.ilsangtech.ilsang.core.network.api.CommentApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 class CommentDataModule {
+    @Provides
+    fun provideCommentDataSource(apiService: CommentApiService): CommentDataSource {
+        return CommentDataSourceImpl(apiService)
+    }
 }
