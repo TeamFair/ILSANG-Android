7352683b9f8eef017e81608d19ff7de4255bdacb
swkim1 <swkim1@hds-secu.com>
2025-12-12 14:34:57 +0900

    [Feat/#296] 댓글, 댓글 작성자 DTO를 도메인 모델로 매핑하는 함수 구현

---
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/mapper/CommentMapper.kt
@@ -0,0 +1,28 @@
+package com.ilsangtech.ilsang.core.data.comment.mapper
+
+import com.ilsangtech.ilsang.core.data.title.mapper.toTitle
+import com.ilsangtech.ilsang.core.model.comment.Comment
+import com.ilsangtech.ilsang.core.model.comment.CommentWriter
+import com.ilsangtech.ilsang.core.network.model.comment.CommentNetworkModel
+import com.ilsangtech.ilsang.core.network.model.comment.CommentWriterNetworkModel
+
+internal fun CommentNetworkModel.toComment(): Comment {
+    return Comment(
+        id = id,
+        parentId = parentId,
+        comment = comment,
+        writer = writer.toCommentWriter(),
+        createdAt = createdAt,
+        hasReported = hasReportedYn,
+        isDeleted = deleteYn
+    )
+}
+
+internal fun CommentWriterNetworkModel.toCommentWriter(): CommentWriter {
+    return CommentWriter(
+        userId = userId,
+        nickname = nickname,
+        profileImageId = profileImageId,
+        title = title.toTitle()
+    )
+}
