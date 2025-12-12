873d1537b9567861e7776456bff733479955ec70
swkim1 <swkim1@hds-secu.com>
2025-12-12 13:41:18 +0900

    [Feat/#296] CommentDataSource 내 댓글 생성 함수 선언 및 구현

--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/datasource/CommentDataSource.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/datasource/CommentDataSource.kt
@@ -1,4 +1,9 @@
 package com.ilsangtech.ilsang.core.data.comment.datasource
 
 interface CommentDataSource {
+    suspend fun createComment(
+        missionHistoryId: Int,
+        parentId: Int?,
+        comment: String
+    )
 }
--- core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/datasource/CommentDataSourceImpl.kt
+++ core/data/src/main/java/com/ilsangtech/ilsang/core/data/comment/datasource/CommentDataSourceImpl.kt
@@ -1,8 +1,22 @@
 package com.ilsangtech.ilsang.core.data.comment.datasource
 
 import com.ilsangtech.ilsang.core.network.api.CommentApiService
+import com.ilsangtech.ilsang.core.network.model.comment.CommentCreationRequest
 
 class CommentDataSourceImpl(
     private val commentApiService: CommentApiService
 ) : CommentDataSource {
+    override suspend fun createComment(
+        missionHistoryId: Int,
+        parentId: Int?,
+        comment: String
+    ) {
+        return commentApiService.createComment(
+            missionHistoryId = missionHistoryId,
+            request = CommentCreationRequest(
+                parentId = parentId,
+                comment = comment
+            )
+        )
+    }
 }
