446fba0b9647854beb77c5147dfb5ed2f13aa775
swkim1 <swkim1@hds-secu.com>
2025-12-12 14:23:13 +0900

    [Feat/#296] CommentApiService 내 댓글 조회 API 호출 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CommentApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CommentApiService.kt
@@ -1,10 +1,13 @@
 package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.comment.CommentCreationRequest
+import com.ilsangtech.ilsang.core.network.model.comment.CommentNetworkModel
 import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CommentApiService {
     @POST("api/v1/mission/user/history/comment/{missionHistoryId}")
@@ -15,4 +18,9 @@ interface CommentApiService {
 
     @DELETE("api/v1/mission/user/history/comment/{commentId}")
     suspend fun deleteComment(@Path("commentId") commentId: Int)
+
+    @GET("api/v1/mission/user/history/comment")
+    suspend fun getComments(
+        @Query("missionHistoryId") missionHistoryId: Int
+    ): List<CommentNetworkModel>
 }
