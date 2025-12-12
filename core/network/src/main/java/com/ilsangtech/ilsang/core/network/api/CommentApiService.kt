be204c49d2b7282d8ec0fe8dc175acef4379c703
swkim1 <swkim1@hds-secu.com>
2025-12-12 13:59:57 +0900

    [Feat/#296] CommentApiService 내 댓글 삭제 API 호출 함수 생성

--- core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CommentApiService.kt
+++ core/network/src/main/java/com/ilsangtech/ilsang/core/network/api/CommentApiService.kt
@@ -2,6 +2,7 @@ package com.ilsangtech.ilsang.core.network.api
 
 import com.ilsangtech.ilsang.core.network.model.comment.CommentCreationRequest
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -11,4 +12,7 @@ interface CommentApiService {
         @Path("missionHistoryId") missionHistoryId: Int,
         @Body request: CommentCreationRequest
     )
+
+    @DELETE("api/v1/mission/user/history/comment/{commentId}")
+    suspend fun deleteComment(@Path("commentId") commentId: Int)
 }
