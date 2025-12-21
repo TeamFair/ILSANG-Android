000020c448687101b894f75059d5242cad25c5b1
kimseongwoo <tjddn929@naver.com>
2025-12-21 22:49:30 +0900

    [Feat/#305] 뷰모델 파라미터로 받는 ApprovalDetailScreen 함수, 프리뷰 함수 생성

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalDetailScreen.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalDetailScreen.kt
@@ -1,7 +1,21 @@
 package com.ilsangtech.ilsang.feature.approval
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+internal fun ApprovalDetailScreen(
+    viewModel: ApprovalDetailViewModel = hiltViewModel()
+) {
+}
 
 @Composable
 private fun ApprovalDetailScreen() {
+}
+
+@Preview
+@Composable
+private fun ApprovalDetailScreenPreview() {
+    ApprovalDetailScreen()
 }
