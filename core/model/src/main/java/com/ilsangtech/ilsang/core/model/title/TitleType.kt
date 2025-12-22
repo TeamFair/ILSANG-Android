17b5f45c36ce536002d439a0da4c737b5e54c72b
swkim1 <swkim1@hds-secu.com>
2025-12-22 14:58:13 +0900

    [Chore/#305] 화면 이동 데이터 전달을 위해 TitleType에 Serializable 어노테이션 추가

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/title/TitleType.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/title/TitleType.kt
@@ -1,8 +1,18 @@
 package com.ilsangtech.ilsang.core.model.title
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface TitleType {
+    @Serializable
     data object None : TitleType
+
+    @Serializable
     data object Metro : TitleType
+
+    @Serializable
     data object Commercial : TitleType
+
+    @Serializable
     data object Contribution : TitleType
 }
