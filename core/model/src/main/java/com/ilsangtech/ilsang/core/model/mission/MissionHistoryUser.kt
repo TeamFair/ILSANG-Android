4beabef13305112559330d567e8b846cfc31ac42
swkim1 <swkim1@hds-secu.com>
2025-12-22 14:56:30 +0900

    [Chore/#305] 화면 이동 데이터 전달을 위해 MissionHistoryUser에 Serializable 어노테이션 추가

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/MissionHistoryUser.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/MissionHistoryUser.kt
@@ -1,7 +1,9 @@
 package com.ilsangtech.ilsang.core.model.mission
 
 import com.ilsangtech.ilsang.core.model.title.Title
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class MissionHistoryUser(
     val userId: String,
     val nickname: String,
