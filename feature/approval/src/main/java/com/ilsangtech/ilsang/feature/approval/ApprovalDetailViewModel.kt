b35a4c2a38695abfd8f5e8ebc4869ee3e8ad438f
swkim1 <swkim1@hds-secu.com>
2025-12-22 15:28:27 +0900

    [Feat/#305] ApprovalDetailViewModel에 savedStateHandle를 통해 화면 이동 시 받은 미션 이력 정보 관리

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalDetailViewModel.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalDetailViewModel.kt
@@ -1,10 +1,17 @@
 package com.ilsangtech.ilsang.feature.approval
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.toRoute
+import com.ilsangtech.ilsang.feature.approval.navigation.ApprovalDetailRoute
+import com.ilsangtech.ilsang.feature.approval.navigation.missionHistoryUiModelTypeMap
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ApprovalDetailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+    val missionHistoryUiModel =
+        savedStateHandle.toRoute<ApprovalDetailRoute>(missionHistoryUiModelTypeMap).missionHistory
 }
