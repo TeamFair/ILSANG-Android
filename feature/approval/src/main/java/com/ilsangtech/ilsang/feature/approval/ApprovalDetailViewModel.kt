5a78737bf379c95f3d4e288cd75c271024d42832
swkim1 <swkim1@hds-secu.com>
2025-12-29 16:00:34 +0900

    [Feat/#305] ApprovalDetailViewModel 내 댓글 추가, 삭제 함수 추가

--- feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalDetailViewModel.kt
+++ feature/approval/src/main/java/com/ilsangtech/ilsang/feature/approval/ApprovalDetailViewModel.kt
@@ -1,5 +1,7 @@
 package com.ilsangtech.ilsang.feature.approval
 
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.clearText
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,16 +10,26 @@ import com.ilsangtech.ilsang.core.domain.CommentRepository
 import com.ilsangtech.ilsang.core.domain.UserRepository
 import com.ilsangtech.ilsang.core.model.comment.Comment
 import com.ilsangtech.ilsang.core.model.user.MyInfo
+import com.ilsangtech.ilsang.feature.approval.model.CommentUiModel
 import com.ilsangtech.ilsang.feature.approval.model.CommentUiState
 import com.ilsangtech.ilsang.feature.approval.model.toUiModel
 import com.ilsangtech.ilsang.feature.approval.navigation.ApprovalDetailRoute
 import com.ilsangtech.ilsang.feature.approval.navigation.missionHistoryUiModelTypeMap
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -31,31 +43,98 @@ class ApprovalDetailViewModel @Inject constructor(
     private val missionHistoryId = missionHistoryUiModel.missionHistoryId
     private val missionUserId = missionHistoryUiModel.user.userId
 
-    val commentUiState = combine<MyInfo, List<Comment>, CommentUiState>(
-        userRepository.getMyInfo(),
-        flow { emit(commentRepository.getComments(missionHistoryId)) }
-    ) { myInfo, comments ->
-        val commentUiModels = comments.flatMap { comment ->
-            listOf(comment) + comment.children
-        }.map { comment ->
-            comment.toUiModel(
-                isMyComment = comment.writer.userId == myInfo.id,
-                isMissionHistoryUser = comment.writer.userId == missionUserId
-            )
-        }
-        val validCommentsSize = commentUiModels
-            .filter { comment -> !comment.isDeleted && !comment.isReported }
-            .size
-
-        CommentUiState.Success(
-            comments = commentUiModels,
-            validCommentsSize = validCommentsSize
-        )
-    }.catch { e ->
-        emit(CommentUiState.Error)
+    private val selectedComment = MutableStateFlow<CommentUiModel?>(null)
+    val selectedCommentWriter = selectedComment.map { comment ->
+        comment?.commentWriter?.nickname
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000),
-        initialValue = CommentUiState.Loading
+        initialValue = null
     )
+
+    val commentTextField = TextFieldState()
+
+    private val commentRetryFlow = MutableSharedFlow<Unit>()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val commentUiState = commentRetryFlow
+        .onStart {
+            emit(Unit)
+        }.flatMapLatest {
+            combine<MyInfo, List<Comment>, CommentUiState>(
+                userRepository.getMyInfo(),
+                flow { emit(commentRepository.getComments(missionHistoryId)) }
+            ) { myInfo, comments ->
+                val commentUiModels = comments.flatMap { comment ->
+                    listOf(comment) + comment.children
+                }.map { comment ->
+                    comment.toUiModel(
+                        isMyComment = comment.writer.userId == myInfo.id,
+                        isMissionHistoryUser = comment.writer.userId == missionUserId
+                    )
+                }
+                val validCommentsSize = commentUiModels
+                    .filter { comment -> !comment.isDeleted && !comment.isReported }
+                    .size
+
+                CommentUiState.Success(
+                    comments = commentUiModels,
+                    validCommentsSize = validCommentsSize
+                )
+            }
+        }.catch { e ->
+            emit(CommentUiState.Error)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = CommentUiState.Loading
+        )
+
+    private val _listScrollRequestPosition = MutableStateFlow<Int?>(null)
+    val listScrollPosition: StateFlow<Int?> = _listScrollRequestPosition
+
+    fun selectComment(commentUiModel: CommentUiModel) {
+        selectedComment.update { commentUiModel }
+    }
+
+    fun unselectComment() {
+        selectedComment.update { null }
+    }
+
+    fun createComment() {
+        viewModelScope.launch {
+            commentRepository.createComment(
+                missionHistoryId = missionHistoryId,
+                parentId = selectedComment.value?.id,
+                comment = commentTextField.text.toString()
+            ).onSuccess {
+                commentRetryFlow.emit(Unit)
+                (commentUiState.value as? CommentUiState.Success)?.let { state ->
+                    _listScrollRequestPosition.update {
+                        if (selectedComment.value?.id == null) {
+                            state.comments.size - 1
+                        } else {
+                            state.comments.indexOfLast {
+                                it.parentId == selectedComment.value?.id
+                            }
+                        }
+                    }
+                }
+            }
+            commentTextField.clearText()
+            unselectComment()
+        }
+    }
+
+    fun deleteComment(commentId: Int) {
+        viewModelScope.launch {
+            commentRepository.deleteComment(commentId).onSuccess {
+                commentRetryFlow.emit(Unit)
+            }
+        }
+    }
+
+    fun clearScrollPosition() {
+        _listScrollRequestPosition.update { null }
+    }
 }
