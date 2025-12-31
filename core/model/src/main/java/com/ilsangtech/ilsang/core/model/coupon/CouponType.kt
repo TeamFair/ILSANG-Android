eec2db04b6cf3822d8f50eec5698e7627f4c1cc6
swkim1 <swkim1@hds-secu.com>
2025-12-31 12:21:14 +0900

    [Feat/#306] 쿠폰 타입 모델 sealed interface 구현

---
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/coupon/CouponType.kt
@@ -0,0 +1,8 @@
+package com.ilsangtech.ilsang.core.model.coupon
+
+sealed interface CouponType {
+    data object Week : CouponType
+    data object Month : CouponType
+    data object Season : CouponType
+    data object RealTime : CouponType
+}
