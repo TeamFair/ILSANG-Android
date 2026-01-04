8d1aa73ffa6cc1911e1c6ee1a612296a0b5d5f37
kimseongwoo <tjddn929@naver.com>
2026-01-04 14:26:00 +0900

    [Feat/#313] 랜덤 미션 이력 도메인 모델에 quest id 추가 및 매핑 로직 적용

--- core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/RandomMissionHistory.kt
+++ core/model/src/main/java/com/ilsangtech/ilsang/core/model/mission/RandomMissionHistory.kt
@@ -3,12 +3,13 @@ package com.ilsangtech.ilsang.core.model.mission
 import com.ilsangtech.ilsang.core.model.quest.QuestType
 
 data class RandomMissionHistory(
+    val missionHistoryId: Int,
+    val questId: Int,
     val commercialAreaCode: String,
     val createdAt: String,
     val currentUserEmojis: List<String>,
     val hateCount: Int,
     val likeCount: Int,
-    val missionHistoryId: Int,
     val submitImageId: String,
     val title: String,
     val writerName: String,
