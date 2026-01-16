dc4943ad840a1cb4cdc704fed1e6cb44dd42f44a
kimseongwoo <tjddn929@naver.com>
2026-01-16 22:19:10 +0900

    [Feat/#319] 빈 QuestDetailScreen 컴포저블 함수 생성

---
+++ feature/quest-detail/src/main/java/com/ilsangtech/ilsang/feature/quest_detail/QuestDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.ilsangtech.ilsang.feature.quest_detail
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun QuestDetailScreen(
+    onBackButtonClick: () -> Unit
+) {
+}
