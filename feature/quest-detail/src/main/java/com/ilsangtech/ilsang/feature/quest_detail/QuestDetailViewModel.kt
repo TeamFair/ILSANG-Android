3ec4423bbec503207b304a39903d0b7ad13931b7
kimseongwoo <tjddn929@naver.com>
2026-01-31 21:23:24 +0900

    [Feat/#319] QuestDetailViewModel에 questDetailUiState 상태 관리 구현

--- feature/quest-detail/src/main/java/com/ilsangtech/ilsang/feature/quest_detail/QuestDetailViewModel.kt
+++ feature/quest-detail/src/main/java/com/ilsangtech/ilsang/feature/quest_detail/QuestDetailViewModel.kt
@@ -2,10 +2,17 @@ package com.ilsangtech.ilsang.feature.quest_detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import com.ilsangtech.ilsang.core.domain.QuestRepository
+import com.ilsangtech.ilsang.feature.quest_detail.model.QuestDetailUiState
+import com.ilsangtech.ilsang.feature.quest_detail.model.toUiModel
 import com.ilsangtech.ilsang.feature.quest_detail.navigation.QuestDetailRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,5 +21,14 @@ class QuestDetailViewModel @Inject constructor(
     private val questRepository: QuestRepository
 ) : ViewModel() {
     private val questId = savedStateHandle.toRoute<QuestDetailRoute>().questId
-    
+    val questDetailUiState =
+        questRepository.getQuestDetail(questId).map { questDetail ->
+            QuestDetailUiState.Success(questDetail.toUiModel()) as QuestDetailUiState
+        }.catch {
+            emit(QuestDetailUiState.Error)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = QuestDetailUiState.Loading
+        )
 }
